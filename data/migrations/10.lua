ba1eff726479be1ec593f8161a52922522afc23c
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2021-04-28 22:48:54 -0300

    Clear migrations, indent schema and add 'towns' table (#8)
    Removed migrations, indented the schema and added the "towns" table

--- data/migrations/10.lua
+++
@@ -1,5 +0,0 @@
-function onUpdateDatabase()
-	Spdlog.info("Updating database to version 11 (Guilds Balance)")
-	db.query("ALTER TABLE `guilds` ADD `balance` bigint(20) UNSIGNED NOT NULL DEFAULT '0';")
-	return true
-end

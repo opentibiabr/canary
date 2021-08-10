c16c630f8e8e70d874ca596a84e2aa36e509b288
dudantas <eduardo.dantas@hotmail.com.br>
2021-08-10 13:39:26 -0300

    Fix indent

--- data/lib/core/functions/tables.lua
+++ data/lib/core/functions/tables.lua
@@ -114,8 +114,8 @@ function pairsByKeys(t, f)
 	local a = {}
 	for n in pairs(t) do table.insert(a, n) end
 	table.sort(a, f)
-	local i = 0      -- iterator variable
-	local iter = function ()   -- iterator function
+	local i = 0 -- iterator variable
+	local iter = function () -- iterator function
 		i = i + 1
 		if a[i] == nil then return nil
 			else return a[i], t[a[i]]

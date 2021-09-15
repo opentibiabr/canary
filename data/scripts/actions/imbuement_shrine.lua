3e889e8ea544a2bc8c102238284d63fd3e29591a
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2021-09-15 14:51:04 -0300

    Remove check

--- data/scripts/actions/imbuement_shrine.lua
+++ data/scripts/actions/imbuement_shrine.lua
@@ -1,10 +1,6 @@
 local imbuement = Action()
 
 function imbuement.onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	if player:getStorageValue(Storage.ForgottenKnowledge.Tomes) ~= 1 then
-		return player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You did not collect enough knowledge from the ancient Shapers. Visit the Shaper temple in Thais for help.")
-	end
-
 	player:sendImbuementPanel(target, true)
 	return true
 end

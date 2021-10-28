e854ac3ec1950342850c4aab70603c182eb8c361
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2021-10-28 02:20:23 -0300

    Enhancement of save interval time

--- data/scripts/globalevents/save_interval.lua
+++ data/scripts/globalevents/save_interval.lua
@@ -1,30 +1,3 @@
--- To switch to minutes change " .. math.floor(interval/3600000) .. " hour(s)!" for " .. math.floor(interval/66000) .. " minutes!"
--- (configKeys.SAVE_INTERVAL_TIME) * 60 * 60 * 1000) for (configKeys.SAVE_INTERVAL_TIME) * 60 * 1000)
-
-local function serverSave(interval)
-	if configManager.getBoolean(configKeys.TOGLE_SAVE_INTERVAL_CLEAN_MAP) then
-		cleanMap()
-	end
-	
-	saveServer()
-	local message = "Server save complete. Next save in " .. math.floor(interval/3600000) .. " hour(s)!", MESSAGE_STATUS_WARNING
-	Webhook.send("Server save", message, WEBHOOK_COLOR_WARNING)
-	Game.broadcastMessage(message, MESSAGE_GAME_HIGHLIGHT)
-end
-
-local save = GlobalEvent("save")
-function save.onTime(interval)
-	local remaningTime = 60 * 1000
-	if configManager.getBoolean(configKeys.TOGLE_SAVE_INTERVAL) then
-		local message = "The server will save all accounts within " .. (remaningTime/1000) .." seconds. \z
-		You might lag or freeze for 5 seconds, please find a safe place.", MESSAGE_STATUS_WARNING
-		Game.broadcastMessage(message, MESSAGE_GAME_HIGHLIGHT)
-		addEvent(serverSave, remaningTime, interval)
-		return true
-	end
-	return not configManager.getBoolean(configKeys.TOGLE_SAVE_INTERVAL)
-end
-
 local saveIntervalType = configManager.getString(configKeys.SAVE_INTERVAL_TYPE)
 local configTime = configManager.getNumber(configKeys.SAVE_INTERVAL_TIME)
 local time
@@ -36,6 +9,30 @@ elseif saveIntervalType == "hour" then
 	time = 60 * 60 * 1000
 else
 	time = 0;
+end
+
+local function serverSave(interval)
+	if configManager.getBoolean(configKeys.TOGLE_SAVE_INTERVAL_CLEAN_MAP) then
+		cleanMap()
+	end
+	
+	saveServer()
+	local message = "Server save complete. Next save in %d %s(s)!"
+	Webhook.send("Server save", message, WEBHOOK_COLOR_WARNING)
+	Game.broadcastMessage(string.format(message, configTime, saveIntervalType), MESSAGE_GAME_HIGHLIGHT)
+end
+
+local save = GlobalEvent("save")
+function save.onTime(interval)
+	local remaningTime = 60 * 1000
+	if configManager.getBoolean(configKeys.TOGLE_SAVE_INTERVAL) then
+		local message = "The server will save all accounts within " .. (remaningTime/1000) .." seconds. \z
+		You might lag or freeze for 5 seconds, please find a safe place."
+		Game.broadcastMessage(message, MESSAGE_GAME_HIGHLIGHT)
+		addEvent(serverSave, remaningTime, interval)
+		return true
+	end
+	return not configManager.getBoolean(configKeys.TOGLE_SAVE_INTERVAL)
 end
 
 save:interval(configTime * time)

1f0a3b55ca205ac3c10ea23ac1b8390edb8b2d1b
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2021-10-28 17:06:08 -0300

    Enhance save interval time

--- data/scripts/globalevents/save_interval.lua
+++ data/scripts/globalevents/save_interval.lua
@@ -1,33 +1,17 @@
-local saveIntervalType = configManager.getString(configKeys.SAVE_INTERVAL_TYPE)
-local configTime = configManager.getNumber(configKeys.SAVE_INTERVAL_TIME)
-local time
-if saveIntervalType == "second" then
-	time = 1000
-elseif saveIntervalType == "minute" then
-	time = 60 * 1000
-elseif saveIntervalType == "hour" then
-	time = 60 * 60 * 1000
-else
-	time = 0;
-end
-
 local function serverSave(interval)
 	if configManager.getBoolean(configKeys.TOGLE_SAVE_INTERVAL_CLEAN_MAP) then
 		cleanMap()
 	end
-	
+
 	saveServer()
 	local message = "Server save complete. Next save in %d %s(s)!"
 	Webhook.send("Server save", message, WEBHOOK_COLOR_WARNING)
-	Game.broadcastMessage(string.format(message, configTime, saveIntervalType), MESSAGE_GAME_HIGHLIGHT)
+	Game.broadcastMessage(string.format(message, SAVE_INTERVAL_CONFIG_TIME, SAVE_INTERVAL_TYPE), MESSAGE_GAME_HIGHLIGHT)
 end
 
 local save = GlobalEvent("save")
-function save.onTime(interval)
-	if time == 0 then
-		return Spdlog.error(string.format("Save interval type '%s' is not valid, use 'second', 'minute' or 'hour'", saveIntervalType))
-	end
 
+function save.onTime(interval)
 	local remaningTime = 60 * 1000
 	if configManager.getBoolean(configKeys.TOGLE_SAVE_INTERVAL) then
 		local message = "The server will save all accounts within " .. (remaningTime/1000) .." seconds. \z
@@ -39,8 +23,10 @@ function save.onTime(interval)
 	return not configManager.getBoolean(configKeys.TOGLE_SAVE_INTERVAL)
 end
 
-if time ~= 0 then
-	save:interval(configTime * time)
+if SAVE_INTERVAL_TIME ~= 0 then
+	save:interval(SAVE_INTERVAL_CONFIG_TIME * SAVE_INTERVAL_TIME)
+else
+	return Spdlog.error(string.format("Save interval type '%s' is not valid, use 'second', 'minute' or 'hour'", SAVE_INTERVAL_TYPE))
 end
 
 save:register()

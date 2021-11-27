1c79aaac4068099e1c5e4bb80592e6f7b6623349
Beats-Dh <daniel15042014@hotmail.com>
2021-11-27 17:02:30 -0400

    Remove monster from spawnedmap when they die to avoid longer respawn
    Co-Authored-By: Jakub <38609240+SaiyansKing@users.noreply.github.com>

--- src/creatures/monsters/monster.cpp
+++ src/creatures/monsters/monster.cpp
@@ -199,6 +199,7 @@ void Monster::onRemoveCreature(Creature* creature, bool isLogout)
 
 	if (creature == this) {
 		if (spawnMonster) {
+			spawnMonster->removeMonster(this);
 			spawnMonster->startSpawnMonsterCheck();
 		}
 

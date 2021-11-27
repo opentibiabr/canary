07195008a48895b9dd07bb4501d6db3d86fdd717
Beats-Dh <daniel15042014@hotmail.com>
2021-11-26 22:33:39 -0400

    update?!
    https: //github.com/opentibiabr/optimized_forgottenserver/commit/891ea8b3df2f608c93d675f0104b3a0b49b70f4a
    Co-Authored-By: Jakub <38609240+SaiyansKing@users.noreply.github.com>

--- src/server/network/protocol/protocolgame.cpp
+++ src/server/network/protocol/protocolgame.cpp
@@ -3480,6 +3480,7 @@ void ProtocolGame::sendCyclopediaCharacterTitles()
 void ProtocolGame::sendTournamentLeaderboard()
 {
 	NetworkMessage msg;
+	//playermsg.reset();
 	msg.addByte(0xC5);
 	msg.addByte(0);
 	msg.addByte(0x01);
@@ -5365,6 +5366,8 @@ void ProtocolGame::sendAddCreature(const Creature *creature, const Position &pos
 
 	msg.addByte(shouldAddExivaRestrictions ? 0x01 : 0x00); // exiva button enabled
 	msg.addByte(0x00); // Tournament button
+	msg.addByte(0x0A); // sendPendingStateEntered
+	msg.addByte(0x0F); // sendEnterWorld
 
 	writeToOutputBuffer(msg);
 

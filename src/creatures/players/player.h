c304008910092c41acefa97128071b72f9c50a74
Beats-Dh <daniel15042014@hotmail.com>
2021-12-16 12:33:50 -0400

    Change experience formula
    -Cipbia formula have less multiplication which should make it a little faster
    -Because it have less multiplication it also don't overflow as often, with Cipbia formula it is possible to reach 1034406 level instead of 717217 with Open Tibia formula

--- src/creatures/players/player.h
+++ src/creatures/players/player.h
@@ -188,9 +188,8 @@ class Player final : public Creature, public Cylinder {
 		void addList() override;
 		void removePlayer(bool displayEffect, bool forced = true);
 
-		static uint64_t getExpForLevel(int32_t lv) {
-			lv--;
-			return ((50ULL * lv * lv * lv) - (150ULL * lv * lv) + (400ULL * lv)) / 3ULL;
+		static uint64_t getExpForLevel(uint64_t lv) {
+			return (((lv - 6ULL) * lv + 17ULL) * lv - 12ULL) / 6ULL * 100ULL;
 		}
 
 		uint16_t getStaminaMinutes() const {

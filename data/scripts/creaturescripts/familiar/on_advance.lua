cfee952d6dd83996e3311085b3c7cd8f7e63d229
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2022-01-09 23:52:47 -0300

    Improve function onAdvance

--- data/scripts/creaturescripts/familiar/on_advance.lua
+++ data/scripts/creaturescripts/familiar/on_advance.lua
@@ -2,9 +2,13 @@ local familiarOnAdvance = CreatureEvent("AdvanceFamiliar")
 
 function familiarOnAdvance.onAdvance(player, skill, oldLevel, newLevel)
 	local vocation = FAMILIAR_ID[player:getVocation():getBaseId()]
-	if vocation and newLevel >= 200 and player:isPremium() then
+	if newLevel < 200 and not player:isPremium() then
+		return true
+	end
+
+	if vocation then
 		if player:getFamiliarLooktype() == 0 then
-				player:setFamiliarLooktype(vocation.id)
+			player:setFamiliarLooktype(vocation.id)
 		end
 		if not player:hasFamiliar(vocation.id) then
 			player:addFamiliar(vocation.id)

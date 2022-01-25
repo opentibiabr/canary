0a2e12f5bed5d16e45f37796df32548b39e73734
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2022-01-25 19:27:44 -0300

    Adjust

--- data/scripts/item_classification/item_tiers.lua
+++ data/scripts/item_classification/item_tiers.lua
@@ -1,48 +1,49 @@
-ItemTierClassifications = {
+local itemTierClassifications = {
 	-- Upgrade classification 1
 	[1] = {
 		-- Update tier 0
-		[0] = {Price = 25000}
+		[0] = {price = 25000}
 	},
 	-- Upgrade classification 2
 	[2] = {
 		-- Update tier 0
-		[0] = {Price = 750000},
+		[0] = {price = 750000},
 		-- Update tier 1
-		[1] = {Price = 5000000}
+		[1] = {price = 5000000}
 	},
 	-- Upgrade classification 3
 	[3] = {
-		[0] = {Price = 4000000},
-		[1] = {Price = 10000000},
-		[2] = {Price = 20000000}
+		[0] = {price = 4000000},
+		[1] = {price = 10000000},
+		[2] = {price = 20000000}
 	},
 	-- Upgrade classification 4
 	[4] = {
-		[0] = {Price = 8000000},
-		[1] = {Price = 20000000},
-		[2] = {Price = 40000000},
-		[3] = {Price = 65000000},
-		[4] = {Price = 100000000},
-		[5] = {Price = 250000000},
-		[6] = {Price = 750000000},
-		[7] = {Price = 2500000000},
-		[8] = {Price = 8000000000},
-		[9] = {Price = 15000000000}
+		[0] = {price = 8000000},
+		[1] = {price = 20000000},
+		[2] = {price = 40000000},
+		[3] = {price = 65000000},
+		[4] = {price = 100000000},
+		[5] = {price = 250000000},
+		[6] = {price = 750000000},
+		[7] = {price = 2500000000},
+		[8] = {price = 8000000000},
+		[9] = {price = 15000000000}
 	}
 }
 
-for classificationId, classificationTable in ipairs(ItemTierClassifications) do
+for classificationId, classificationTable in ipairs(itemTierClassifications) do
 	local itemClassification = Game.createItemClassification(classificationId)
 	local classification = {}
 
+	-- Registers table for register_item_tier.lua interface
 	classification.Upgrades = {}
 	for tierId, tierTable in ipairs(classificationTable) do
 		if tierId then
 			classification.Upgrades.TierId = tierId
 		end
-		if tierTable.Price then
-			classification.Upgrades.Price = tierTable.Price
+		if tierTable.price then
+			classification.Upgrades.Price = tierTable.price
 		end
 	end
 	-- Create item classification and register classification table

b7e23d25a187d3cef8c7e57801c28ce66e59e419
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2022-01-25 19:20:28 -0300

    Enhancement of item tier classifications

---
+++ data/scripts/item_classification/item_tiers.lua
@@ -0,0 +1,50 @@
+ItemTierClassifications = {
+	-- Upgade classification 1
+	[1] = {
+		-- Update tier 0
+		[0] = {Price = 25000}
+	},
+	-- Upgade classification 2
+	[2] = {
+		-- Update tier 0
+		[0] = {Price = 750000},
+		-- Update tier 1
+		[1] = {Price = 5000000}
+	},
+	-- Upgade classification 3
+	[3] = {
+		[0] = {Price = 4000000},
+		[1] = {Price = 10000000},
+		[2] = {Price = 20000000}
+	},
+	-- Upgade classification 4
+	[4] = {
+		[0] = {Price = 8000000},
+		[1] = {Price = 20000000},
+		[2] = {Price = 40000000},
+		[3] = {Price = 65000000},
+		[4] = {Price = 100000000},
+		[5] = {Price = 250000000},
+		[6] = {Price = 750000000},
+		[7] = {Price = 2500000000},
+		[8] = {Price = 8000000000},
+		[9] = {Price = 15000000000}
+	}
+}
+
+for classificationId, classificationTable in ipairs(ItemTierClassifications) do
+	local itemClassification = Game.createItemClassification(classificationId)
+	local classification = {}
+
+	classification.Upgrades = {}
+	for tierId, tierTable in ipairs(classificationTable) do
+		if tierId then
+			classification.Upgrades.TierId = tierId
+		end
+		if tierTable.Price then
+			classification.Upgrades.Price = tierTable.Price
+		end
+	end
+	-- Create item classification and register classification table
+	itemClassification:register(classification)
+end

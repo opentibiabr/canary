693a1274ff32c9b3cdeb345dcffd4cb5807be5b5
Krzysztof Kołodziejczyk <kkolodziejczyk@gog.com>
2022-02-07 11:46:02 +0100

    fix dowload myaac script

--- docker/data/download-myaac.sh
+++ docker/data/download-myaac.sh
@@ -4,10 +4,10 @@ wget https://github.com/slawkens/myaac/archive/master.zip -O myaac.zip
 unzip -o myaac.zip -d .
 
 mv myaac-master/* ./web
-rm myaac.zip myaac-master -rf
+rm -rf myaac.zip myaac-master
 
-wget https://github.com/opentibiabr/myaac-tibia12-login/archive/refs/heads/develop.zip -O myaac-otbr-plugin.zip
-unzip -o myaac-otbr-plugin.zip -d .
+wget https://github.com/opentibiabr/myaac-tibia12-login/releases/download/2.0/myaac-tibia12-login-v2.0.zip -O myaac-otbr-plugin.zip
+unzip -o myaac-otbr-plugin.zip -d myaac-tibia12-login-master/
 
-cp -r myaac-tibia12-login-develop/* ./web
-rm -rf myaac-otbr-plugin.zip myaac-tibia12-login-develop
+cp -r myaac-tibia12-login-master/* ./web
+rm -rf myaac-otbr-plugin.zip myaac-tibia12-login-master

2c9f2cdaa83ef91148a7ffbb7e1557576cebe94d
omeranha <53233626+omeranha@users.noreply.github.com>
2022-02-19 00:16:43 -0300

    ohno

--- src/config/configmanager.cpp
+++ src/config/configmanager.cpp
@@ -176,7 +176,7 @@ bool ConfigManager::load()
 	boolean[WEATHER_RAIN] = getGlobalBoolean(L, "weatherRain", false);
 	boolean[WEATHER_THUNDER] = getGlobalBoolean(L, "thunderEffect", false);
 	boolean[ALL_CONSOLE_LOG] = getGlobalBoolean(L, "allConsoleLog", false);
-	boolean[TOGGLE_FREE_QUEST] = getGlobalBoolean(L, "freeQuests", false);
+	boolean[TOGGLE_FREE_QUEST] = getGlobalBoolean(L, "toggleFreeQuest", false);
 	boolean[AUTOLOOT] = getGlobalBoolean(L, "autoLoot", false);
 	boolean[STAMINA_TRAINER] = getGlobalBoolean(L, "staminaTrainer", false);
 	boolean[STAMINA_PZ] = getGlobalBoolean(L, "staminaPz", false);

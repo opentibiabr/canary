3f4056e44c07914a9cb6cb93932769c81de5b794
Marcos <66353315+marcosvf132@users.noreply.github.com>
2022-02-26 01:35:45 -0300

    Experience removal

--- src/creatures/players/player.cpp
+++ src/creatures/players/player.cpp
@@ -2273,6 +2273,7 @@ void Player::removeExperience(uint64_t exp, bool sendText/* = false*/)
 		levelPercent = 0;
 	}
 	sendStats();
+	sendExperienceTracker(0, -exp);
 }
 
 double_t Player::getPercentLevel(uint64_t count, uint64_t nextLevelCount)

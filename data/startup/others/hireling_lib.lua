6a217a7a0875e58bcdd5c7b3a296526bf7408eab
Marcos <66353315+marcosvf132@users.noreply.github.com>
2022-03-16 00:23:25 -0300

    Initial commit

--- data/startup/others/hireling_lib.lua
+++ data/startup/others/hireling_lib.lua
@@ -72,18 +72,18 @@ HIRELING_STORAGE = {
 	OUTFIT = 28900
 }
 
-HIRELING_LAMP_ID = 34070
+HIRELING_LAMP_ID = 29432
 HIRELING_ATTRIBUTE = "HIRELING_ID"
 
 HIRELING_FOODS_BOOST = {
-	MAGIC = 35174,
-	MELEE = 35175,
-	SHIELDING = 35172,
+	MAGIC = 29410,
+	MELEE = 29411,
+	SHIELDING = 29408,
 	DISTANCE = 35173,
 }
 
 HIRELING_FOODS = { -- only the non-skill ones
-	35176, 35177, 35178, 35179, 35180
+	29412, 29413, 29414, 29415, 29416
 }
 
 -- [[ LOCAL FUNCTIONS AND UTILS ]]
@@ -540,56 +540,43 @@ function Player:getHirelingChangingOutfit()
 	return getHirelingById(id)
 end
 
-local function addOutfit(msg, outfit)
+function Player:sendHirelingOutfitWindow(hireling)
+	local outfit = hireling:getOutfit()
+	local msg = NetworkMessage()
+	msg:addByte(0xC8) -- 'ProtocolGame::sendOutfitWindow()'' header
 	msg:addU16(outfit.lookType)
 
-	msg:addByte(outfit.lookHead)
-	msg:addByte(outfit.lookBody)
-	msg:addByte(outfit.lookLegs)
-	msg:addByte(outfit.lookFeet)
-	msg:addByte(outfit.lookAddons)
+	if outfit.lookType == 0 then
+		msg:addU16(outfit.lookTypeEx)
+	else
+		msg:addByte(outfit.lookHead)
+		msg:addByte(outfit.lookBody)
+		msg:addByte(outfit.lookLegs)
+		msg:addByte(outfit.lookFeet)
+		msg:addByte(outfit.lookAddons)
+	end
 	msg:addU16(outfit.lookMount)
-	msg:addByte(0)
-	msg:addByte(0)
-	msg:addByte(0)
-	msg:addByte(0)
-	msg:addU16(0)
-end
 
-function Player:sendHirelingOutfitWindow(hireling)
-	local msg = NetworkMessage()
-	local client = self:getClient()
-	msg:addByte(200) -- header
-	addOutfit(msg, hireling:getOutfit()) -- current outfit
+	msg:addByte(0x00) -- Mount head
+	msg:addByte(0x00) -- Mount body
+	msg:addByte(0x00) -- Mount legs
+	msg:addByte(0x00) -- Mount feet
+	msg:addU16(0x00) -- Familiar
 
 	local availableOutfits = hireling:getAvailableOutfits()
-
-	if client.version >= 1185 then
-		msg:addU16(#availableOutfits)
-	else
-		msg:addByte(#availableOutfits)
-	end
-
+	msg:addU16(#availableOutfits)
 	for _,outfit in ipairs(availableOutfits) do
 		msg:addU16(outfit.lookType)
 		msg:addString(outfit.name)
 		msg:addByte(0x00) -- addons
-
-		if client.version >= 1185 then
-			-- something related to the store button (offer_id maybe) not using now
-			msg:addByte(0x00)
-		end
+		msg:addByte(0x00) -- Store bool
 	end
 
-	-- mounts disabled for hirelings
-	if client.version >= 1185 then
-		msg:addU16(0x00) --mounts count
-		msg:addU16(0x00) --familiar count
-		msg:addByte(0x00) -- dunno
-		msg:addByte(0x00) -- dunno2
-	else
-		msg:addByte(0x00)
-	end
+	msg:addU16(0x00) -- Mounts count
+	msg:addU16(0x00) -- Familiar count
+	msg:addByte(0x00) -- Try outfit bool
+	msg:addByte(0x00) -- Is mounted bool
+	msg:addByte(0x00) -- Random outfit bool
 
 	msg:sendToPlayer(self)
 end

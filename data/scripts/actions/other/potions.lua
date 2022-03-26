f7c22155593b277be8e60446b30ed6bdbc3d6aaa
Marcos <66353315+marcosvf132@users.noreply.github.com>
2022-03-26 15:28:10 -0300

    Missing potions healing source

--- data/scripts/actions/other/potions.lua
+++ data/scripts/actions/other/potions.lua
@@ -174,11 +174,11 @@ function potions.onUse(player, item, fromPosition, target, toPosition, isHotkey)
 		return true
 	else
 		if potion.health then
-			doTargetCombatHealth(0, target, COMBAT_HEALING, potion.health[1], potion.health[2])
+			doTargetCombatHealth(player, target, COMBAT_HEALING, potion.health[1], potion.health[2])
 		end
 
 		if potion.mana then
-			doTargetCombatMana(0, target, COMBAT_MANADRAIN, potion.mana[1], potion.mana[2])
+			doTargetCombatMana(player, target, COMBAT_MANADRAIN, potion.mana[1], potion.mana[2])
 		end
 
 		if potion.antidote then

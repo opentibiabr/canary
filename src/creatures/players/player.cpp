cd0562c24af14203af3031a8ff351023ac86e5bb
Beats-Dh <daniel15042014@hotmail.com>
2022-03-26 20:24:22 -0400

    Fix green skull party

--- src/creatures/players/player.cpp
+++ src/creatures/players/player.cpp
@@ -4586,7 +4586,7 @@ Skulls_t Player::getSkullClient(const Creature* creature) const
 			return SKULL_YELLOW;
 		}
 
-		if (isPartner(player)) {
+		if (party && party == player->party) {
 			return SKULL_GREEN;
 		}
 	}

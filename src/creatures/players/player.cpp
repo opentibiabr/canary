fed9e3fcdbad9108b355bfca8978146ef22d8f48
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2022-04-03 15:54:17 -0300

    Somes improvements, thanks @lgrossi for suggestions
    Co-Authored-By: Lucas Grossi <34237492+lgrossi@users.noreply.github.com>

--- src/creatures/players/player.cpp
+++ src/creatures/players/player.cpp
@@ -508,7 +508,7 @@ void Player::updateInventoryImbuement(bool init /* = false */)
 			}
 
 			int32_t duration = std::max<int32_t>(0, imbuementInfo.duration - EVENT_IMBUEMENT_INTERVAL / 1000);
-			item->setImbuement(slotid, imbuementInfo.imbuement->getID(), duration);
+			item->addImbuement(slotid, imbuementInfo.imbuement->getID(), duration);
 			if (duration == 0) {
 				removeItemImbuementStats(imbuementInfo.imbuement);
 				g_game.decreasePlayerActiveImbuements(getID());
@@ -1317,7 +1317,7 @@ void Player::onClearImbuement(Item* item, uint8_t slot)
 		removeItemImbuementStats(imbuementInfo.imbuement);
 	}
 
-	item->setImbuement(slot, imbuementInfo.imbuement->getID(), 0);
+	item->clearImbuement(slot, imbuementInfo.imbuement->getID());
 	this->openImbuementWindow(item);
 }
 

99f3d9dbe6cf1e8828d901a98c335ddb5fb40f0e
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2022-04-22 07:34:30 -0300

    Fix load lua items only by load main map and somes others fixes
    Fixed message of error on map load wrong or missing file
    Improvement on worldboard script

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -669,7 +669,7 @@ bool Game::loadMainMap(const std::string& filename)
 {
 	Monster::despawnRange = g_configManager().getNumber(DEFAULT_DESPAWNRANGE);
 	Monster::despawnRadius = g_configManager().getNumber(DEFAULT_DESPAWNRADIUS);
-	return map.loadMap("data/world/" + filename + ".otbm", true, true, true);
+	return map.loadMap("data/world/" + filename + ".otbm", true, true, true, true);
 }
 
 bool Game::loadCustomMap(const std::string& filename)
@@ -681,7 +681,7 @@ bool Game::loadCustomMap(const std::string& filename)
 
 void Game::loadMap(const std::string& path)
 {
-	map.loadMap(path, false, false, false);
+	map.loadMap(path);
 }
 
 Cylinder* Game::internalGetCylinder(Player* player, const Position& pos) const

1ba9233ac8f165e6aaad2179724a784ce0056f5a
Renato Foot Guimarães Costallat <costallat@hotmail.com>
2022-05-19 17:27:34 -0300

    Update tests/build_and_run.sh
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/build_and_run.sh
+++ tests/build_and_run.sh
@@ -8,5 +8,5 @@ cd build
 cmake -DUT=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_TOOLCHAIN_FILE=/opt/workspace/vcpkg/scripts/buildsystems/vcpkg.cmake .. ; make -j`nproc`
 ./tests/canary_unittest  --reporter compact --success -d yes
 cd  ..
-cd tests
+cd tests || exit
 docker-compose down --rmi all -v --remove-orphans

6abd02f4a00b9999ff39f07ef0b8edffd0ccf80a
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2022-06-02 16:53:48 -0300

    Fix luajit windows

--- cmake/FindLuaJIT.cmake
+++ cmake/FindLuaJIT.cmake
@@ -1,38 +1,88 @@
-# - Try to find luajit
-# Once done this will define
-#  LUAJIT_FOUND - System has luajit
-#  LUAJIT_INCLUDE_DIRS - The luajit include directories
-#  LUAJIT_LIBRARIES - The libraries needed to use luajit
+# Once done these will be defined:
+#
+#  LUAJIT_FOUND
+#  LUAJIT_INCLUDE_DIRS
+#  LUAJIT_LIBRARIES
+#
+# For use in OBS: 
+#
+#  LUAJIT_INCLUDE_DIR
 
-find_package(PkgConfig)
-if (PKG_CONFIG_FOUND)
-  pkg_check_modules(PC_LUAJIT QUIET luajit)
-endif()
+IF(CMAKE_SIZEOF_VOID_P EQUAL 8)
+	SET(_LIB_SUFFIX 64)
+ELSE()
+	SET(_LIB_SUFFIX 32)
+ENDIF()
 
-set(LUAJIT_DEFINITIONS ${PC_LUAJIT_CFLAGS_OTHER})
+FIND_PATH(LUAJIT_INCLUDE_DIR
+	NAMES lua.h lualib.h
+	HINTS
+		ENV LuajitPath${_LIB_SUFFIX}
+		ENV LuajitPath
+		ENV DepsPath${_LIB_SUFFIX}
+		ENV DepsPath
+		${LuajitPath${_LIB_SUFFIX}}
+		${LuajitPath}
+		${DepsPath${_LIB_SUFFIX}}
+		${DepsPath}
+		${_LUAJIT_INCLUDE_DIRS}
+	PATHS
+		/usr/include
+		/usr/local/include
+		/opt/local/include
+		/opt/local
+		/sw/include
+		~/Library/Frameworks
+		/Library/Frameworks
+	PATH_SUFFIXES
+		include
+		luajit
+		luajit/src
+		include/luajit
+		include/luajit/src
+		luajit-2.0
+		include/luajit-2.0
+		luajit2.0
+		include/luajit2.0
+		luajit-2.1
+		include/luajit-2.1
+		luajit2.1
+		include/luajit2.1
+		)
 
-find_path(LUAJIT_INCLUDE_DIRS luajit.h
-          PATHS ${PC_LUAJIT_INCLUDEDIR} ${LUAJIT_INCLUDE_DIRS}
-          PATH_SUFFIXES luajit-2.0 luajit-2.1)
-
-if(MSVC)
-  list(APPEND LUAJIT_NAMES lua51)
-elseif(MINGW)
-  list(APPEND LUAJIT_NAMES libluajit libluajit-5.1)
-else()
-  list(APPEND LUAJIT_NAMES luajit-5.1)
-endif()
-
-find_library(LUAJIT_LIBRARIES NAMES ${LUAJIT_NAMES}
-             PATHS ${PC_LUAJIT_LIBDIR} ${PC_LUAJIT_LIBRARIES_DIRS})
-
-set(LUAJIT_LIBRARIES ${LUAJIT_LIBRARIES})
-set(LUAJIT_INCLUDE_DIRS ${LUAJIT_INCLUDE_DIRS})
+find_library(LUAJIT_LIB
+	NAMES ${_LUAJIT_LIBRARIES} luajit luajit-51 luajit-5.1 lua51
+	HINTS
+		ENV LuajitPath${_lib_suffix}
+		ENV LuajitPath
+		ENV DepsPath${_lib_suffix}
+		ENV DepsPath
+		${LuajitPath${_lib_suffix}}
+		${LuajitPath}
+		${DepsPath${_lib_suffix}}
+		${DepsPath}
+		${_LUAJIT_LIBRARY_DIRS}
+	PATHS
+		/usr/lib
+		/usr/local/lib
+		/opt/local/lib
+		/opt/local
+		/sw/lib
+		~/Library/Frameworks
+		/Library/Frameworks
+	PATH_SUFFIXES
+		lib${_lib_suffix} lib
+		libs${_lib_suffix} libs
+		bin${_lib_suffix} bin
+		../lib${_lib_suffix} ../lib
+		../libs${_lib_suffix} ../libs
+		../bin${_lib_suffix} ../bin)
 
 include(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set LUAJIT_FOUND to TRUE
-# if all listed variables are TRUE
-find_package_handle_standard_args(LuaJit DEFAULT_MSG
-                                  LUAJIT_LIBRARIES LUAJIT_INCLUDE_DIRS)
+find_package_handle_standard_args(Luajit DEFAULT_MSG LUAJIT_LIB LUAJIT_INCLUDE_DIR)
+mark_as_advanced(LUAJIT_INCLUDE_DIR LUAJIT_LIB)
 
-mark_as_advanced(LUAJIT_INCLUDE_DIRS LUAJIT_LIBRARIES)
+if(LUAJIT_FOUND)
+	set(LUAJIT_INCLUDE_DIRS ${LUAJIT_INCLUDE_DIR})
+	set(LUAJIT_LIBRARIES ${LUAJIT_LIB})
+endif()

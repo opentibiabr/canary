fe4da21ff8c3a2f5cd0d0a320d3e4a67b9517e5d
Beats <daniel15042014@hotmail.com>
2022-06-07 22:04:26 -0300

    Fix CreatureEvent revscripts unsafe code
    - Fixes possible CreatureEvent null dereference
    - Fixes possible CreatureEvent memory leaks
    
    Co-Authored-By: Jakub <38609240+SaiyansKing@users.noreply.github.com>

--- src/lua/functions/events/creature_event_functions.cpp
+++ src/lua/functions/events/creature_event_functions.cpp
@@ -82,14 +82,16 @@ int CreatureEventFunctions::luaCreatureEventType(lua_State* L) {
 }
 
 int CreatureEventFunctions::luaCreatureEventRegister(lua_State* L) {
-	// creatureevent:register()
-	CreatureEvent* creature = getUserdata<CreatureEvent>(L, 1);
-	if (creature) {
+	CreatureEvent** creaturePtr = getRawUserdata<CreatureEvent>(L, 1);
+	if (creaturePtr && *creaturePtr) {
+		CreatureEvent* creature = *creaturePtr;
 		if (!creature->isScripted()) {
 			pushBoolean(L, false);
-			return 1;
+			delete creature;
+		} else {
+			pushBoolean(L, g_creatureEvents().registerLuaEvent(creature));
 		}
-		pushBoolean(L, g_creatureEvents().registerLuaEvent(creature));
+		*creaturePtr = nullptr; // Remove luascript reference
 	} else {
 		lua_pushnil(L);
 	}

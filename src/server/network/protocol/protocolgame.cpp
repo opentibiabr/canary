5b25b43cbcf3dc835976892badb53576abed1565
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2022-06-30 01:53:16 -0300

    Fix some npcs bugs and change behaviour  (#396)
    Fixed:
    Total cost for custom currencys
    Bank npcs depot, transfer and withdraw logic
    
    Changed:
    MsgContains behaviour, added new function "MsgFind" so that we avoid some bugs in messages that have the same words
    Added debug log for assistant with debug build

--- src/server/network/protocol/protocolgame.cpp
+++ src/server/network/protocol/protocolgame.cpp
@@ -597,7 +597,7 @@ void ProtocolGame::parsePacket(NetworkMessage& msg)
 		return;
 	}
 
-	//a dead player can not performs actions
+	// A dead player can not performs actions
 	if (player->isDead() || player->getHealth() <= 0) {
 		if (recvbyte == 0x14) {
 			disconnect();
@@ -641,7 +641,7 @@ void ProtocolGame::parsePacket(NetworkMessage& msg)
 	}
 
 	// Modules system
-	if(player && recvbyte != 0xD3){
+	if (player && recvbyte != 0xD3) {
 		g_dispatcher().addTask(createTask(std::bind(&Modules::executeOnRecvbyte, &g_modules(), player->getID(), msg, recvbyte)));
 	}
 

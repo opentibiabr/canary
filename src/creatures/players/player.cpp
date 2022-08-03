7ff063e6378edf248b6df9f853692237b934fbb4
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2022-08-03 00:23:45 -0300

    Fix imbuement not decaying time after second imbuement on login

--- src/creatures/players/player.cpp
+++ src/creatures/players/player.cpp
@@ -1322,6 +1322,7 @@ void Player::onApplyImbuement(Imbuement *imbuement, Item *item, uint8_t slot, bo
 	if (item->getParent() == this) {
 		addItemImbuementStats(imbuement);
 	}
+	updateInventoryImbuement(true);
 	item->addImbuement(slot, imbuement->getID(), baseImbuement->duration);
 	openImbuementWindow(item);
 }

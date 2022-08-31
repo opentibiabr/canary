6e82559e67f3bf9bfbe7743fc307317c953e8a39
Beats <daniel15042014@hotmail.com>
2022-08-30 21:51:07 -0300

    Update download-myaac.sh

--- docker/data/download-myaac.sh
+++ docker/data/download-myaac.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-wget https://github.com/slawkens/myaac/archive/master.zip -O myaac.zip
+wget https://github.com/opentibiabr/myaac/archive/refs/heads/main.zip -O myaac.zip
 unzip -o myaac.zip -d .
 
 mv myaac-master/* ./web
 rm myaac.zip myaac-master -rf
 
-wget https://github.com/opentibiabr/myaac-tibia12-login/archive/refs/heads/develop.zip -O myaac-otbr-plugin.zip
-unzip -o myaac-otbr-plugin.zip -d .
+wget https://github.com/opentibiabr/myaac-tibia12-login/archive/refs/heads/main.zip -O myaac-otbr-plugin.zip
+unzip -o myaac-otbr-plugin.zip -d myaac-tibia12-login-main/
 
-cp -r myaac-tibia12-login-develop/* ./web
-rm -rf myaac-otbr-plugin.zip myaac-tibia12-login-develop
+cp -r myaac-tibia12-login-main/* ./web
+rm -rf myaac-otbr-plugin.zip myaac-tibia12-login-main

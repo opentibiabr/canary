71d8757276ee6d6bc21a6a9bc3893c77a14e2447
Gustavo Bosmak <gustavo.bosmak@gmail.com>
2022-09-06 22:00:09 -0300

    run formatter on updated base

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,11 +1,11 @@
 // #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #define CATCH_CONFIG_RUNNER
-#include <catch2/catch.hpp>
 #include "src/otpch.h"
+#include <catch2/catch.hpp>
 
 int main(int argc, char* argv[]) {
 
-  int result = Catch::Session().run(argc, argv);
+	int result = Catch::Session().run(argc, argv);
 
-  return result;
+	return result;
 }

1b1f57c5cb4bc9f8cb7391e382869a132f71b2c9
Pedro-Pessoa <pedro_santos_40@hotmail.com>
2022-10-11 17:08:07 -0300

    add note to the reload.lua script

--- data/scripts/talkactions/god/reload.lua
+++ data/scripts/talkactions/god/reload.lua
@@ -1,3 +1,10 @@
+-- NOTE: Using this script might cause unwanted changes.
+-- This script forces a reload in the entire server, this means
+-- that everything that is stored in memory might stop to work
+-- properly and/or completely.
+--
+-- This script should be used in test environments only.
+
 local reloadTypes = {
 	["all"] = RELOAD_TYPE_ALL,
 

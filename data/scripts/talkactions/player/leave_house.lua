7fbf8b8c94924d68eac6f73d0f06c011b4e4a0de
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2022-10-26 10:43:16 -0300

    Fix leave house remove hireling

--- data/scripts/talkactions/player/leave_house.lua
+++ data/scripts/talkactions/player/leave_house.lua
@@ -16,6 +16,19 @@ function leaveHouse.onSay(player, words, param)
 		return false
 	end
 
+	local tiles = house:getTiles()
+	if tiles then
+		for i, tile in pairs(tiles) do
+			if tile then
+				local position = Position(tile:getPosition())
+				local hireling = getHirelingByPosition(position)
+				if (hireling) then
+					hireling:returnToLamp(player:getGuid())
+				end
+			end
+		end
+	end
+
 	house:setOwnerGuid(0)
 	player:sendTextMessage(MESSAGE_LOOK, "You have successfully left your house.")
 	position:sendMagicEffect(CONST_ME_POFF)

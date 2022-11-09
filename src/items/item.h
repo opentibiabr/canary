e2073f2bfcdc11a7ccd7b536252ae4ad6541b07a
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2022-11-08 23:54:39 -0300

    Add new function

--- src/items/item.h
+++ src/items/item.h
@@ -1017,6 +1017,10 @@ class Item : virtual public Thing
 
 		virtual void startDecaying();
 		virtual void stopDecaying();
+		
+		bool getLoadedFromMap() {
+			return loadedFromMap;
+		}
 
 		void setLoadedFromMap(bool value) {
 			loadedFromMap = value;

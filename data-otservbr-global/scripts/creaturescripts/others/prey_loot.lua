ad0e65182e889ca6d947c03c383b56cc28135c36
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2022-11-25 06:33:00 -0300

    Correct prey loot logic

---
+++ data-otservbr-global/scripts/creaturescripts/others/prey_loot.lua
@@ -0,0 +1,52 @@
+local bonusCountPercent = 3
+
+local preyLootBonusKill = CreatureEvent("PreyLootBonusKill")
+
+function preyLootBonusKill.onKill(player, target, lastHit)
+	-- Monster verify
+	if not target or not target:isMonster() or target:getMaster() then
+		return true
+	end
+
+	local mType = target:getType()
+	
+	local preyLootPercent = player:getPreyLootPercentage(mType:raceId())
+	if preyLootPercent > 0 and probability < preyLootPercent then
+		target:registerEvent('BonusPreyLootDeath')
+	end
+	return true
+end
+
+preyLootBonusKill:register()
+
+local bonusPreyLootDeath = CreatureEvent("BonusPreyLootDeath")
+function bonusPreyLootDeath.onDeath(creature, corpse, killer, mostDamageKiller, unjustified, mostDamageUnjustified)
+	if not creature then
+		return true
+	end
+	local monsterLoot = creature:getType():getLoot()
+  local mType = creature:getType()
+	if not mType:getLoot() or not corpse then
+		return true
+	end
+  
+  local preyChanceBoost = 100
+	local charmBonus = false
+	if player and mType and mType:raceId() > 0 then
+		preyChanceBoost = player:getPreyLootPercentage(mType:raceId())
+		local charm = player:getCharmMonsterType(CHARM_GUT)
+		if charm and charm:raceId() == mType:raceId() then
+			charmBonus = true
+		end
+	end
+
+	for i, loot in pairs(monsterLoot) do
+		local item = corpse:createLootItem(monsterLoot[i], charmBonus, preyChanceBoost)
+		if not item then
+			Spdlog.warn(string.format("[Monster:onDropLoot] - Could not add loot item to monster: %s, from corpse id: %d.", self:getName(), corpse:getId()))
+		end
+	end
+	return true
+end
+
+bonusPreyLootDeath:register()

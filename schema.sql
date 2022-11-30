e8e228b4916b6396f770dcad66d5b9f5853355b4
murilo09 <78226931+murilo09@users.noreply.github.com>
2022-11-30 10:17:03 -0300

    Add forge_history table

--- schema.sql
+++ schema.sql
@@ -243,6 +243,22 @@ CREATE TABLE IF NOT EXISTS `daily_reward_history` (
         ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- Tabble Structure `forge_history`
+CREATE TABLE `forge_history` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `player_id` int NOT NULL,
+    `action_type` int NOT NULL DEFAULT '0',
+    `description` text NOT NULL,
+    `is_success` tinyint NOT NULL DEFAULT '0',
+    `bonus` tinyint NOT NULL DEFAULT '0',
+    `done_at` bigint NOT NULL,
+    `done_at_date` datetime DEFAULT NOW(),
+    `cost` bigint UNSIGNED NOT NULL DEFAULT '0',
+    `gained` bigint UNSIGNED NOT NULL DEFAULT '0',
+    CONSTRAINT `forge_history_pk` PRIMARY KEY (`id`),
+    FOREIGN KEY (`player_id`) REFERENCES `players` (`id`) ON DELETE CASCADE
+) ENGINE='InnoDB' DEFAULT CHARSET=utf8;
+
 
 -- Table structure `global_storage`
 CREATE TABLE IF NOT EXISTS `global_storage` (

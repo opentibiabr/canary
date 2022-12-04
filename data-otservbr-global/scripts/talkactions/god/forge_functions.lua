5b3bdb363d3e2d72e38c0ae9b30b1a74a35ca09e
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2022-12-04 14:18:44 -0300

    Add forge script in talkactions

--- data-otservbr-global/scripts/talkactions/god/forge_functions.lua
+++ data-otservbr-global/scripts/talkactions/god/forge_functions.lua
@@ -198,3 +198,18 @@ end
 
 setDusts:separator(" ")
 setDusts:register()
+
+-- Goto fiendish monster
+local forge = TalkAction("/fiendish")
+
+function forge.onSay(player, words, param)
+	if not player:getGroup():getAccess() or player:getAccountType() < ACCOUNT_TYPE_GOD then
+		return true
+	end
+
+	local monster = Monster(ForgeMonster:pickFiendish())
+	player:teleportTo(monster:getPosition())
+	return false
+end
+
+forge:register()

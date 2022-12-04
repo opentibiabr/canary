e5e654df52226d01076eca90f8b1c5736d11ff5d
omeranha <53233626+omeranha@users.noreply.github.com>
2022-12-04 01:17:19 -0300

    Update src/items/containers/container.cpp
    Co-authored-by: Eduardo Dantas <eduardo.dantas@hotmail.com.br>

--- src/items/containers/container.cpp
+++ src/items/containers/container.cpp
@@ -402,7 +402,15 @@ ReturnValue Container::queryAdd(int32_t addIndex, const Thing& addThing, uint32_
 	}
 	
 	if (isQuiver() && item->getWeaponType() != WEAPON_AMMO) {
-		Player* player = actor->getPlayer();
+		Player* player = actor ? actor->getPlayer() : nullptr;
+		if (player) {
+			Item* slotItem = player->getInventoryItem(CONST_SLOT_RIGHT);
+			if (slotItem && slotItem->getID() == getID() && item->getWeaponType() == WEAPON_SHIELD) {
+				g_game().playerEquipItem(player->getID(), item->getID());
+			} else {
+				return RETURNVALUE_ONLYAMMOINQUIVER;
+			}
+		}
 		Item* slotItem = player->getInventoryItem(CONST_SLOT_RIGHT);
 		if (slotItem && slotItem->getID() == getID() && item->getWeaponType() == WEAPON_SHIELD) {
 			g_game().playerEquipItem(player->getID(), item->getID());

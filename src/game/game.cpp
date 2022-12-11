f804cccf53c9dd4593ee43cc2079ab741f0346c2
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2022-12-11 16:43:47 -0300

    Fix indent

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -880,7 +880,7 @@ void Game::playerMoveThing(uint32_t playerId, const Position& fromPos,
 	// Prevent the player from being able to move the item within the imbuement window
 	if (player->hasImbuingItem()) {
         return;
-    }
+	}
 
 	uint8_t fromIndex = 0;
 	if (fromPos.x == 0xFFFF) {

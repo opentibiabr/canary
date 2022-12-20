6b6f0b2d97352416b7c5a0cbc70ae0d0dc49cde1
Karin <pessoacarlos98@gmail.com>
2022-12-20 16:51:01 -0300

    [Bug] added nullpointer check to prevent crash (#709)
    Check if the container is nullptr before accessing its methods

--- src/creatures/players/player.cpp
+++ src/creatures/players/player.cpp
@@ -3849,6 +3849,10 @@ void Player::postAddNotification(Thing* thing, const Cylinder* oldParent, int32_
 
 			for (const auto& it : openContainers) {
 				Container* container = it.second.container;
+				if (container == nullptr) {
+					continue;
+				}
+
 				if (!Position::areInRange<1, 1, 0>(container->getPosition(), getPosition())) {
 					containers.push_back(container);
 				}

369f7de84cfea25e77021456d8a32a4fd5386667
Beats <daniel15042014@hotmail.com>
2022-12-28 10:44:29 -0400

    Update tasks.cpp

--- src/game/scheduling/tasks.cpp
+++ src/game/scheduling/tasks.cpp
@@ -12,72 +12,96 @@
 #include "game/game.h"
 #include "game/scheduling/tasks.h"
 
-void Dispatcher::threadMain()
-{
-	io_service.run();
-	g_database.disconnect();
+void Dispatcher::threadMain() {
+    io_service.run();
+    g_database.disconnect();
 }
 
-void Dispatcher::addTask(std::function<void (void)> functor)
-{
-	asio::post(io_service, [this, functor]() {
-		++dispatcherCycle;
+void Dispatcher::addTask(std::function<void (void)> functor) try {
+	// Adds a task to the Dispatcher to be executed asynchronously.
+	// functor The functor representing the task to be added.
 
-		// execute it
-		(functor)();
-	});
+	// Increments the Dispatcher's cycle counter
+	++dispatcherCycle;
+
+	// Adds the task to the io_service object to be executed asynchronously
+	asio::post(io_service,
+		[this, f = std::move(functor)]() mutable {
+			// Executes the functor
+			(f)();
+		});
+} catch (const std::exception& ex) {
+	// Handles the exception thrown by the functor or the io_service object
+	SPDLOG_ERROR("Error adding task to Dispatcher: {}: {}", __FUNCTION__, ex.what());
 }
 
-uint64_t Dispatcher::addEvent(uint32_t delay, std::function<void (void)> functor)
-{
+uint64_t Dispatcher::addEvent(uint32_t delay, std::function<void (void)> functor) try {
+	// Adds an event to the Dispatcher to be executed asynchronously after a specified delay.
+	// delay The delay in milliseconds before the event should be executed.
+	// functor The functor representing the event to be added.
+	// The ID of the added event. Returns 0 if the Dispatcher is in the TERMINATED state.
+
 	if (getState() == THREAD_STATE_TERMINATED) {
 		return 0;
 	}
 
 	uint64_t eventId = ++lastEventId;
-	auto res = eventIds.emplace(std::piecewise_construct, std::forward_as_tuple(eventId), std::forward_as_tuple(io_service));
+	auto [iter, success] = eventIds.emplace(std::piecewise_construct, std::forward_as_tuple(eventId), std::forward_as_tuple(io_service));
 
-	asio::high_resolution_timer& timer = res.first->second;
-	timer.expires_from_now(std::chrono::milliseconds(delay));
-	#ifdef __cpp_generic_lambdas
-	timer.async_wait([this, eventId, f = std::move(functor)](const std::error_code& error) {
-	#else
-	timer.async_wait([this, eventId, functor](const const std::error_code& error) {
-	#endif
-		eventIds.erase(eventId);
+	if (success) {
+		asio::high_resolution_timer& timer = iter->second;
+		timer.expires_from_now(std::chrono::milliseconds(delay));
 
-		if (error == asio::error::operation_aborted || getState() == THREAD_STATE_TERMINATED) {
-			return;
-		}
+		timer.async_wait([this, eventId, f = std::move(functor)](const std::error_code& error) {
+			eventIds.erase(eventId);
 
-		// execute it
-		++dispatcherCycle;
-		#ifdef __cpp_generic_lambdas
-		(f)();
-		#else
-		(functor)();
-		#endif
-	});
+			if (error == asio::error::operation_aborted || getState() == THREAD_STATE_TERMINATED) {
+				return;
+			}
 
+			// Execute the functor
+			++dispatcherCycle;
+			(f)();
+		});
+	}
 	return eventId;
+} catch (const std::exception& ex) {
+	SPDLOG_ERROR("Error adding event to Dispatcher:{}: {}", __FUNCTION__, ex.what());
+    return 0;
 }
 
-void Dispatcher::stopEvent(uint64_t eventId)
-{
-	auto it = eventIds.find(eventId);
-	if (it != eventIds.end()) {
+void Dispatcher::stopEvent(uint64_t eventId) {
+	// Stops an event with the specified ID from being executed.
+	// Stops an event with the specified ID from being executed.
+
+	// Find the event in the eventIds map
+	if (auto it = eventIds.find(eventId); it != eventIds.end()) {
+		// Cancel the event
 		it->second.cancel();
 	}
 }
 
-void Dispatcher::shutdown()
-{
-	setState(THREAD_STATE_TERMINATED);
-	asio::post(io_service, [this]() {
-		for (auto& it : eventIds) {
-			it.second.cancel();
-		}
+void Dispatcher::shutdown() {
+	// Shut down the Dispatcher and cancel all pending events.
+ 	//std::system_error If an error occurs while canceling the events or resetting the work object.
 
-		work.reset();
-	});
+    // Set the state to terminated and cancel all pending events
+    setState(THREAD_STATE_TERMINATED);
+    asio::post(io_service, [this]() {
+        // Cancel all pending events
+        for (auto& [eventId, timer] : eventIds) {
+            try {
+                timer.cancel();
+            } catch (const std::system_error& ex) {
+                SPDLOG_ERROR("Error canceling event: {}: {}", __FUNCTION__, ex.what());
+            }
+        }
+
+        // Reset the work object
+        try {
+            work.reset();
+        } catch (const std::system_error& ex) {
+            SPDLOG_ERROR("Error resetting work object: {}: {}", __FUNCTION__, ex.what());
+        }
+    });
 }

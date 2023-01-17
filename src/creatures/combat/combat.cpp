8001dd2a430a1379a0288869332f90a056d5d4d5
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2023-01-17 18:52:10 -0300

    Fix nullpointer

--- src/creatures/combat/combat.cpp
+++ src/creatures/combat/combat.cpp
@@ -632,7 +632,7 @@ void Combat::CombatConditionFunc(Creature* caster, Creature* target, const Comba
 			}
 		}
 
-		if (caster == target || !target->isImmune(condition->getType())) {
+		if (caster == target || target && !target->isImmune(condition->getType())) {
 			Condition* conditionCopy = condition->clone();
 			if (caster) {
 				conditionCopy->setParam(CONDITION_PARAM_OWNER, caster->getID());

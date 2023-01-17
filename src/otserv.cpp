3b9de1ed60265021dc30ab4c3014c2c7c86f1d6c
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2023-01-17 20:02:09 -0300

    Added wait check for g_loaderSignal

--- src/otserv.cpp
+++ src/otserv.cpp
@@ -42,6 +42,7 @@
 std::mutex g_loaderLock;
 std::condition_variable g_loaderSignal;
 std::unique_lock<std::mutex> g_loaderUniqueLock(g_loaderLock);
+bool g_loaderDone = false;
 
 /**
  *It is preferable to keep the close button off as it closes the server without saving (this can cause the player to lose items from houses and others informations, since windows automatically closes the process in five seconds, when forcing the close)
@@ -217,7 +218,9 @@ int main(int argc, char* argv[]) {
 	g_dispatcher().addTask(createTask(std::bind(mainLoader, argc, argv,
 												&serviceManager)));
 
-	g_loaderSignal.wait(g_loaderUniqueLock);
+	g_loaderSignal.wait(g_loaderUniqueLock, [] {
+		return g_loaderDone;
+	});
 
 	if (serviceManager.is_running()) {
 		SPDLOG_INFO("{} {}", g_configManager().getString(SERVER_NAME),
@@ -382,6 +385,8 @@ void mainLoader(int, char*[], ServiceManager* services) {
 
 	std::string url = g_configManager().getString(DISCORD_WEBHOOK_URL);
 	webhook_send_message("Server is now online", "Server has successfully started.", WEBHOOK_COLOR_ONLINE, url);
+
+	g_loaderDone = true;
 
 	g_loaderSignal.notify_all();
 }

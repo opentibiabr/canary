202f089136a27603c74da956583407a63c371005
murilo09 <78226931+murilo09@users.noreply.github.com>
2023-01-24 23:06:27 -0300

    fix: assigning negative value to unsigned integer (getSkillLevel) (#769)
    Negative value was being assigned to an uint16_t, due to a wrong variable type in std::max.

--- src/creatures/players/player.h
+++ src/creatures/players/player.h
@@ -761,14 +761,14 @@ class Player final : public Creature, public Cylinder
 		}
 
 		uint16_t getSkillLevel(uint8_t skill) const {
-			uint16_t skillLevel = std::max<uint16_t>(0, skills[skill].level + varSkills[skill]);
+			auto skillLevel = std::max<int32_t>(0, skills[skill].level + varSkills[skill]);
 
-			auto it = maxValuePerSkill.find(skill);
-			if (it != maxValuePerSkill.end()) {
-				skillLevel = std::min<uint16_t>(it->second, skillLevel);
+			if (auto it = maxValuePerSkill.find(skill);
+				it != maxValuePerSkill.end()) {
+				skillLevel = std::min<int32_t>(it->second, skillLevel);
 			}
 
-			return skillLevel;
+			return static_cast<uint16_t>(skillLevel);
 		}
 		uint16_t getBaseSkill(uint8_t skill) const {
 			return skills[skill].level;

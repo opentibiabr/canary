119ceb6229e3ebe6e2b07229ba9e85307593f0ab
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2023-01-27 00:06:25 -0300

    Add logs

--- src/items/item.cpp
+++ src/items/item.cpp
@@ -727,14 +727,14 @@ Attr_ReadValue Item::readAttr(AttrTypes_t attr, PropStream& propStream)
 				// Unserialize key type and value
 				std::string key;
 				if (!propStream.readString(key)) {
-					SPDLOG_ERROR("[DEPRECATED] Failed to read key");
+					SPDLOG_ERROR("[DEPRECATED] Failed to read key for item {}, on position {}", getID(), getPosition());
 					return ATTR_READ_ERROR;
 				}
 
 				// Unserialize value type and value
 				CustomAttribute customAttribute;
 				if (!customAttribute.unserialize(propStream, __FUNCTION__)) {
-					SPDLOG_ERROR("[DEPRECATED] Failed to unserialize attributes");
+					SPDLOG_ERROR("[DEPRECATED] Failed to unserialize attributes for item {}, on position {}", getID(), getPosition());
 					return ATTR_READ_ERROR;
 				}
 

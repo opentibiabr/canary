5d82a57b04387dd36c83fbbc0d7d2df0512410ef
Bruno Vavretchek <66841959+brunovavretcheklourenco@users.noreply.github.com>
2023-01-30 14:42:46 -0300

    commitando srcs servidor atual

--- src/creatures/players/grouping/party.cpp
+++ src/creatures/players/grouping/party.cpp
@@ -151,7 +151,6 @@ bool Party::passPartyLeadership(Player* player)
 	memberList.insert(memberList.begin(), oldLeader);
 
 	updateSharedExperience();
-	updateTrackerAnalyzer();
 
 	for (Player* member : memberList) {
 		member->sendPartyCreatureShield(oldLeader);

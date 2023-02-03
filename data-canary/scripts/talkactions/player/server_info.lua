6103ed09bea39e1652712da701df8260aa56ca60
thunerm <82473579+thunerm@users.noreply.github.com>
2023-02-03 14:59:26 -0300

    fix data-canary

--- data-canary/scripts/talkactions/player/server_info.lua
+++ data-canary/scripts/talkactions/player/server_info.lua
@@ -2,12 +2,28 @@ local serverInfo = TalkAction("!serverinfo")
 
 function serverInfo.onSay(player, words, param)
 	local configRateSkill =  configManager.getNumber(configKeys.RATE_SKILL)
-	local text = "Server Info: \n\n"
+	local text = "Server Info Rates: \n"
 	.. "\nExp rate: x" .. configManager.getNumber(configKeys.RATE_EXPERIENCE)
-	.. "\nExp rate stages: x" .. configManager.getNumber(configKeys.RATE_USE_STAGES)
 	.. "\nSkill rate: x" .. configManager.getNumber(configKeys.RATE_SKILL)
 	.. "\nMagic rate: x" .. configManager.getNumber(configKeys.RATE_MAGIC)
 	.. "\nLoot rate: x" .. configManager.getNumber(configKeys.RATE_LOOT)
+	.. "\nSpawns rate: x" .. configManager.getNumber(configKeys.RATE_SPAWN)
+	text = text .. "\n"
+	text = text .. "\n"
+	text = text .. "Server Info Stages Rates: \n"
+	.. "\nExp rate stages: x" .. configManager.getNumber(configKeys.RATE_USE_STAGES)
+	.. "\nSword Skill Stages rate: x" .. getRateFromTable(skillsStages, player:getSkillLevel(SKILL_SWORD), configRateSkill)
+	.. "\nClub Skill Stages rate: x" .. getRateFromTable(skillsStages, player:getSkillLevel(SKILL_CLUB), configRateSkill)
+	.. "\nAxe Skill Stages rate: x" .. getRateFromTable(skillsStages, player:getSkillLevel(SKILL_AXE), configRateSkill)
+	.. "\nDistance Skill Stages rate: x" .. getRateFromTable(skillsStages, player:getSkillLevel(SKILL_DISTANCE), configRateSkill)
+	.. "\nShield Skill Stages rate: x" .. getRateFromTable(skillsStages, player:getSkillLevel(SKILL_SHIELD), configRateSkill)
+	.. "\nFist Skill Stages rate: x" .. getRateFromTable(skillsStages, player:getSkillLevel(SKILL_FIST), configRateSkill)
+	.. "\nMagic rate: x" .. getRateFromTable(magicLevelStages, player:getBaseMagicLevel(), configManager.getNumber(configKeys.RATE_MAGIC))
+	.. "\nLoot rate: x" .. configManager.getNumber(configKeys.RATE_LOOT)
+	.. "\nSpawns rate: x" .. configManager.getNumber(configKeys.RATE_SPAWN)
+	text = text .. "\n"
+	text = text .. "\n"
+	text = text .. "More Server Info: \n"
 	.. "\nLevel to buy house " .. configManager.getNumber(configKeys.HOUSE_BUY_LEVEL)
 	.. "\nProtection level: " .. configManager.getNumber(configKeys.PROTECTION_LEVEL)
 	.. "\nWorldType: " .. configManager.getString(configKeys.WORLD_TYPE)

49cb6bbc0fd95728adc256163130d11a9b801e44
Carlos Pessoa <pessoacarlos98@gmail.com>
2023-02-14 12:31:36 -0300

    aux

--- data/libs/functions/player.lua
+++ data/libs/functions/player.lua
@@ -451,3 +451,20 @@ function Player.getFinalBaseRateExperience(self)
 	end
 	return baseRate
 end
+
+function Player.setExhaustion(self, value, time)
+	self:setStorageValue(value, time + os.time())
+end
+
+function Player.getExhaustion(self, value)
+	local storage = self:getStorageValue(value)
+	if not storage or storage <= os.time() then
+		return 0
+	end
+
+	return storage - os.time()
+end
+
+function Player:hasExhaustion(value)
+	return self:getExhaustion(value) >= os.time() and true or false
+end

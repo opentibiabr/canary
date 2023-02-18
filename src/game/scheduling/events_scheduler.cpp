b5dbc8e3d2668b75f5bf215b46294701ab32c3c7
Beats <daniel15042014@hotmail.com>
2023-02-18 16:21:32 -0400

    up

--- src/game/scheduling/events_scheduler.cpp
+++ src/game/scheduling/events_scheduler.cpp
@@ -5,7 +5,7 @@
  * License: https://github.com/opentibiabr/canary/blob/main/LICENSE
  * Contributors: https://github.com/opentibiabr/canary/graphs/contributors
  * Website: https://docs.opentibiabr.org/
- */
+*/
 
 #include "pch.hpp"
 
@@ -14,7 +14,7 @@
 #include "lua/scripts/scripts.h"
 #include "utils/pugicast.h"
 
-bool EventsScheduler::loadScheduleEventFromXml() const
+bool EventsScheduler::loadScheduleEventFromXml()
 {
     pugi::xml_document doc;
     auto folder = g_configManager().getString(CORE_DIRECTORY) + "/XML/events.xml";
@@ -30,9 +30,11 @@ bool EventsScheduler::loadScheduleEventFromXml() const
     int daysMath = ((timePtr->tm_year + 1900) * 365) + ((timePtr->tm_mon + 1) * 30) + (timePtr->tm_mday);
 
     // Keep track of loaded scripts to check for duplicates
+    int count = 0;
     std::set<std::string> loadedScripts;
     for (const auto& eventNode : doc.child("events").children()) {
         std::string eventScript = eventNode.attribute("script").as_string();
+        std::string eventName = eventNode.attribute("name").as_string();
 
         int16_t startYear;
         int16_t startMonth;
@@ -49,13 +51,17 @@ bool EventsScheduler::loadScheduleEventFromXml() const
             continue;
         }
 
+        ++count;
+        if (count >= 2) {
+            SPDLOG_WARN("{} - More than one event scheduled for the same day.", __FUNCTION__);
+        }
+
         if (!eventScript.empty() && loadedScripts.count(eventScript) > 0) {
             SPDLOG_WARN("{} - Script declaration '{}' in duplicate 'data/XML/events.xml'.", __FUNCTION__, eventScript);
             continue;
         }
 
         loadedScripts.insert(eventScript);
-
         if (!eventScript.empty() && !g_scripts().loadEventSchedulerScripts(eventScript)) {
             SPDLOG_WARN("{} - Can not load the file '{}' on '/events/scripts/scheduler/'",
                         __FUNCTION__, eventScript);
@@ -79,20 +85,13 @@ bool EventsScheduler::loadScheduleEventFromXml() const
 				g_eventsScheduler().setSkillSchedule(static_cast<uint16_t>(ingameNode.attribute("skillrate").as_uint()));
 			}
 		}
+        eventScheduler.push_back(EventScheduler(eventName, startDays, endDays));
 	}
 
-    if(expSchedule == 0) {
-       g_eventsScheduler().setExpSchedule(100);
+    for (const auto& event : eventScheduler) {
+        if (daysMath >= event.startDays && daysMath <= event.endDays) {
+            SPDLOG_INFO("Active EventScheduler: {}", event.name);
+        }
     }
-    if(lootSchedule == 0) {
-      g_eventsScheduler().setLootSchedule(100);
-    }
-    if(spawnMonsterSchedule == 0) {
-       g_eventsScheduler().setSpawnMonsterSchedule(100);
-    }
-    if(skillSchedule == 0) {
-       g_eventsScheduler().setSkillSchedule(100);
-    }
-
 	return true;
 }

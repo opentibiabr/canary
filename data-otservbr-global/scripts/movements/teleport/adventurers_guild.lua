a5415d420acd03ef22f65d2ef590119cdb97243b
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2023-02-20 06:21:32 -0300

    fix to positions

--- data-otservbr-global/scripts/movements/teleport/adventurers_guild.lua
+++ data-otservbr-global/scripts/movements/teleport/adventurers_guild.lua
@@ -17,5 +17,6 @@ function adventurersGuild.onStepIn(creature, item, position, fromPosition)
 end
 
 adventurersGuild:type("stepin")
-adventurersGuild:aid(4255)
+adventurersGuild:position({x = 32209, y = 32292, z = 6})
+adventurersGuild:position({x = 32210, y = 32292, z = 6})
 adventurersGuild:register()

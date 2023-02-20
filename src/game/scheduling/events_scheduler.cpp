825a36353a783aa1ed880f8e77b8613cee0ac37a
Beats <61994374+beats-dh@users.noreply.github.com>
2023-02-20 02:07:04 -0400

    fix: duplicated active events warning and log (#768)
    Previously, having two active events for the same date would cause a warning on server load. This pull request fixes the issue and also adds a log for all active events on server load. However, be careful when defining two events with values other than 100, as it will double the final value.

--- src/game/scheduling/events_scheduler.cpp
+++ src/game/scheduling/events_scheduler.cpp
@@ -14,91 +14,80 @@
 #include "lua/scripts/scripts.h"
 #include "utils/pugicast.h"
 
-bool EventsScheduler::loadScheduleEventFromXml() const {
+bool EventsScheduler::loadScheduleEventFromXml() {
 	pugi::xml_document doc;
 	auto folder = g_configManager().getString(CORE_DIRECTORY) + "/XML/events.xml";
-	if (
-		// Init-statement
-		pugi::xml_parse_result result = doc.load_file(folder.c_str());
-		// Condition
-		!result
-	) {
-		printXMLError(__FUNCTION__, folder, result);
+	if (!doc.load_file(folder.c_str())) {
+		printXMLError(__FUNCTION__, folder, doc.load_file(folder.c_str()));
 		consoleHandlerExit();
 		return false;
 	}
 
-	int daysNow;
 	time_t t = time(nullptr);
 	const tm* timePtr = localtime(&t);
 	int daysMath = ((timePtr->tm_year + 1900) * 365) + ((timePtr->tm_mon + 1) * 30) + (timePtr->tm_mday);
 
-	for (auto schedNode : doc.child("events").children()) {
-		std::string ss_d;
-		std::stringstream ss;
+	// Keep track of loaded scripts to check for duplicates
+	int count = 0;
+	std::set<std::string_view, std::less<>> loadedScripts;
+	for (const auto &eventNode : doc.child("events").children()) {
+		std::string eventScript = eventNode.attribute("script").as_string();
+		std::string eventName = eventNode.attribute("name").as_string();
 
-		pugi::xml_attribute attr;
-		if ((attr = schedNode.attribute("name"))) {
-			ss_d = attr.as_string();
-			ss << ss_d << " event";
-		}
+		int16_t startYear;
+		int16_t startMonth;
+		int16_t startDay;
+		int16_t endYear;
+		int16_t endMonth;
+		int16_t endDay;
+		sscanf(eventNode.attribute("startdate").as_string(), "%hd/%hd/%hd", &startMonth, &startDay, &startYear);
+		sscanf(eventNode.attribute("enddate").as_string(), "%hd/%hd/%hd", &endMonth, &endDay, &endYear);
+		int startDays = ((startYear * 365) + (startMonth * 30) + startDay);
+		int endDays = ((endYear * 365) + (endMonth * 30) + endDay);
 
-		int16_t year;
-		int16_t day;
-		int16_t month;
-
-		if (!(attr = schedNode.attribute("enddate"))) {
+		if (daysMath < startDays || daysMath > endDays) {
 			continue;
-		} else {
-			ss_d = attr.as_string();
-			sscanf(ss_d.c_str(), "%hd/%hd/%hd", &month, &day, &year);
-			daysNow = ((year * 365) + (month * 30) + day);
-			if (daysMath > daysNow) {
-				continue;
-			}
 		}
 
-		if (!(attr = schedNode.attribute("startdate"))) {
+		++count;
+		if (count >= 2) {
+			SPDLOG_WARN("{} - More than one event scheduled for the same day.", __FUNCTION__);
+		}
+
+		if (!eventScript.empty() && loadedScripts.contains(eventScript)) {
+			SPDLOG_WARN("{} - Script declaration '{}' in duplicate 'data/XML/events.xml'.", __FUNCTION__, eventScript);
 			continue;
-		} else {
-			ss_d = attr.as_string();
-			sscanf(ss_d.c_str(), "%hd/%hd/%hd", &month, &day, &year);
-			daysNow = ((year * 365) + (month * 30) + day);
-			if (daysMath < daysNow) {
-				continue;
-			}
 		}
 
-		if ((attr = schedNode.attribute("script")) && (!(g_scripts().loadEventSchedulerScripts(attr.as_string())))) {
-			SPDLOG_WARN("{} - Can not load the file '{}' on '/events/scripts/scheduler/'", __FUNCTION__, attr.as_string());
+		loadedScripts.insert(eventScript);
+		if (!eventScript.empty() && !g_scripts().loadEventSchedulerScripts(eventScript)) {
+			SPDLOG_WARN("{} - Can not load the file '{}' on '/events/scripts/scheduler/'", __FUNCTION__, eventScript);
 			return false;
 		}
 
-		for (auto schedENode : schedNode.children()) {
-			if ((schedENode.attribute("exprate"))) {
-				uint16_t exprate = pugi::cast<uint16_t>(schedENode.attribute("exprate").value());
-				g_eventsScheduler().setExpSchedule(exprate);
-				ss << " exp: " << exprate << "%";
+		for (const auto &ingameNode : eventNode.children()) {
+			if (ingameNode.attribute("exprate")) {
+				g_eventsScheduler().setExpSchedule(static_cast<uint16_t>(ingameNode.attribute("exprate").as_uint()));
 			}
 
-			if ((schedENode.attribute("lootrate"))) {
-				uint16_t lootrate = pugi::cast<uint16_t>(schedENode.attribute("lootrate").value());
-				g_eventsScheduler().setLootSchedule(lootrate);
-				ss << ", loot: " << lootrate << "%";
+			if (ingameNode.attribute("lootrate")) {
+				g_eventsScheduler().setLootSchedule(ingameNode.attribute("lootrate").as_uint());
 			}
 
-			if ((schedENode.attribute("spawnrate"))) {
-				uint32_t spawnrate = pugi::cast<uint32_t>(schedENode.attribute("spawnrate").value());
-				g_eventsScheduler().setSpawnMonsterSchedule(spawnrate);
-				ss << ", spawn: " << spawnrate << "%";
+			if (ingameNode.attribute("spawnrate")) {
+				g_eventsScheduler().setSpawnMonsterSchedule(ingameNode.attribute("spawnrate").as_uint());
 			}
 
-			if ((schedENode.attribute("skillrate"))) {
-				uint16_t skillrate = pugi::cast<uint16_t>(schedENode.attribute("skillrate").value());
-				g_eventsScheduler().setSkillSchedule(skillrate);
-				ss << ", skill: " << skillrate << "%";
-				break;
+			if (ingameNode.attribute("skillrate")) {
+				g_eventsScheduler().setSkillSchedule(static_cast<uint16_t>(ingameNode.attribute("skillrate").as_uint()));
 			}
+		}
+		eventScheduler.push_back(EventScheduler(eventName, startDays, endDays));
+	}
+
+	for (const auto &event : eventScheduler) {
+		if (daysMath >= event.startDays && daysMath <= event.endDays) {
+			SPDLOG_INFO("Active EventScheduler: {}", event.name);
 		}
 	}
 	return true;

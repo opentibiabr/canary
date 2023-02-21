b3bbbbd8f297c1fe25ce263eabc4398f4b6b3400
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2023-02-21 00:40:40 -0300

    Indent

--- data-otservbr-global/scripts/movements/quests/children_of_the_revolution/teleport.lua
+++ data-otservbr-global/scripts/movements/quests/children_of_the_revolution/teleport.lua
@@ -6,22 +6,23 @@ local positions = {
 local teleport = MoveEvent()
 
 function teleport.onStepIn(creature, item, position, fromPosition)
-	if not creature:isPlayer() then
+	local player = creature:getPlayer()
+	if not creature or not player then
 		return true
 	end
-	if creature:getStorageValue(Storage.ChildrenoftheRevolution.teleportAccess) == 1 then
-		if creature:getPosition() == Position(positions[1]) then
-			creature:teleportTo(Position(33261, 31077, 8))
-			creature:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+	if player:getStorageValue(Storage.ChildrenoftheRevolution.teleportAccess) == 1 then
+		if player:getPosition() == Position(positions[1]) then
+			player:teleportTo(Position(33261, 31077, 8))
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 			return true
 		else
-			creature:teleportTo(Position(33356, 31126, 7))
-			creature:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+			player:teleportTo(Position(33356, 31126, 7))
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 			return true
 		end
 	end
-	creature:teleportTo(fromPosition)
-	creature:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+	player:teleportTo(fromPosition)
+	player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 	return true
 end
 

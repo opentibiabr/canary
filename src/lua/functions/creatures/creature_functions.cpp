ecc6830c0eac3d354e4e22cdc57472d0a8f2f815
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2023-03-02 10:34:46 -0300

    add again old position

--- src/lua/functions/creatures/creature_functions.cpp
+++ src/lua/functions/creatures/creature_functions.cpp
@@ -748,10 +748,11 @@ int CreatureFunctions::luaCreatureTeleportTo(lua_State* L) {
 		return 1;
 	}
 
+	const Position oldPosition = creature->getPosition();
 	if (auto ret = g_game().internalTeleport(creature, position, pushMovement);
 		ret != RETURNVALUE_NOERROR) {
 		player->sendCancelMessage(ret);
-		SPDLOG_ERROR("[{}] Failed to teleport creature {}, on position {}, error code: {}", __FUNCTION__, creature->getName(), creature->getPosition().toString(), getReturnMessage(ret));
+		SPDLOG_ERROR("[{}] Failed to teleport creature {}, on position {}, error code: {}", __FUNCTION__, creature->getName(), oldPosition.toString(), getReturnMessage(ret));
 		pushBoolean(L, false);
 		return 1;
 	}

93fc5054114e4184374a65eed5ed35674a969955
ElimarCosta <elimar.u.t.costa@gmail.com>
2023-03-06 17:28:52 -0300

    feat: missing factions (#886)
    Add missing factions
    There are a few factions in war in the world of Tibia
    Extended explanation: https://www.tibiaqa.com/14283/what-creatures-attack-other-creatures?show=23278#a23278

--- data-otservbr-global/monster/aquatics/deepling_elite.lua
+++ data-otservbr-global/monster/aquatics/deepling_elite.lua
@@ -24,7 +24,7 @@ monster.Bestiary = {
 	Stars = 3,
 	Occurrence = 1,
 	Locations = "Fiehonja (Tanjis lair)."
-	}
+}
 
 monster.health = 3200
 monster.maxHealth = 3200
@@ -32,6 +32,9 @@ monster.race = "blood"
 monster.corpse = 13713
 monster.speed = 165
 monster.manaCost = 0
+
+monster.faction = FACTION_DEEPLING
+monster.enemyFactions = {FACTION_DEATHLING, FACTION_PLAYER}
 
 monster.changeTarget = {
 	interval = 4000,

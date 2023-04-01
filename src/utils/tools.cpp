02a2b390cf4bd89bb23d943352f4b6acd0f6420b
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2023-03-31 20:08:36 -0700

    fix: more specific exception

--- src/utils/tools.cpp
+++ src/utils/tools.cpp
@@ -349,7 +349,7 @@ std::string convertIPToString(uint32_t ip) {
 std::string formatDate(time_t time) {
 	try {
 		return fmt::format("{:%d/%m/%Y %H:%M:%S}", fmt::localtime(time));
-	} catch (const std::exception &exception) {
+	} catch (const std::out_of_range &exception) {
 		SPDLOG_ERROR("Failed to format date with error code {}", exception.what());
 	}
 	return {};
@@ -358,7 +358,7 @@ std::string formatDate(time_t time) {
 std::string formatDateShort(time_t time) {
 	try {
 		return fmt::format("{:%Y-%m-%d %X}", fmt::localtime(time));
-	} catch (const std::exception &exception) {
+	} catch (const std::out_of_range &exception) {
 		SPDLOG_ERROR("Failed to format date short with error code {}", exception.what());
 	}
 	return {};

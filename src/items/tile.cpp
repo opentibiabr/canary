f71867df9cb6532f72630c20a5f29062b1833042
Eduardo Augusto <duuh-l@hotmail.com>
2023-04-10 14:40:27 -0300

    Fix: move container contains storeItem in tile

--- src/items/tile.cpp
+++ src/items/tile.cpp
@@ -645,7 +645,19 @@ ReturnValue Tile::queryAdd(int32_t, const Thing &thing, uint32_t, uint32_t tileF
 			return RETURNVALUE_NOTPOSSIBLE;
 		}
 
-		if (item->isStoreItem() && !dynamic_cast<const HouseTile*>(this)) {
+		auto itemContainer = item->getContainer();
+		bool hasStoreItemInContainer = false;
+		if (itemContainer) {
+			for(Item* containerItem : itemContainer->getItems(true)) {
+				if (containerItem->isStoreItem()) {
+					hasStoreItemInContainer = true;
+					break;
+				}
+				continue;
+			}
+		}
+
+		if ((item->isStoreItem() || hasStoreItemInContainer) && !dynamic_cast<const HouseTile*>(this)) {
 			return RETURNVALUE_NOTPOSSIBLE;
 		}
 

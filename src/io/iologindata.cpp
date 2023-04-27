17aa85f0a3c72627016c22f2db1b443b2a172257
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2023-04-27 04:48:16 -0300

    fix lastday

--- src/io/iologindata.cpp
+++ src/io/iologindata.cpp
@@ -1352,8 +1352,8 @@ void IOLoginData::removeVIPEntry(uint32_t accountId, uint32_t guid) {
 void IOLoginData::addPremiumDays(uint32_t accountId, int32_t addDays) {
 	std::ostringstream query;
 	query << "UPDATE `accounts` SET `premdays` = `premdays` + " << addDays
-		  << ", `lastday` = NOW()"
-		  << " WHERE `id` = " << accountId;
+      << ", `lastday` = " << getTimeNow() <<
+      << " WHERE `id` = " << accountId;
 
 	Database::getInstance().executeQuery(query.str());
 }

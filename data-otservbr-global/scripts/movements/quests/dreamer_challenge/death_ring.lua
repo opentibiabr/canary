84e4c6fe8a89368831fc9f5f4a7c22058e9aa4fe
Luan Santos <github@luan.sh>
2023-05-18 11:27:59 -0700

    fix trees

--- data-otservbr-global/scripts/movements/quests/dreamer_challenge/death_ring.lua
+++ data-otservbr-global/scripts/movements/quests/dreamer_challenge/death_ring.lua
@@ -11,16 +11,17 @@ function deathRing.onAddItem(moveitem, tileitem, position)
 		return true
 	end
 
+	local found = false
 	for i = 1, #trees do
 		local treeItem = Tile(trees[i]):getItemById(3636)
 		if treeItem then
 			treeItem:remove()
 			trees[i]:sendMagicEffect(CONST_ME_MAGIC_BLUE)
 			moveitem:remove()
-			return true
+			found = true
 		end
 	end
-	return false
+	return found
 end
 
 deathRing:type("additem")

01de8ef1e2a1057180badb9f5b122eb1f841351b
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2023-05-20 14:29:06 -0300

    fix: reorganize functions names, add doxygen and indent

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -8976,40 +8976,42 @@ bool Game::addItemStoreInbox(const Player* player, uint32_t itemId) {
 	return true;
 }
 
-void Game::addUniquePlayerNames(Player* player) {
+void Game::addPlayerUniqueLogin(Player* player) {
 	if (!player) {
-		SPDLOG_WARN("Attempted to add null player to unique player names list.");
+		SPDLOG_ERROR("Attempted to add null player to unique player names list");
 		return;
 	}
+
 	const std::string &lowercase_name = asLowerCaseString(player->getName());
-	UniquePlayerNames[lowercase_name] = player;
+	m_uniqueLoginPlayerNames[lowercase_name] = player;
 }
 
-Player* Game::getUniquePlayerNames(const std::string &s) {
-	if (s.empty()) {
-		SPDLOG_WARN("Attempted to get player with empty name string.");
+Player* Game::getPlayerUniqueLogin(const std::string &playerName) const {
+	if (playerName.empty()) {
+		SPDLOG_ERROR("Attempted to get player with empty name string");
 		return nullptr;
 	}
 
-	auto it = UniquePlayerNames.find(asLowerCaseString(s));
-	return (it != UniquePlayerNames.end()) ? it->second : nullptr;
+	auto it = m_uniqueLoginPlayerNames.find(asLowerCaseString(playerName));
+	return (it != m_uniqueLoginPlayerNames.end()) ? it->second : nullptr;
 }
 
-void Game::removeUniquePlayerNames(const std::string &s) {
-	if (s.empty()) {
-		SPDLOG_WARN("Attempted to remove player with empty name string from unique player names list.");
+void Game::removePlayerUniqueLogin(const std::string &playerName) {
+	if (playerName.empty()) {
+		SPDLOG_ERROR("Attempted to remove player with empty name string from unique player names list");
 		return;
 	}
 
-	const std::string &lowercase_name = asLowerCaseString(s);
-	UniquePlayerNames.erase(lowercase_name);
+	const std::string &lowercase_name = asLowerCaseString(playerName);
+	m_uniqueLoginPlayerNames.erase(lowercase_name);
 }
 
-void Game::removeUniquePlayerNames(Player* player) {
+void Game::removePlayerUniqueLogin(Player* player) {
 	if (!player) {
-		SPDLOG_WARN("Attempted to remove null player from unique player names list.");
+		SPDLOG_ERROR("Attempted to remove null player from unique player names list.");
 		return;
 	}
+
 	const std::string &lowercase_name = asLowerCaseString(player->getName());
-	UniquePlayerNames.erase(lowercase_name);
+	m_uniqueLoginPlayerNames.erase(lowercase_name);
 }

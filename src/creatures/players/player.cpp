11b51aa20b17fddc5184d810f17209b0aa6b69d4
Nestor Ortega (NvSo) <nvsoonline@gmail.com>
2023-05-20 17:47:36 -0500

    OTCv8 detection, custom features packet.

--- src/creatures/players/player.cpp
+++ src/creatures/players/player.cpp
@@ -1493,7 +1493,15 @@ void Player::onCreatureAppear(Creature* creature, bool isLogin) {
 		}
 
 		auto version = client->oldProtocol ? getProtocolVersion() : CLIENT_VERSION;
-		SPDLOG_INFO("{} has logged in. (Protocol: {})", name, version);
+		OperatingSystem_t os = getOperatingSystem();
+		// Show player OTclient info
+		if (os > CLIENTOS_NEW_MAC && os < CLIENTOS_OTCLIENTV8_LINUX) {
+			SPDLOG_INFO("{} has logged in. (Protocol: {}, Client: OTClient)", name, version);
+		} else if (os >= CLIENTOS_OTCLIENTV8_LINUX) {
+			SPDLOG_INFO("{} has logged in. (Protocol: {}, Client: OTClientV8)", name, version, os);
+		} else {
+			SPDLOG_INFO("{} has logged in. (Protocol: {})", name, version);
+		}
 
 		if (guild) {
 			guild->addMember(this);
--- src/server/network/protocol/protocolgame.h
+++ src/server/network/protocol/protocolgame.h
@@ -449,6 +449,9 @@ class ProtocolGame final : public Protocol {
 		// otclient
 		void parseExtendedOpcode(NetworkMessage &msg);
 
+		// OTCv8
+		void sendFeatures();
+
 		void parseInventoryImbuements(NetworkMessage &msg);
 		void sendInventoryImbuements(const std::map<Slots_t, Item*> items);
 
@@ -476,6 +479,8 @@ class ProtocolGame final : public Protocol {
 		bool shouldAddExivaRestrictions = false;
 
 		bool oldProtocol = false;
+
+		uint16_t otclientV8 = 0;
 
 		void sendInventory();
 		void sendOpenStash();

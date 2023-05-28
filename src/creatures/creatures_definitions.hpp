ae995009d996a2021a942edb605cf410d44429d2
Nestor Ortega (NvSo) <91865189+Corlyone@users.noreply.github.com>
2023-05-28 11:01:04 -0500

    Update creatures_definitions.hpp

--- src/creatures/creatures_definitions.hpp
+++ src/creatures/creatures_definitions.hpp
@@ -492,7 +492,7 @@ enum class ForgeClassifications_t : uint8_t {
 	FORGE_FIENDISH_MONSTER = 2,
 };
 
-enum GameFeature : uint8_t {
+enum class GameFeature : uint8_t {
 	GameProtocolChecksum = 1,
 	GameAccountNames = 2,
 	GameChallengeOnLogin = 3,

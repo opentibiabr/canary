8ba65ef69153d8e6ccc0620d1b469e4b1ce16fe5
Thunerm <82473579+thunerm@users.noreply.github.com>
2023-06-05 16:21:30 -0300

    fix

--- src/server/network/protocol/protocolgame.cpp
+++ src/server/network/protocol/protocolgame.cpp
@@ -555,7 +555,7 @@ void ProtocolGame::onRecvFirstMessage(NetworkMessage &msg) {
 	std::string password = sessionKey.substr(pos + 1);
 	std::string characterName = msg.getString();
 
-	Player* foundPlayer = g_game().getPlayerUniqueLogin(characterName);
+	Player* foundPlayer = const_cast<Player*>(g_game().getPlayerUniqueLogin(characterName));
 	if (foundPlayer != nullptr) {
 		foundPlayer->client->disconnectClient("You are already connected through another client. Please use only one client at a time!");
 	}

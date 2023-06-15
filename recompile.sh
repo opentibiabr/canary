39a91ff04735f64fa77401e3fe98321edca02893
Beats <61994374+beats-dh@users.noreply.github.com>
2023-06-15 09:17:53 -0300

    improve: refactor recompile script for improve build process (#1176)
    This introduces a refactored version of the recompile script. The script has been optimized and improved to enhance the build process for the project. Key changes include:
    • Cleaner and more organized code structure.
    • Added error handling and exit conditions to ensure a robust build.
    • Improved logging and information messages for better visibility during the build.
    • Proper configuration and setup of Canary with the ability to handle different architectures.

--- recompile.sh
+++ recompile.sh
@@ -1,36 +1,117 @@
 #!/bin/bash
 
-cd ~/vcpkg && export VCPKG_FORCE_SYSTEM_BINARIES=1 && cd ~/canary
+set -euo pipefail
 
-if [ -d "build" ]; then
-	echo "Directory 'build' already exists, moving to it"
-	cd build
-	export VCPKG_FORCE_SYSTEM_BINARIES=1
-	echo "Clean build directory"
-	rm -rf *
-	echo "Configuring"
-	# for root directory
-	cmake -DCMAKE_TOOLCHAIN_FILE=../../vcpkg/scripts/buildsystems/vcpkg.cmake .. --preset linux-release
-	# for home directory
-	# cmake -DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake .. --preset linux-release
-else
-	mkdir "build" && cd build
-	export VCPKG_FORCE_SYSTEM_BINARIES=1
-	# for root directory
-	cmake -DCMAKE_TOOLCHAIN_FILE=../../vcpkg/scripts/buildsystems/vcpkg.cmake .. --preset linux-release	
-	# for home directory
-	# cmake -DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake .. --preset linux-release
-fi
+# Variáveis
+VCPKG_PATH="$HOME/vcpkg/scripts/buildsystems/vcpkg.cmake"
+BUILD_TYPE="linux-release"
+ARCHITECTURE=$(uname -m)
+ARCHITECTUREVALUE=0
 
-cmake --build linux-release || exit 1
-if [ $? -eq 1 ]; then
-	echo "Compilation failed!"
-else
-	echo "Compilation successful!"
-	cd ~/canary
-	if [ -f "canary" ]; then
-		echo "Saving old build"
-		mv ./canary ./canary.old
+# Function to print information messages
+info() {
+	echo -e "\033[1;34m[INFO]\033[0m $1"
+}
+
+# Function to check if a command is available
+check_command() {
+	if ! command -v "$1" >/dev/null; then
+		echo "The command '$1' is not available. Please install it and try again."
+		exit 1
 	fi
-	cp ./build/linux-release/bin/canary ./canary
-fi
+}
+
+check_architecture() {
+	if [[ $ARCHITECTURE == "aarch64"* ]]; then
+		info "its architecture is ARM"
+		ARCHITECTUREVALUE=1
+	else
+		info "its architecture is ARM $ARCHITECTURE"
+	fi
+}
+
+# Function to configure Canary
+setup_canary() {
+	if [ -d "build" ]; then
+		cd build
+	else
+		mkdir -p build && cd build
+		info "Canary has already been configured, skipping this step..."
+	fi
+}
+
+# Function to build Canary
+build_canary() {
+	info "Configuring Canary..."
+	if [[ $ARCHITECTUREVALUE == 1 ]]; then
+		export VCPKG_FORCE_SYSTEM_BINARIES=1
+	fi
+	cmake -DCMAKE_TOOLCHAIN_FILE="$VCPKG_PATH" .. --preset "$BUILD_TYPE" >cmake_log.txt 2>&1 || {
+		cat cmake_log.txt
+		return 1
+	}
+
+	info "Starting the build process..."
+
+	local total_steps=0
+	local progress=0
+	local build_status=0
+
+	global_beats=0
+	local temp_file="temp_global_beats.txt"
+	echo "0" >$temp_file
+
+	cmake --build "$BUILD_TYPE" 2>&1 > >(while IFS= read -r line; do
+		echo "$line" >>build_log.txt
+		if [[ $line =~ ^\[([0-9]+)/([0-9]+)\].* ]]; then
+			current_step=${BASH_REMATCH[1]}
+			total_steps=${BASH_REMATCH[2]}
+			progress=$((current_step * 100 / total_steps))
+			printf "\r\033[1;32m[INFO]\033[0m Progress build: [%3d%%]" $progress
+			echo "1" >$temp_file
+		fi
+	done) || build_status=1
+
+	global_beats=$(cat $temp_file)
+	rm $temp_file
+
+	if [[ $build_status -eq 0 ]]; then
+		if [[ $global_beats == 1 ]]; then
+			echo
+		fi
+		return 0
+	else
+		echo
+		cat build_log.txt
+		return 1
+	fi
+}
+
+# Function to move the generated executable
+move_executable() {
+	local executable_name="canary"
+	cd ..
+	if [ -e "$executable_name" ]; then
+		info "Saving old build"
+		mv ./"$executable_name" ./"$executable_name".old
+	fi
+	info "Moving the generated executable to the canary folder directory..."
+	cp ./build/linux-release/bin/"$executable_name" ./"$executable_name"
+	info "Build completed successfully!"
+}
+
+# Main function
+main() {
+	check_command "cmake"
+	check_architecture
+	setup_canary
+
+	if build_canary; then
+		move_executable
+	else
+		echo -e "\033[31m[ERROR]\033[0m Build failed..."
+		exit 1
+	fi
+}
+
+main

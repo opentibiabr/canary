29041482ac6969db2123ecf277105d43c5034250
Luan Santos <git@luan.sh>
2023-07-04 16:59:00 -0700

    fix accidental magma bubble immunity change

--- data-otservbr-global/monster/quests/primal_ordeal_quest/magma_bubble.lua
+++ data-otservbr-global/monster/quests/primal_ordeal_quest/magma_bubble.lua
@@ -113,7 +113,7 @@ monster.defenses = {
 monster.elements = {
 	{type = COMBAT_PHYSICALDAMAGE, percent = 0},
 	{type = COMBAT_ENERGYDAMAGE, percent = 0},
-	{type = COMBAT_EARTHDAMAGE, percent = 30},
+	{type = COMBAT_EARTHDAMAGE, percent = 100},
 	{type = COMBAT_FIREDAMAGE, percent = 0},
 	{type = COMBAT_LIFEDRAIN, percent = 0},
 	{type = COMBAT_MANADRAIN, percent = 0},

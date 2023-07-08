fc1018d15cb7a2f2d20c4049166631fd66bb3f60
Luan Santos <git@luan.sh>
2023-07-07 21:48:54 -0700

    fix

--- data-otservbr-global/migrations/31.lua
+++ data-otservbr-global/migrations/31.lua
@@ -1,14 +1,15 @@
 function onUpdateDatabase()
 	Spdlog.info("Updating database to version 32 (account_sessions)")
 	db.query([[
-			CREATE TABLE IF NOT EXISTS `account_sessions` (
-				`id` VARCHAR(191) NOT NULL,
-				`account_id` INTEGER UNSIGNED NOT NULL,
-				`expires` BIGINT UNSIGNED NOT NULL,
+		CREATE TABLE IF NOT EXISTS `account_sessions` (
+			`id` VARCHAR(191) NOT NULL,
+			`account_id` INTEGER UNSIGNED NOT NULL,
+			`expires` BIGINT UNSIGNED NOT NULL,
 
-				PRIMARY KEY (`id`)
-		) ENGINE=MEMORY DEFAULT CHARSET=utf8;
+			PRIMARY KEY (`id`)
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 	]])
+	-- Switch to TEXT to allow longer passwords (such as bcrypt encrypted)
 	db.query([[
 		ALTER TABLE `accounts` MODIFY `password` TEXT NOT NULL;
 	]])

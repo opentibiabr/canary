73486b9ea2b9e051bbd0b3b5beb4d9c953adbc3f
Luan Santos <git@luan.sh>
2023-07-08 18:58:29 -0700

    feat: ingol doors

--- data-otservbr-global/lib/core/storages.lua
+++ data-otservbr-global/lib/core/storages.lua
@@ -2862,6 +2862,17 @@ Storage = {
 				},
 			},
 		},
+		U13_10 = { -- update 13.10 - Reserved Storages 47901 - 47951
+			CradleOfMonsters = {
+				QuestLine = 47901,
+				Access = {
+					Ingol = 47902,
+					LowerIngol = 47903,
+					Monster = 47904,
+					MutatedAbomination = 47905,
+				},
+			}
+		}
 	},
 	-- Reserved storage from 63951 - 63999
 	ThaisExhibition = {

1d7fed415209b899c2d58ef9534c0db3a08c8b00
Sorairei <79299476+Sorairei@users.noreply.github.com>
2023-07-23 00:07:08 -0600

    Bag You Covet script

---
+++ data-otservbr-global/scripts/actions/other/bag_you_covet.lua
@@ -0,0 +1,32 @@
+local rewards = {
+    { id = 43864, name = "Sanguine Blade" },
+    { id = 43866, name = "Sanguine Cudgel" },
+    { id = 43868, name = "Sanguine Hatchet" },
+    { id = 43870, name = "Sanguine Razor" },
+    { id = 43872, name = "Sanguine Bludgeon" },
+    { id = 43874, name = "Sanguine Battleaxe" },
+    { id = 43876, name = "Sanguine Legs" },
+    { id = 43877, name = "Sanguine Bow" },
+    { id = 43879, name = "Sanguine Crossbow" },
+    { id = 43881, name = "Sanguine Greaves" },
+    { id = 43882, name = "Sanguine Coil" },
+    { id = 43884, name = "Sanguine Boots" },
+    { id = 43885, name = "Sanguine Rod" },
+    { id = 43887, name = "Sanguine Galoshes" }
+}
+
+local bagyouCovet = Action()
+
+function bagyouCovet.onUse(player, item, fromPosition, target, toPosition, isHotkey)
+	local randId = math.random(1, #rewards)
+	local rewardItem = rewards[randId]
+
+	player:addItem(rewardItem.id, 1)
+	item:remove(1)
+
+	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, 'You received a ' .. rewardItem.name .. '.')
+	return true
+end
+
+bagyouCovet:id(43895)
+bagyouCovet:register()

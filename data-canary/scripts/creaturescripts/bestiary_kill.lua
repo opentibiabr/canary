e2e4004be823f88d781da2323d1d60032cc1e4c9
Luan Santos <git@luan.sh>
2023-07-24 10:38:20 -0700

    oops wrong file

--- data-canary/scripts/creaturescripts/bestiary_kill.lua
+++ data-canary/scripts/creaturescripts/bestiary_kill.lua
@@ -1,24 +1,28 @@
 local bestiaryOnKill = CreatureEvent("BestiaryOnKill")
+
 function bestiaryOnKill.onKill(player, creature, lastHit)
 	if not player:isPlayer() or not creature:isMonster() or creature:hasBeenSummoned() or creature:isPlayer() then
 		return true
 	end
 
-	local bestiaryBetterment = Concoction.find(Concoction.Ids.BestiaryBetterment)
-	if not bestiaryBetterment then
-		Spdlog.warn("[BestiaryOnKill] - Could not find BestiaryBetterment concoction.")
-	end
+	local bestiaryMultiplier = (configManager.getNumber(configKeys.BESTIARY_KILL_MULTIPLIER) or 1)
 	for cid, damage in pairs(creature:getDamageMap()) do
 		local participant = Player(cid)
 		if participant and participant:isPlayer() then
-			local bestiaryMultiplier = (configManager.getNumber(configKeys.BESTIARY_KILL_MULTIPLIER) or 1)
-			if bestiaryBetterment and bestiaryBetterment:active(participant) then
-				bestiaryMultiplier = bestiaryMultiplier * bestiaryBetterment.config.multiplier
-			end
 			participant:addBestiaryKill(creature:getName(), bestiaryMultiplier)
 		end
 	end
 
 	return true
 end
+
 bestiaryOnKill:register()
+
+local loginBestiaryPlayer = CreatureEvent("loginBestiaryPlayer")
+
+function loginBestiaryPlayer.onLogin(player)
+	player:registerEvent("BestiaryOnKill")
+	return true
+end
+
+loginBestiaryPlayer:register()

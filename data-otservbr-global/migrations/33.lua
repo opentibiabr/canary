41952fd1953678026b91e19978a654f10e1152b7
Antonio Thiago de Araújo Ferreira <thiago1234_@hotmail.com>
2023-07-26 01:04:55 -0300

    Update data-otservbr-global/migrations/33.lua
    Co-authored-by: Luan Santos <github@luan.sh>

--- data-otservbr-global/migrations/33.lua
+++ data-otservbr-global/migrations/33.lua
@@ -1,33 +1,30 @@
 function onUpdateDatabase()
 	Spdlog.info("Updating database to version 34 (multi-world)")
-	db.query(
-		[[
-			CREATE TABLE IF NOT EXISTS `worlds`(
+	db.query([[
+		CREATE TABLE IF NOT EXISTS `worlds`(
 			`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
 			`name` varchar(32) NOT NULL,
 			CONSTRAINT `worlds_pk` PRIMARY KEY (`id`),
 			CONSTRAINT `worlds_unique` UNIQUE (`name`)
 		) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+	]])
 
-		]]
-	)
 	db.query([[
-		ALTER TABLE players ADD COLUMN IF NOT EXISTS world_id int(11) unsigned not null default 1,
-		ADD CONSTRAINT `world_id_fk`
-        FOREIGN KEY (`world_id`) REFERENCES `worlds` (`id`)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-		]])
-	
+		ALTER TABLE players
+			ADD COLUMN world_id int(11) unsigned not null default 1,
+			ADD CONSTRAINT `world_id_fk` FOREIGN KEY (`world_id`) REFERENCES `worlds` (`id`)
+			ON DELETE CASCADE
+			ON UPDATE CASCADE
+	]])
+
 	db.query([[
-		ALTER TABLE houses 
-		ADD COLUMN IF NOT EXISTS map_id int(25) not null,
-		ADD COLUMN IF NOT EXISTS world_id int(11) unsigned not null default 1,
-		ADD CONSTRAINT `houses_world_id_fk`
-		FOREIGN KEY (`world_id`) REFERENCES `worlds` (`id`)
-		ON DELETE CASCADE
-		ON UPDATE CASCADE
-		]])
+		ALTER TABLE houses
+			ADD COLUMN map_id int(25) not null,
+			ADD COLUMN world_id int(11) unsigned not null default 1,
+			ADD CONSTRAINT `houses_world_id_fk` FOREIGN KEY (`world_id`) REFERENCES `worlds` (`id`)
+			ON DELETE CASCADE
+			ON UPDATE CASCADE
+	]])
 		
 	return true
 end

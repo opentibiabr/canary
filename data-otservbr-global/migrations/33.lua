46d9ab3a5174e7d120002a69d255a274df55cc24
Elson Costa <elsongabriel@hotmail.com>
2023-07-27 17:36:34 -0300

    Vip System: removed migration unused.

--- data-otservbr-global/migrations/33.lua
+++ data-otservbr-global/migrations/33.lua
@@ -1,11 +1,3 @@
 function onUpdateDatabase()
-	Spdlog.info("Updating database to version 34 (create vip system columns)")
-	db.query(
-		[[
-			ALTER TABLE `accounts`
-				ADD COLUMN `vipdays` int(11) NOT NULL DEFAULT 0 AFTER `lastday`,
-				ADD COLUMN `viplastday` int(10) NOT NULL DEFAULT 0 AFTER `vipdays`;
-		]]
-	)
-	return true
+	return false -- true = There are others migrations file | false = this is the last migration file
 end

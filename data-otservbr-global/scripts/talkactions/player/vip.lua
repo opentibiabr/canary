b5e717caac74b4281bb643c5af2b0277f9cd7071
Elson Costa <elsongabriel@hotmail.com>
2023-07-27 16:36:24 -0300

    Vip System: talkactions improvements (removed script duplicated).

--- data-otservbr-global/scripts/talkactions/player/vip.lua
+++ data-otservbr-global/scripts/talkactions/player/vip.lua
@@ -1,12 +1,13 @@
-local vip = TalkAction("!vip")
+local vip = TalkAction("!checkvip", "!vip")
 
 function vip.onSay(player, words, param)
 	local days = player:getVipDays()
 	if days == 0 then
-		player:sendCancelMessage('You do not have VIP on your account.')
-		player:sendTextMessage(MESSAGE_STATUS, 'You do not have VIP on your account.')
+		local msg = 'You do not have VIP on your account.'
+		player:sendCancelMessage(msg)
+		player:sendTextMessage(MESSAGE_STATUS, msg)
 	else
-		player:sendTextMessage(MESSAGE_STATUS, 'You have ' .. days .. ' days of VIP on your account.')
+		player:sendTextMessage(MESSAGE_STATUS, string.format('You have %s VIP day%s left.', (days == 0xFFFF and 'infinite amount of' or days), (days == 1 and '' or 's')))
 	end
 	return false
 end

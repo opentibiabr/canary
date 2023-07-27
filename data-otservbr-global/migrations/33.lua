c6e2b4b60cf7d44cb0526d8173cbc80227161d71
Elson Costa <elsongabriel@hotmail.com>
2023-07-27 07:35:55 -0300

    Vip System: merge and update migrations.

--- data-otservbr-global/migrations/33.lua
+++ data-otservbr-global/migrations/33.lua
@@ -1,3 +1,11 @@
 function onUpdateDatabase()
-	return false -- true = There are others migrations file | false = this is the last migration file
+	Spdlog.info("Updating database to version 34 (create vip system columns)")
+	db.query(
+		[[
+			ALTER TABLE `accounts`
+				ADD COLUMN `vipdays` int(11) NOT NULL DEFAULT 0 AFTER `lastday`,
+				ADD COLUMN `viplastday` int(10) NOT NULL DEFAULT 0 AFTER `vipdays`;
+		]]
+	)
+	return true
 end

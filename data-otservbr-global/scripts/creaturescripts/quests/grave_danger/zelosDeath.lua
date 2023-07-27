efc0418fd44299ff976736a7aeb2686a5be4b40e
Douglas dos Santos <47014952+QuebradaZN@users.noreply.github.com>
2023-07-27 01:06:06 -0300

    Remove RemoveMonster

--- data-otservbr-global/scripts/creaturescripts/quests/grave_danger/zelosDeath.lua
+++ data-otservbr-global/scripts/creaturescripts/quests/grave_danger/zelosDeath.lua
@@ -1,18 +1,15 @@
 local config = {
     centerPosition = Position(33443, 31545, 13),
-	rangeX = 11,
-	rangeY = 11,
+    rangeX = 11,
+    rangeY = 11,
 }
 
 local KingzelosDeath = CreatureEvent("zelosDeath")
-local removeMonsters = false
 
 function KingzelosDeath.onPrepareDeath(creature)
     local spectators = Game.getSpectators(config.centerPosition, false, false, config.rangeX, config.rangeX, config.rangeY, config.rangeY)
     for _, cid in pairs(spectators) do
-        if removeMonsters and cid:isMonster() and not cid:getMaster() then
-            cid:remove()
-        elseif cid:isPlayer() then
+        if cid:isPlayer() then
             if cid:getStorageValue(Storage.Quest.U12_20.GraveDanger.Bosses.InquisitionOutfitReceived) == -1 then
                 cid:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Congratulations you received the Hand of the Inquisition Outfit.")
                 cid:addOutfit(1244, 0)

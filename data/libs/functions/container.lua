7f0c3af7873b3691ef0496dea4868bea78c151a5
Luan Santos <git@luan.sh>
2023-07-27 20:34:51 -0700

    fix: use loot modifiers correctly when creating loot (#1310)
    Container:createLootItem now returns false when no loot was created (either by error or because we rolled a random that didn't add it). This was causing a warning to pop up on the monster callback. This was a harmless warning, but we were missing logs when something actually went wrong (not enough space on the corpse or invalid itemID).
    
    This adds logs inside createLootItem and silences the spurious alerts above.

--- data/libs/functions/container.lua
+++ data/libs/functions/container.lua
@@ -4,6 +4,7 @@ end
 
 function Container.createLootItem(self, item, charm, modifier)
 	if self:getEmptySlots() == 0 then
+		Spdlog.warn(string.format("[Container:createLootItem] - Could not add loot item to ontainer id: %d because no more empty slots were available", self:getId()))
 		return false
 	end
 
@@ -13,6 +14,7 @@ function Container.createLootItem(self, item, charm, modifier)
 	local chanceTo = item.chance
 
 	if not lootBlockType then
+		Spdlog.warn(string.format("[Container:createLootItem] - Could not add loot item to ontainer id: %d because item type was not found", self:getId(), item.itemId))
 		return false
 	end
 

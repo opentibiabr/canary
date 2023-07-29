691025c6e541eff67d7c4c863cf14c44df14a70b
Luan Luciano <luanluciano@outlook.com>
2023-07-29 02:37:58 -0300

    Ajustment

--- data-canary/scripts/spells/support/charge.lua
+++ data-canary/scripts/spells/support/charge.lua
@@ -18,7 +18,7 @@ function spell.onCastSpell(creature, variant)
 			if summon_t and summon_t:familiar() then
 				local deltaSpeed = math.max(creature:getBaseSpeed() - summon:getBaseSpeed(), 0)
 				local FamiliarSpeed = ((summon:getBaseSpeed() + deltaSpeed) * 0.9) - 72
-				local FamiliarHaste = createConditionObject(CONDITION_HASTE)
+				local FamiliarHaste = Condition(CONDITION_HASTE)
 				setConditionParam(FamiliarHaste, CONDITION_PARAM_TICKS, 5000)
 				setConditionParam(FamiliarHaste, CONDITION_PARAM_SPEED, FamiliarSpeed)
 				summon:addCondition(FamiliarHaste)
--- data-canary/scripts/spells/support/strong_haste.lua
+++ data-canary/scripts/spells/support/strong_haste.lua
@@ -18,7 +18,7 @@ function spell.onCastSpell(creature, var)
 			if summon_t and summon_t:familiar() then
 				local deltaSpeed = math.max(creature:getBaseSpeed() - summon:getBaseSpeed(), 0)
 				local FamiliarSpeed = ((summon:getBaseSpeed() + deltaSpeed) * 0.7) - 56
-				local FamiliarHaste = createConditionObject(CONDITION_HASTE)
+				local FamiliarHaste = Condition(CONDITION_HASTE)
 				setConditionParam(FamiliarHaste, CONDITION_PARAM_TICKS, 22000)
 				setConditionParam(FamiliarHaste, CONDITION_PARAM_SPEED, FamiliarSpeed)
 				summon:addCondition(FamiliarHaste)

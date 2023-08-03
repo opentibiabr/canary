0239dd243dc18cbf935cda4abca6f25c1f70963e
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2023-08-03 12:50:57 -0300

    feat: send money to the bank in the quickloot

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -2550,6 +2550,21 @@ ReturnValue Game::internalCollectLootItems(Player* player, Item* item, ObjectCat
 		return RETURNVALUE_NOTPOSSIBLE;
 	}
 
+	// Send money to the bank
+	if (item->getID() == ITEM_GOLD_COIN || item->getID() == ITEM_PLATINUM_COIN || item->getID() == ITEM_CRYSTAL_COIN){
+		uint64_t money = 0;
+		if (item->getID() == ITEM_PLATINUM_COIN){
+			money = item->getItemCount() * 100;
+		} else if (item->getID() == ITEM_CRYSTAL_COIN){
+			money = item->getItemCount() * 10000;
+		} else {
+			money = item->getItemCount();
+		}
+		internalRemoveItem(item, item->getItemCount());
+		player->setBankBalance(player->getBankBalance()+ money);
+		return RETURNVALUE_NOERROR;
+	}
+
 	bool fallbackConsumed = false;
 	Container* lootContainer = findLootContainer(player, fallbackConsumed, category);
 	if (!lootContainer) {

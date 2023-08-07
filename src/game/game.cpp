4e87219eccb134e1c2efb3678ad6a9a4f9100e21
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2023-08-07 20:23:29 -0300

    fix: don't remove reward containers on open/close reward chest (#1364)
    It will only remove on login. This will possibly fix the crash related to reward collect/reward chest.
    
    Apparently in some scenario there is a reward container being removed from memory and being used later, which causes memory usage after clearing resulting in undefined behaviour (crash).

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -2600,7 +2600,7 @@ ReturnValue Game::collectRewardChestItems(Player* player, uint32_t maxMoveItems 
 		if (limitMove) {
 			lootedItemsMessage = fmt::format("You can only collect {} items at a time. {} of {} objects were picked up.", maxMoveItems, movedRewardItems, rewardCount);
 			player->sendTextMessage(MESSAGE_EVENT_ADVANCE, lootedItemsMessage);
-			return RETURNVALUE_NOTPOSSIBLE;
+			return RETURNVALUE_NOERROR;
 		}
 
 		ObjectCategory_t category = getObjectCategory(item);
@@ -2613,7 +2613,7 @@ ReturnValue Game::collectRewardChestItems(Player* player, uint32_t maxMoveItems 
 	player->sendTextMessage(MESSAGE_EVENT_ADVANCE, lootedItemsMessage);
 
 	if (movedRewardItems == 0) {
-		return RETURNVALUE_NOTENOUGHROOM;
+		return RETURNVALUE_NOTPOSSIBLE;
 	}
 
 	return RETURNVALUE_NOERROR;

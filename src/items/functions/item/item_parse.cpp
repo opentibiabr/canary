6652d5f9cc666524941c384ed58c3dcce30c30ed
Beats <daniel15042014@hotmail.com>
2023-08-13 22:30:21 -0300

    fix/enable debug mode cmake

--- src/items/functions/item/item_parse.cpp
+++ src/items/functions/item/item_parse.cpp
@@ -585,7 +585,7 @@ void ItemParse::parseAbsorbPercent(const std::string &tmpStrValue, pugi::xml_att
 void ItemParse::parseSupressDrunk(const std::string &tmpStrValue, pugi::xml_attribute valueAttribute, ItemType &itemType) {
 	std::string stringValue = tmpStrValue;
 	if (valueAttribute.as_bool()) {
-		ConditionType_t conditionType;
+		ConditionType_t conditionType = CONDITION_NONE;
 		if (stringValue == "suppressdrunk") {
 			conditionType = CONDITION_DRUNK;
 		} else if (stringValue == "suppressenergy") {
--- src/security/rsa.cpp
+++ src/security/rsa.cpp
@@ -135,8 +135,8 @@ enum {
 	CRYPT_RSA_ASN1_BITSTRING = 3
 };
 
-uint16_t RSA::decodeLength(char*&pos) const {
-	std::string buffer;
+uint16_t RSA::decodeLength(char*& pos) const {
+	uint8_t buffer[4] = {0}; // Inicialize buffer como um array de 4 bytes
 	auto length = static_cast<uint16_t>(static_cast<uint8_t>(*pos++));
 	if (length & 0x80) {
 		length &= 0x7F;
@@ -144,23 +144,19 @@ uint16_t RSA::decodeLength(char*&pos) const {
 			SPDLOG_ERROR("[RSA::loadPEM] - Invalid 'length'");
 			return 0;
 		}
-		buffer[0] = buffer[1] = buffer[2] = buffer[3] = 0;
+		// Removendo a inicialização redundante de buffer
 		switch (length) {
-			case 4:
-				buffer[3] = static_cast<uint8_t>(*pos++);
-			case 3:
-				buffer[2] = static_cast<uint8_t>(*pos++);
-			case 2:
-				buffer[1] = static_cast<uint8_t>(*pos++);
-			case 1:
-				buffer[0] = static_cast<uint8_t>(*pos++);
-			default:
-				break;
+			case 4: buffer[3] = static_cast<uint8_t>(*pos++);
+			case 3: buffer[2] = static_cast<uint8_t>(*pos++);
+			case 2: buffer[1] = static_cast<uint8_t>(*pos++);
+			case 1: buffer[0] = static_cast<uint8_t>(*pos++);
+			default: break;
 		}
-		std::memcpy(&length, buffer.data(), sizeof(length));
+		std::memcpy(&length, buffer, sizeof(length)); // buffer é um array, então não precisa de .data()
 	}
 	return length;
 }
+
 
 void RSA::readHexString(char*&pos, uint16_t length, std::string &output) const {
 	output.reserve(static_cast<size_t>(length) * 2);

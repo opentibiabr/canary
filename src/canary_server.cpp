0b821458e4e42e5d7230c1f0de8146859b329388
lgrossi <lucas.ggrossi@gmail.com>
2023-08-16 21:32:55 +0200

    improve: Move scheduler behind thread pool

--- src/canary_server.cpp
+++ src/canary_server.cpp
@@ -55,8 +55,6 @@ CanaryServer::CanaryServer(
 }
 
 int CanaryServer::run() {
-	g_scheduler().start();
-
 	g_dispatcher().addTask([this] {
 		loadConfigLua();
 
@@ -371,6 +369,5 @@ void CanaryServer::startupErrorMessage() {
 }
 
 void CanaryServer::shutdown() {
-	g_scheduler().shutdown();
 	inject<ThreadPool>().shutdown();
 }

5e1ba6be1a18b110b1b621ab036f5b76fbdb136d
jarrodpjohnston <jarrodpjohnston@gmail.com>
2023-08-20 18:17:38 -0500

    feat: executioner throw chain

--- data-otservbr-global/scripts/spells/attack/executioners_throw.lua
+++ data-otservbr-global/scripts/spells/attack/executioners_throw.lua
@@ -1,81 +1,35 @@
-local combatBounce = Combat()
-combatBounce:setParameter(COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
-combatBounce:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_HITAREA)
-combatBounce:setParameter(COMBAT_PARAM_BLOCKARMOR, 1)
-combatBounce:setParameter(COMBAT_PARAM_USECHARGES, 1)
+local combat = Combat()
+combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
+combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_WHIRLWINDAXE)
+combat:setParameter(COMBAT_PARAM_BLOCKARMOR, 1)
 
 function onGetFormulaValues(player, skill, attack, factor)
 	local skillTotal = skill * attack
 	local levelTotal = player:getLevel() / 5
 	return -(((skillTotal * 0.17) + 17) + (levelTotal)) * 1.28, -(((skillTotal * 0.20) + 40) + (levelTotal)) * 1.28
 end
-combatBounce:setCallback(CALLBACK_PARAM_SKILLVALUE, "onGetFormulaValues")
 
-local combatCast = Combat()
-combatCast:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_WEAPONTYPE)
-function onTargetCreature(creature, target)
-	if notcreature or not target or not creature:isPlayer() then
+combat:setCallback(CALLBACK_PARAM_SKILLVALUE, "onGetFormulaValues")
+
+function getChainValue(creature)
+	local grade = creature:revelationStageWOD("Executioner's Throw")
+	if grade == 0 then
 		return false
 	end
 
-	local grade = creature:upgradeSpellsWOD("Executioner's Throw")
-	if grade == WHEEL_GRADE_NONE then
-		return false
-	end
 	local bounces = 0
-	if grade >= WHEEL_GRADE_MAX then
+	if grade >= 3 then
 		bounces = 4
-	elseif grade >= WHEEL_GRADE_UPGRADED then
+	elseif grade >= 2 then
 		bounces = 3
-	elseif grade >= WHEEL_GRADE_REGULAR then
+	elseif grade >= 1 then
 		bounces = 2
 	end
 
-	local weaponType = WEAPON_AXE
-	local effect = CONST_ANI_WHIRLWINDAXE
-	local item = creature:getSlotItem(CONST_SLOT_RIGHT)
-	if item then
-		local itemType = ItemType(item:getId())
-		if itemType then
-			weaponType = itemType:getWeaponType()
-		end
-	end
-	if weaponType == WEAPON_SWORD then
-		effect = CONST_ANI_WHIRLWINDSWORD
-	elseif weaponType == WEAPON_CLUB then
-		effect = CONST_ANI_WHIRLWINDCLUB
-	end
-
-	local position = creature:getPosition()
-	local targetPosition = target:getPosition()
-
-	local targetId = target:getId()
-	local var = {}
-	var.instantName = "Executioner's Throw"
-	var.runeName = ""
-	var.type = 1 -- VARIANT_NUMBER
-	var.number = targetId
-	combatBounce:execute(creature, var)
-	position:sendDistanceEffect(targetPosition, effect)
-	position = targetPosition
-	local spectators = Game.getSpectators(targetPosition, false, false, 5, 5, 5, 5)
-	for _, spectator in ipairs(spectators) do
-		if bounces == 0 then
-			return true
-		elseif spectator and spectator:getId() ~= targetId and spectator:isMonster() then
-			var.number = spectator:getId()
-			local spectatorPosition = spectator:getPosition()
-			if combatBounce:execute(creature, var) then
-				position:sendDistanceEffect(spectatorPosition, effect)
-				position = spectatorPosition
-				bounces = bounces - 1
-			end
-		end
-	end
-
-	return true
+	return bounces + 1, 3, false
 end
-combatCast:setCallback(CALLBACK_PARAM_TARGETCREATURE, "onTargetCreature")
+
+combat:setCallback(CALLBACK_PARAM_CHAINVALUE, "getChainValue")
 
 local spell = Spell("instant")
 
@@ -84,23 +38,25 @@ function spell.onCastSpell(creature, var)
 		return false
 	end
 
-	local grade = creature:upgradeSpellsWOD("Executioner's Throw")
-	if grade == WHEEL_GRADE_NONE then
+	local grade = creature:revelationStageWOD("Executioner's Throw")
+	Spdlog.info(grade)
+	if grade == 0 then
 		creature:sendCancelMessage("You cannot cast this spell")
 		creature:getPosition():sendMagicEffect(CONST_ME_POFF)
 		return false
 	end
+
 	local cooldown = 0
-	if grade >= WHEEL_GRADE_MAX then
+	if grade >= 3 then
 		cooldown = 10
-	elseif grade >= WHEEL_GRADE_UPGRADED then
+	elseif grade >= 2 then
 		cooldown = 14
-	elseif grade >= WHEEL_GRADE_REGULAR then
+	elseif grade >= 1 then
 		cooldown = 18
 	end
 
-	var.instantName = "Executioner's Throw Cast"
-	if combatCast:execute(creature, var) then
+	var.instantName = "Executioner's Throw"
+	if combat:execute(creature, var) then
 		local condition = Condition(CONDITION_SPELLCOOLDOWN, CONDITIONID_DEFAULT, 261)
 		condition:setTicks((cooldown * 1000)/configManager.getFloat(configKeys.RATE_SPELL_COOLDOWN))
 		creature:addCondition(condition)

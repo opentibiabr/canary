9b7115ba9a5a1655c5c5bc6f4a66ba6d93e33394
LeoTK <41605307+LeoTKBR@users.noreply.github.com>
2023-08-20 21:08:43 -0300

    Update charger_spawn.lua
    Fix Error spawn

--- data-otservbr-global/scripts/creaturescripts/quests/heart_of_destruction/charger_spawn.lua
+++ data-otservbr-global/scripts/creaturescripts/quests/heart_of_destruction/charger_spawn.lua
@@ -1,4 +1,4 @@
-local function chargerSpawn(pos)
+local function chargerSpawnFix(pos)
 	Game.createMonster("charger", pos, false, true)
 	spawningCharge = false
 	return true
@@ -20,7 +20,7 @@ function chargerSpawn.onDeath(creature)
 	}
 
 	local pos = positions[math.random(1, #positions)]
-	addEvent(chargerSpawn, 6000, pos)
+	addEvent(chargerSpawnFix, 6000, pos)
 	spawningCharge = true
 	return true
 end

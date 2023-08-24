33bae1b4303e0df2cc1972b9a4d7ca1ce9d3ffd8
lgrossi <lucas.ggrossi@gmail.com>
2023-08-24 19:32:29 +0200

    fix: inject g_logger from interface not impl
    Logger interface was created to generate testable abstraction to tests. This is working fine for log ingestion in construction level. However, for those using g_logger it has a hard bind to the impl class LogWithSpdLog instead of the Interface.
    
    This commit changes this to make it more flexible and improve testing.

--- src/lib/logging/log_with_spd_log.hpp
+++ src/lib/logging/log_with_spd_log.hpp
@@ -13,10 +13,10 @@ public:
 	LogWithSpdLog();
 	~LogWithSpdLog() override = default;
 
-	static LogWithSpdLog &getInstance();
+	static Logger &getInstance();
 
 	void setLevel(const std::string &name) override;
-	[[nodiscard]] virtual std::string getLevel() const override;
+	[[nodiscard]] std::string getLevel() const override;
 
 	void log(std::string lvl, fmt::basic_string_view<char> msg) const override;
 };

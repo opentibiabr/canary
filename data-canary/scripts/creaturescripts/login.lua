a2486a53ecaa4c8a0a23c7afdd4d6ede2f5313aa
Luan Santos <github@luan.sh>
2023-08-26 11:42:47 -0700

    Apply suggestions from code review
    Co-authored-by: Eduardo Dantas <eduardo.dantas@hotmail.com.br>

--- data-canary/scripts/creaturescripts/login.lua
+++ data-canary/scripts/creaturescripts/login.lua
@@ -100,7 +100,7 @@ function login.onLogin(player)
 		baseRate = baseRate * 100
 		if configManager.getBoolean(configKeys.VIP_SYSTEM_ENABLED) then
 			local vipBonusExp = configManager.getNumber(configKeys.VIP_BONUS_EXP)
-			if (vipBonusExp > 0 and player:isVip()) then
+			if vipBonusExp > 0 and player:isVip() then
 				vipBonusExp = (vipBonusExp > 100 and 100) or vipBonusExp
 				baseRate = baseRate * (1 + (vipBonusExp / 100))
 				player:sendTextMessage(MESSAGE_BOOSTED_CREATURE, "Normal base xp is: " .. baseRate .. "%, because you are VIP, bonus of " ..vipBonusExp .."%")

23d01c1a2dbbc80ad3037dd8d03bf1ac852829df
Luan Santos <git@luan.sh>
2023-08-29 11:14:41 -0700

    feat: magma bubble full mechanics (#1481)
    Video of the full mechanics (it's long, if you want to check it out i'd
    speed it up :P):
    https://www.youtube.com/watch?v=JqUK-iGql1k

--- data-otservbr-global/monster/quests/primal_ordeal_quest/magma_bubble.lua
+++ data-otservbr-global/monster/quests/primal_ordeal_quest/magma_bubble.lua
@@ -20,8 +20,8 @@ monster.events = {
 monster.health = 300000
 monster.maxHealth = 300000
 monster.race = "undead"
-monster.corpse = 36843
-monster.speed = 40
+monster.corpse = 36847
+monster.speed = 0
 monster.manaCost = 0
 
 monster.changeTarget = {
@@ -36,10 +36,7 @@ monster.bosstiary = {
 }
 
 monster.strategiesTarget = {
-	nearest = 70,
-	health = 10,
-	damage = 10,
-	random = 10,
+	nearest = 100,
 }
 
 monster.flags = {
@@ -59,7 +56,7 @@ monster.flags = {
 	isBlockable = false,
 	canWalkOnEnergy = true,
 	canWalkOnFire = true,
-	canWalkOnPoison = true
+	canWalkOnPoison = true,
 }
 
 monster.light = {
@@ -84,6 +81,7 @@ monster.loot = {
 	{ name = "giant emerald", chance = 4082 },
 	{ name = "giant sapphire", chance = 2041 },
 	{ name = "giant topaz", chance = 2041 },
+	{ name = "fiery tear", chance = 1000 },
 	{ name = "arboreal tome", chance = 250 },
 	{ name = "arboreal crown", chance = 250 },
 	{ name = "spiritthorn armor", id = 39147, chance = 250 },
@@ -96,20 +94,21 @@ monster.loot = {
 	{ id = 39186, chance = 250 }, -- name = "charged arboreal ring"
 	{ id = 39180, chance = 250 }, -- name = "charged alicorn ring"
 	{ id = 39177, chance = 250 }, -- name = "charged spiritthorn ring"
+	{ name = "portable flame", chance = 250 },
+	{ name = "firefighting axe", chance = 250 },
 }
 
 monster.attacks = {
-	{ name = "melee", interval = 2000, chance = 100, minDamage = -600, maxDamage = -1300 },
-	{ name = "combat", interval = 200, chance = 20, type = COMBAT_ENERGYDAMAGE, minDamage = -600, maxDamage = -1500, target = false },
-	{ name = "combat", interval = 500, chance = 10, type = COMBAT_ENERGYDAMAGE, minDamage = -200, maxDamage = -2100, length = 8, spread = 3, effect = CONST_ME_MORTAREA, target = true },
-	{ name = "combat", interval = 500, chance = 10, type = COMBAT_ENERGYDAMAGE, minDamage = -250, maxDamage = -2600, radius = 8, effect = CONST_ME_MORTAREA, target = false },
-	{ name = "combat", interval = 2000, chance = 30, type = COMBAT_FIREDAMAGE, minDamage = -1000, maxDamage = -2000, target = true },
-	{ name = "combat", interval = 2000, chance = 25, type = COMBAT_FIREDAMAGE, minDamage = -1500, maxDamage = -2000, length = 8, spread = 0, effect = CONST_ME_FIREAREA, target = false },
+	{ name = "melee", interval = 2000, chance = 100, minDamage = -275, maxDamage = -750 },
+	{ name = "combat", interval = 2000, chance = 75, type = COMBAT_FIREDAMAGE, minDamage = -525, maxDamage = -650, radius = 3, range = 8, effect = CONST_ME_HITBYFIRE, target = true },
+	{ name = "combat", interval = 3700, chance = 37, type = COMBAT_FIREDAMAGE, minDamage = -1700, maxDamage = -2750, length = 8, spread = 3, effect = CONST_ME_MAGIC_RED, target = false },
+	{ name = "combat", interval = 3100, chance = 15, type = COMBAT_FIREDAMAGE, minDamage = -1000, maxDamage = -2000, range = 8, effect = CONST_ME_FIREAREA, shootEffect = CONST_ANI_FIRE, target = true },
 }
 
 monster.defenses = {
 	defense = 65,
 	armor = 55,
+	mitigation = 2.0,
 	{ name = "combat", interval = 3000, chance = 35, type = COMBAT_HEALING, minDamage = 400, maxDamage = 500, effect = CONST_ME_MAGIC_BLUE, target = false },
 	{ name = "speed", interval = 2000, chance = 15, speedChange = 320, effect = CONST_ME_MAGIC_RED, target = false, duration = 5000 }
 }

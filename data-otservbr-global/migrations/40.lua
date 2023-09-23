c72176d3b03e14e5fb855268fb0bf7e3dca11015
Luan Santos <git@luan.sh>
2023-09-23 13:11:41 -0700

    improve: optimize house/kv saves

--- data-otservbr-global/migrations/40.lua
+++ data-otservbr-global/migrations/40.lua
@@ -1,3 +1,14 @@
 function onUpdateDatabase()
-	return false -- true = There are others migrations file | false = this is the last migration file
+	logger.info("Updating database to version 41 (optimize house_lists)")
+	db.query([[
+			ALTER TABLE `house_lists` DROP COLUMN `id`;
+		]])
+
+	db.query([[
+			ALTER TABLE `house_lists`
+			ADD COLUMN `version` bigint NOT NULL DEFAULT 0 AFTER `listid`,
+			ADD INDEX `version` (`version`),
+			ADD PRIMARY KEY (`house_id`, `listid`);
+		]])
+	return true
 end

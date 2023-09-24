862dd0c56466436a7c752812fea14e01f9834f00
RCP91 <roberto.cpmg@gmail.com>
2023-09-23 23:29:07 -0400

    fix: transform foreshock & aftershock
    They don't transform, now the count is like tibia global. The removed realityQuakeStage is not necessary as we use life storage of transformations with a global variable.

--- data-otservbr-global/scripts/creaturescripts/quests/heart_of_destruction/foreshock_transform.lua
+++ data-otservbr-global/scripts/creaturescripts/quests/heart_of_destruction/foreshock_transform.lua
@@ -15,26 +15,23 @@ function foreshockTransform.onThink(creature)
 		[80] = { fromStage = 0, toStage = 1 },
 		[60] = { fromStage = 1, toStage = 2 },
 		[40] = { fromStage = 2, toStage = 3 },
-		[25] = { fromStage = 3, toStage = 4 },
+		[20] = { fromStage = 3, toStage = 4 },
 		[10] = { fromStage = 4, toStage = 5 },
 	}
 
 	for index, value in pairs(monsterTable) do
 		local hp = (creature:getHealth() / creature:getMaxHealth()) * 100
-		if realityQuakeStage == 0 then
-			local foreshockHealth = creature:getHealth()
-			if hp <= index and aftershockStage == value.fromStage then
-				creature:remove()
-				for i = 1, #sparkOfDestructionPositions do
-					Game.createMonster("spark of destruction", #sparkOfDestructionPositions[i], false, true)
-				end
-				local monster = Game.createMonster("aftershock", { x = 32208, y = 31248, z = 14 }, false, true)
-				monster:addHealth(-monster:getHealth() + foreshockHealth, COMBAT_PHYSICALDAMAGE)
-				foreshockStage = value.toStage
+		foreshockHealth = creature:getHealth()
+		if hp <= index and foreshockStage == value.fromStage then
+			creature:remove()
+			for i = 1, #sparkOfDestructionPositions do
+				Game.createMonster("spark of destruction", sparkOfDestructionPositions[i], false, true)
 			end
+			local monster = Game.createMonster("aftershock", { x = 32208, y = 31248, z = 14 }, false, true)
+			monster:addHealth(-monster:getHealth() + aftershockHealth, COMBAT_PHYSICALDAMAGE)
+			foreshockStage = value.toStage
 		end
 	end
 	return true
 end
-
 foreshockTransform:register()

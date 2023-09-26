ca5f63c220073203c5b6a16e8fa4131a7ed3b399
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2023-09-26 15:08:33 -0300

    fix: attr condition enums (subid foods and others) (#1641)

--- src/lua/functions/core/game/lua_enums.cpp
+++ src/lua/functions/core/game/lua_enums.cpp
@@ -110,6 +110,7 @@ void LuaEnums::init(lua_State* L) {
 	initBosstiaryEnums(L);
 	initSoundEnums(L);
 	initWheelEnums(L);
+	initAttributeConditionSubIdEnums(L);
 }
 
 void LuaEnums::initOthersEnums(lua_State* L) {
@@ -439,6 +440,13 @@ void LuaEnums::initConditionParamEnums(lua_State* L) {
 	registerEnum(L, CONDITION_PARAM_INCREASE_MANADRAINPERCENT);
 	registerEnum(L, CONDITION_PARAM_INCREASE_DROWNPERCENT);
 	registerEnum(L, CONDITION_PARAM_CHARM_CHANCE_MODIFIER);
+}
+
+void LuaEnums::initAttributeConditionSubIdEnums(lua_State* L) {
+	std::string luaNamespace = "AttrSubId_";
+	for (auto value : magic_enum::enum_values<AttrSubId_t>()) {
+		registerEnumClassNamespace(L, luaNamespace, value);
+	}
 }
 
 void LuaEnums::initConstMeEnums(lua_State* L) {

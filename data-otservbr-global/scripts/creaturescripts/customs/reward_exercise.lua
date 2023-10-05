f0160ed8bdd912b4c85625c29de80721b307f976
Elson Costa <elsongabriel@hotmail.com>
2023-10-05 10:50:35 -0300

    Reward Exercise: Adjusted validation to ignore players in Dawnport or rookgard.

--- data-otservbr-global/scripts/creaturescripts/customs/reward_exercise.lua
+++ data-otservbr-global/scripts/creaturescripts/customs/reward_exercise.lua
@@ -1,12 +1,10 @@
 local winReward = CreatureEvent("WinReward")
 
 function winReward.onLogin(player)
-	if configManager.getBoolean(configKeys.TOGGLE_RECEIVE_REWARD) then
+	if (configManager.getBoolean(configKeys.TOGGLE_RECEIVE_REWARD) and player:getTown():getId() >= TOWNS_LIST.AB_DENDRIEL) then
 		-- check user won exercise weapon and send message
-		if not table.contains({ TOWNS_LIST.DAWNPORT, TOWNS_LIST.DAWNPORT_TUTORIAL }, player:getTown():getId()) then
-			if player:getStorageValue(tonumber(Storage.PlayerWeaponReward)) ~= 1 then
-				player:sendTextMessage(MESSAGE_LOGIN, "You can receive an exercise weapon using command !reward")
-			end
+		if player:getStorageValue(tonumber(Storage.PlayerWeaponReward)) ~= 1 then
+			player:sendTextMessage(MESSAGE_LOGIN, "You can receive an exercise weapon using command !reward")
 		end
 	end
 	return true

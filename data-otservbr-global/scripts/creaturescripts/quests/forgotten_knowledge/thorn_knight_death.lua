9df5f27e545f685dc32a2f2f4141e0c4278da302
Luan Santos <git@luan.sh>
2023-11-03 15:00:34 -0700

    fix: add missing set.lua import (#1773)

--- data-otservbr-global/scripts/creaturescripts/quests/forgotten_knowledge/thorn_knight_death.lua
+++ data-otservbr-global/scripts/creaturescripts/quests/forgotten_knowledge/thorn_knight_death.lua
@@ -10,19 +10,15 @@ local function checkBlood(position)
 end
 local thornKnightDeath = CreatureEvent("ThornKnightDeath")
 function thornKnightDeath.onDeath(creature, corpse, lasthitkiller, mostdamagekiller, lasthitunjustified, mostdamageunjustified)
-	local targetMonster = creature:getMonster()
-	if not targetMonster then
+	if creature:getName():lower() == "mounted thorn knight" then
+		creature:say("The thorn knight unmounts!", TALKTYPE_MONSTER_SAY)
+		Game.createMonster("the shielded thorn knight", creature:getPosition(), true, true)
+		Game.createMonster("thorn steed", creature:getPosition(), false, true)
+		addEvent(checkBlood, 1, creature:getPosition())
 		return true
-	end
-	if targetMonster:getName():lower() == "mounted thorn knight" then
-		targetMonster:say("The thorn knight unmounts!", TALKTYPE_MONSTER_SAY)
-		Game.createMonster("the shielded thorn knight", targetMonster:getPosition(), true, true)
-		Game.createMonster("thorn steed", targetMonster:getPosition(), false, true)
-		addEvent(checkBlood, 1, targetMonster:getPosition())
-		return true
-	elseif targetMonster:getName():lower() == "the shielded thorn knight" then
-		Game.createMonster("the enraged thorn knight", targetMonster:getPosition(), true, true)
-		addEvent(checkBlood, 1, targetMonster:getPosition())
+	elseif creature:getName():lower() == "the shielded thorn knight" then
+		Game.createMonster("the enraged thorn knight", creature:getPosition(), true, true)
+		addEvent(checkBlood, 1, creature:getPosition())
 		return true
 	end
 	return true

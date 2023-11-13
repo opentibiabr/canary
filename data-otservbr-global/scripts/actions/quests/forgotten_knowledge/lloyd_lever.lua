c42d7480975f26324445a8682af692e54c728573
Luan Colombo <94877887+luancolombo@users.noreply.github.com>
2023-11-13 16:48:45 +0000

    Update data-otservbr-global/scripts/actions/quests/forgotten_knowledge/lloyd_lever.lua
    Co-authored-by: Elson Costa <elsongabriel@hotmail.com>

--- data-otservbr-global/scripts/actions/quests/forgotten_knowledge/lloyd_lever.lua
+++ data-otservbr-global/scripts/actions/quests/forgotten_knowledge/lloyd_lever.lua
@@ -4,7 +4,6 @@ local config = {
 		position = Position(32799, 32827, 14),
 	},
 	requiredLevel = 250,
-
 	playerPositions = {
 		{ pos = Position(32759, 32868, 14), teleport = Position(32800, 32831, 14) },
 		{ pos = Position(32759, 32869, 14), teleport = Position(32800, 32831, 14) },

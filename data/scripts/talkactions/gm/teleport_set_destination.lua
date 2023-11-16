d79d7041d9437c2b4c64edcd84613488a088dd65
Elson Costa <elsongabriel@hotmail.com>
2023-11-16 15:38:31 -0300

    adjusted scripts.

--- data/scripts/talkactions/gm/teleport_set_destination.lua
+++ data/scripts/talkactions/gm/teleport_set_destination.lua
@@ -1,38 +1,32 @@
 -- Script for set teleport destination
 -- /teleport xxxx, xxxx, x
-local teleportSetDestination = TalkAction("/teleport")
+local teleportSetDestination = TalkAction("/teleport", "/tp")
 
 function teleportSetDestination.onSay(player, words, param)
 	-- create log
 	logCommand(player, words, param)
 
 	if param == "" then
-		player:sendCancelMessage("Teleport position required.")
+		player:sendCancelMessage("Command param required.")
 		return true
 	end
 
-	local position = player:getPosition()
-	position:getNextPosition(player:getDirection(), 1)
-	local tile = Tile(position)
-	local teleport = tile and tile:getItemByType(ITEM_TYPE_TELEPORT)
-	if teleport then
-		local split = param:split(",") -- Split always return a table, even if it's empty
-		if #split ~= 3 then
-			player:sendCancelMessage('You need to declare the X, Y of Z of destination. Please use "/teleport X, Y, Z".')
-			return true
-		else
-			local destPosition = Position(split[1], split[2], split[3])
-			if destPosition and destPosition:getTile() then
-				teleport:setDestination(destPosition)
+	local params = param:split(",")
+	if params[3] then
+		local position = player:getPosition()
+		position:getNextPosition(player:getDirection(), 1)
+		local destination = Position(params[1], params[2], params[3])
+		if destination and destination:getTile() then
+			local tp = Game.createItem(35502, 1, position)
+			if tp then
+				tp:setDestination(destination)
 				player:sendTextMessage(MESSAGE_EVENT_ADVANCE, string.format("New position: %s", param))
-			else
-				player:sendCancelMessage("Destination position is not valid.")
-				return true
 			end
+		else
+			player:sendCancelMessage("Destination position is not valid.")
 		end
 	else
-		player:sendCancelMessage("The item is not a teleport type")
-		return true
+		player:sendCancelMessage('You need to declare the X, Y of Z of destination. Please use "/teleport X, Y, Z".')
 	end
 	return true
 end

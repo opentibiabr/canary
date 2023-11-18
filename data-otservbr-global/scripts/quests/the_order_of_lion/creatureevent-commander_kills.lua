264efd5352097ce330d80024121399c4dd2666e3
Luan Luciano <luanluciano@outlook.com>
2023-11-17 21:19:02 -0300

    fix: Drume boss mechanic (#1836)
    - Fail the encounter if Lion commanders die
    - Kesar is invulnerable

--- data-otservbr-global/scripts/quests/the_order_of_lion/creatureevent-commander_kills.lua
+++ data-otservbr-global/scripts/quests/the_order_of_lion/creatureevent-commander_kills.lua
@@ -1,25 +1,23 @@
 local config = {
-	firstPlayerPosition = Position(32457, 32508, 6),
 	centerPosition = Position(32439, 32523, 7), -- Center Room
 	exitPosition = Position(32453, 32503, 7), -- Exit Position
-	newPosition = Position(32453, 32510, 7),
 	rangeX = 22,
 	rangeY = 16,
 }
 
-local lionCommanderDeath = CreatureEvent("lionCommanderDeath")
+local lionCommanderDeath = CreatureEvent("LionCommanderDeath")
 function lionCommanderDeath.onPrepareDeath(creature)
-	local totalCommanders = Game.getStorageValue(Storage.TheOrderOfTheLion.Drume.TotalLionCommanders)
+	local totalCommanders = Game.getStorageValue(GlobalStorage.TheOrderOfTheLion.Drume.TotalLionCommanders)
 	if totalCommanders > 1 then
-		Game.setStorageValue(Storage.TheOrderOfTheLion.Drume.TotalLionCommanders, totalCommanders - 1)
+		Game.setStorageValue(GlobalStorage.TheOrderOfTheLion.Drume.TotalLionCommanders, totalCommanders - 1)
 	else
 		local spectators = Game.getSpectators(config.centerPosition, false, false, config.rangeX, config.rangeX, config.rangeY, config.rangeY)
-		for _, cid in pairs(spectators) do
-			if cid:isMonster() and not cid:getMaster() then
-				cid:remove()
-			elseif cid:isPlayer() then
-				cid:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You lost the skirmish.")
-				cid:teleportTo(config.exitPosition)
+		for _, spectator in pairs(spectators) do
+			if spectator:isMonster() and not spectator:getMaster() then
+				spectator:remove()
+			elseif spectator:isPlayer() then
+				spectator:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You lost the skirmish.")
+				spectator:teleportTo(config.exitPosition)
 			end
 		end
 		config.exitPosition:sendMagicEffect(CONST_ME_TELEPORT)
@@ -29,15 +27,24 @@ end
 
 lionCommanderDeath:register()
 
-local usurperCommanderDeath = CreatureEvent("usurperCommanderDeath")
+local usurperCommanderDeath = CreatureEvent("UsurperCommanderDeath")
 function usurperCommanderDeath.onPrepareDeath(creature)
-	local totalCommanders = Game.getStorageValue(Storage.TheOrderOfTheLion.Drume.TotalUsurperCommanders)
-	Game.setStorageValue(Storage.TheOrderOfTheLion.Drume.TotalUsurperCommanders, totalCommanders - 1)
-	if totalCommanders == 1 then
-		Game.createMonster("Kesar", Position(32444, 32515, 7), false, true)
-		Game.createMonster("Drume", Position(32444, 32516, 7), false, true)
+	local totalCommanders = Game.getStorageValue(GlobalStorage.TheOrderOfTheLion.Drume.TotalUsurperCommanders)
+	if totalCommanders > 0 then
+		Game.setStorageValue(GlobalStorage.TheOrderOfTheLion.Drume.TotalUsurperCommanders, totalCommanders - 1)
+		if totalCommanders == 1 then
+			Game.createMonster("Kesar", Position(32444, 32515, 7), false, true)
+			Game.createMonster("Drume", Position(32444, 32516, 7), false, true)
+		end
 	end
 	return true
 end
 
 usurperCommanderDeath:register()
+
+local kesarHealthChange = CreatureEvent("KesarImmortal")
+function kesarHealthChange.onHealthChange(creature, attacker, primaryDamage, primaryType, secondaryDamage, secondaryType, origin)
+	return 0, 0, 0, 0
+end
+
+kesarHealthChange:register()

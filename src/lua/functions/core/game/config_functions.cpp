ab43bfbb0a1cf85da0d9055c819bb904478456ff
Beats <daniel15042014@hotmail.com>
2023-11-22 21:05:55 -0300

    feat: rework on configmanager system

--- src/lua/functions/core/game/config_functions.cpp
+++ src/lua/functions/core/game/config_functions.cpp
@@ -14,10 +14,10 @@
 
 void ConfigFunctions::init(lua_State* L) {
 	registerTable(L, "configManager");
-	registerMethod(L, "configManager", "getString", ConfigFunctions::luaConfigManagerGetString);
-	registerMethod(L, "configManager", "getNumber", ConfigFunctions::luaConfigManagerGetNumber);
-	registerMethod(L, "configManager", "getBoolean", ConfigFunctions::luaConfigManagerGetBoolean);
-	registerMethod(L, "configManager", "getFloat", ConfigFunctions::luaConfigManagerGetFloat);
+	registerMethod(L, "configManager", "getString", luaConfigManagerGetString);
+	registerMethod(L, "configManager", "getNumber", luaConfigManagerGetNumber);
+	registerMethod(L, "configManager", "getBoolean", luaConfigManagerGetBoolean);
+	registerMethod(L, "configManager", "getFloat", luaConfigManagerGetFloat);
 
 #define registerEnumIn(L, tableName, value)                                                     \
 	do {                                                                                        \
@@ -258,21 +258,21 @@ void ConfigFunctions::init(lua_State* L) {
 }
 
 int ConfigFunctions::luaConfigManagerGetString(lua_State* L) {
-	pushString(L, g_configManager().getString(getNumber<stringConfig_t>(L, -1)));
+	pushString(L, g_configManager().getString(getNumber<ConfigKey>(L, -1)));
 	return 1;
 }
 
 int ConfigFunctions::luaConfigManagerGetNumber(lua_State* L) {
-	lua_pushnumber(L, g_configManager().getNumber(getNumber<integerConfig_t>(L, -1)));
+	lua_pushnumber(L, g_configManager().getNumber(getNumber<ConfigKey>(L, -1)));
 	return 1;
 }
 
 int ConfigFunctions::luaConfigManagerGetBoolean(lua_State* L) {
-	pushBoolean(L, g_configManager().getBoolean(getNumber<booleanConfig_t>(L, -1)));
+	pushBoolean(L, g_configManager().getBoolean(getNumber<ConfigKey>(L, -1)));
 	return 1;
 }
 
 int ConfigFunctions::luaConfigManagerGetFloat(lua_State* L) {
-	lua_pushnumber(L, g_configManager().getFloat(getNumber<floatingConfig_t>(L, -1)));
+	lua_pushnumber(L, g_configManager().getFloat(getNumber<ConfigKey>(L, -1)));
 	return 1;
 }

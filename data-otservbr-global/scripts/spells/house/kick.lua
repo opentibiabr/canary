9311fe706a76d9f47f598bbb9cd225a17b243dad
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2023-11-25 06:14:12 -0300

    fix: comment

--- data-otservbr-global/scripts/spells/house/kick.lua
+++ data-otservbr-global/scripts/spells/house/kick.lua
@@ -4,6 +4,7 @@ function spell.onCastSpell(player, variant)
 	local targetPlayer = Player(variant:getString()) or player
 	local guest = targetPlayer:getTile():getHouse()
 	local owner = player:getTile():getHouse()
+	-- Owner kick yourself from house
 	if targetPlayer == player then
 		player:getPosition():sendMagicEffect(CONST_ME_POFF)
 		player:teleportTo(owner:getExitPosition())

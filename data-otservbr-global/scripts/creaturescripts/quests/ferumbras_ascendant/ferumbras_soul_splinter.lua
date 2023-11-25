0edd09adf075d30ba239ad2003a7103ecd4a7d29
Elson Costa <elsongabriel@hotmail.com>
2023-11-25 09:10:57 -0300

    Edu suggestion.
    Co-authored-by: Eduardo Dantas <eduardo.dantas@hotmail.com.br>

--- data-otservbr-global/scripts/creaturescripts/quests/ferumbras_ascendant/ferumbras_soul_splinter.lua
+++ data-otservbr-global/scripts/creaturescripts/quests/ferumbras_ascendant/ferumbras_soul_splinter.lua
@@ -1,7 +1,11 @@
 local ferumbrasSoulSplinter = CreatureEvent("FerumbrasSoulSplinterDeath")
 
 function ferumbrasSoulSplinter.onDeath(creature, corpse, lasthitkiller, mostdamagekiller, lasthitunjustified, mostdamageunjustified)
-	Game.createMonster("ferumbras essence", creature:getPosition(), true, true)
+	local monster = Game.createMonster("ferumbras essence", creature:getPosition(), true, true)
+	if not monster then
+		logger.error("[ferumbrasSoulSplinter.onDeath] cannot create monster on position {}", creature:getPosition():toString())
+		return true
+	end
 	return true
 end
 

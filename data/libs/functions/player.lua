0baea211c0df5d4e04da7811eea2f8993eceb9ff
Luan Luciano <luanluciano@outlook.com>
2023-12-24 15:28:13 -0300

    Update player.lua

--- data/libs/functions/player.lua
+++ data/libs/functions/player.lua
@@ -622,16 +622,17 @@ function Player:calculateLootFactor(monster)
 	}
 end
 
-function Player:setExhaustion(key, seconds)
-	return self:setStorageValue(key, os.time() + seconds)
+function Player:setExhaustion(scope, seconds)
+	return self:kv():set(scope, os.time() + seconds)
 end
 
-function Player:getExhaustion(key)
-	return math.max(self:getStorageValue(key) - os.time(), 0)
+function Player:getExhaustion(scope)
+	local exhaustionKV = self:kv():get(scope) or 0
+	return math.max(exhaustionKV - os.time(), 0)
 end
 
-function Player:hasExhaustion(key)
-	return self:getExhaustion(key) > 0 and true or false
+function Player:hasExhaustion(scope)
+	return self:getExhaustion(scope) > 0 and true or false
 end
 
 function Player:setFiendish()

c4284e242ae294ccb70bbaec0d3f569da8830d71
Luan Santos <git@luan.sh>
2023-12-28 10:50:22 -0800

    feat: missing creatures and bosses (#2004)
    Adds a chunk of missing content such as "The Brain Head", "The Monster"
    (no lever yet, that's in another PR) and Oskayaat creatures along with a
    few other fixes to creature scripts/spells.
    
    ---------
    
    Co-authored-by: Elson Costa <elsongabriel@hotmail.com>

---
+++ data-otservbr-global/scripts/spells/monster/white_weretiger_ice_ring.lua
@@ -0,0 +1,78 @@
+local arrLarge = {
+	{ 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+	{ 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0 },
+	{ 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0 },
+	{ 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0 },
+	{ 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1 },
+	{ 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1 },
+	{ 1, 1, 0, 0, 0, 0, 3, 0, 0, 0, 0, 1, 1 },
+	{ 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1 },
+	{ 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1 },
+	{ 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0 },
+	{ 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0 },
+	{ 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0 },
+	{ 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+}
+
+local arrMedium = {
+	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+	{ 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0 },
+	{ 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0 },
+	{ 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0 },
+	{ 0, 0, 1, 1, 0, 0, 3, 0, 0, 1, 1, 0, 0 },
+	{ 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0 },
+	{ 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0 },
+	{ 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0 },
+	{ 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+}
+
+local arrSmall = {
+	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+	{ 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0 },
+	{ 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0 },
+	{ 0, 0, 0, 1, 1, 0, 3, 0, 1, 1, 0, 0, 0 },
+	{ 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0 },
+	{ 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0 },
+	{ 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+}
+
+local combatSmallRing = Combat()
+combatSmallRing:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
+combatSmallRing:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICEAREA)
+combatSmallRing:setArea(createCombatArea(arrSmall))
+
+local combatMediumRing = Combat()
+combatMediumRing:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
+combatMediumRing:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICEAREA)
+combatMediumRing:setArea(createCombatArea(arrMedium))
+
+local combatLargeRing = Combat()
+combatLargeRing:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
+combatLargeRing:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICEAREA)
+combatLargeRing:setArea(createCombatArea(arrLarge))
+
+local spell = Spell("instant")
+
+local combats = { combatSmallRing, combatMediumRing, combatLargeRing }
+
+function spell.onCastSpell(creature, var)
+	local randomCombat = combats[math.random(#combats)]
+	return randomCombat:execute(creature, var)
+end
+
+spell:name("white weretiger ice ring")
+spell:words("###6053")
+spell:needLearn(true)
+spell:cooldown("2000")
+spell:isSelfTarget(true)
+spell:register()

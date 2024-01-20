6b023ab83f81d4f1cba44be8d07f5ec36775e560
Luan Luciano <luanluciano@outlook.com>
2024-01-20 01:04:43 -0300

    Update rupture_heal.lua

--- data-otservbr-global/scripts/creaturescripts/quests/heart_of_destruction/rupture_heal.lua
+++ data-otservbr-global/scripts/creaturescripts/quests/heart_of_destruction/rupture_heal.lua
@@ -1,7 +1,13 @@
 local ruptureHeal = CreatureEvent("RuptureHeal")
+
 function ruptureHeal.onHealthChange(creature, attacker, primaryDamage, primaryType, secondaryDamage, secondaryType, origin)
 	local healthGain = math.random(5000, 10000)
-	if attacker and attacker:isPlayer() and resonanceActive == true then
+	local resonanceActive = Game.getStorageValue(GlobalStorage.HeartOfDestruction.RuptureResonanceActive)
+	if resonanceActive and resonanceActive < 1 then
+		resonanceActive = false
+	end
+
+	if attacker and attacker:isPlayer() and resonanceActive then
 		creature:addHealth(healthGain)
 		creature:getPosition():sendMagicEffect(CONST_ME_MAGIC_GREEN)
 	end

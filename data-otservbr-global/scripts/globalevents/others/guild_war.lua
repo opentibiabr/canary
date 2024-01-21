26c2236d82a92cf044fc7b08d1cda642c6dca26b
Luan Luciano <luanluciano@outlook.com>
2024-01-21 11:16:52 -0300

    Update guild_war.lua

--- data-otservbr-global/scripts/globalevents/others/guild_war.lua
+++ data-otservbr-global/scripts/globalevents/others/guild_war.lua
@@ -2,7 +2,7 @@ local guildWar = GlobalEvent("guildwar")
 
 function guildWar.onThink(interval)
 	local time = os.time()
-	db.query("UPDATE `guild_wars` SET `status` = 4 WHERE `status` = 1 AND `ended` < " .. time)
+	db.query("UPDATE `guild_wars` SET `status` = 4 WHERE `status` = 1 AND `ended` != 0 AND `ended` < " .. time)
 	return true
 end
 

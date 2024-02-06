563f15fb051150ee7d4fa540b11378d5eaef679a
Marco <marcomoa@hotmail.com>
2024-02-06 04:33:27 -0300

    refactor: enhance and organize the server initialization script

--- data-otservbr-global/scripts/globalevents/others/startup.lua
+++ data-otservbr-global/scripts/globalevents/others/startup.lua
@@ -74,10 +74,6 @@ function serverstartup.onStartup()
 	-- reset storages and allow purchase of boost in the store
 	db.query("UPDATE `player_storage` SET `value` = 0 WHERE `player_storage`.`key` = 51052")
 
-	-- reset familiars message storage
-	db.query("DELETE FROM `player_storage` WHERE `key` = " .. Global.Storage.FamiliarSummonEvent10)
-	db.query("DELETE FROM `player_storage` WHERE `key` = " .. Global.Storage.FamiliarSummonEvent60)
-
 	-- delete canceled and rejected guilds
 	db.asyncQuery("DELETE FROM `guild_wars` WHERE `status` = 2")
 	db.asyncQuery("DELETE FROM `guild_wars` WHERE `status` = 3")
@@ -90,85 +86,11 @@ function serverstartup.onStartup()
 	db.asyncQuery("DELETE FROM `market_history` WHERE `inserted` <= \z
 	" .. (time - configManager.getNumber(configKeys.MARKET_OFFER_DURATION)))
 
-	-- Move expired bans to ban history
-	local banResultId = db.storeQuery("SELECT * FROM `account_bans` WHERE `expires_at` != 0 AND `expires_at` <= " .. time)
-	if banResultId ~= false then
-		repeat
-			local accountId = Result.getNumber(banResultId, "account_id")
-			db.asyncQuery("INSERT INTO `account_ban_history` (`account_id`, `reason`, `banned_at`, \z
-			`expired_at`, `banned_by`) VALUES (" .. accountId .. ", \z
-			" .. db.escapeString(Result.getString(banResultId, "reason")) .. ", \z
-			" .. Result.getNumber(banResultId, "banned_at") .. ", " .. Result.getNumber(banResultId, "expires_at") .. ", \z
-			" .. Result.getNumber(banResultId, "banned_by") .. ")")
-			db.asyncQuery("DELETE FROM `account_bans` WHERE `account_id` = " .. accountId)
-		until not Result.next(banResultId)
-		Result.free(banResultId)
-	end
-
 	-- Ferumbras Ascendant quest
 	for i = 1, #GlobalStorage.FerumbrasAscendant.Habitats do
 		local storage = GlobalStorage.FerumbrasAscendant.Habitats[i]
 		Game.setStorageValue(storage, 0)
 	end
-
-	-- Check house auctions
-	local resultId = db.storeQuery("SELECT `id`, `highest_bidder`, `last_bid`, (SELECT `balance` FROM \z
-	`players` WHERE `players`.`id` = `highest_bidder`) AS `balance` FROM `houses` WHERE `owner` = 0 AND \z
-	`bid_end` != 0 AND `bid_end` < " .. time)
-	if resultId ~= false then
-		repeat
-			local house = House(Result.getNumber(resultId, "id"))
-			if house then
-				local highestBidder = Result.getNumber(resultId, "highest_bidder")
-				local balance = Result.getNumber(resultId, "balance")
-				local lastBid = Result.getNumber(resultId, "last_bid")
-				if balance >= lastBid then
-					db.query("UPDATE `players` SET `balance` = " .. (balance - lastBid) .. " WHERE `id` = " .. highestBidder)
-					house:setOwnerGuid(highestBidder)
-				end
-				db.asyncQuery("UPDATE `houses` SET `last_bid` = 0, `bid_end` = 0, `highest_bidder` = 0, \z
-				`bid` = 0 WHERE `id` = " .. house:getId())
-			end
-		until not Result.next(resultId)
-		Result.free(resultId)
-	end
-
-	-- store towns in database
-	db.query("TRUNCATE TABLE `towns`")
-	for i, town in ipairs(Game.getTowns()) do
-		local position = town:getTemplePosition()
-		db.query("INSERT INTO `towns` (`id`, `name`, `posx`, `posy`, `posz`) VALUES (" .. town:getId() .. ", " .. db.escapeString(town:getName()) .. ", " .. position.x .. ", " .. position.y .. ", " .. position.z .. ")")
-	end
-
-	do
-		-- Event Schedule rates
-		local lootRate = EventsScheduler.getEventSLoot()
-		if lootRate ~= 100 then
-			SCHEDULE_LOOT_RATE = lootRate
-		end
-
-		local expRate = EventsScheduler.getEventSExp()
-		if expRate ~= 100 then
-			SCHEDULE_EXP_RATE = expRate
-		end
-
-		local skillRate = EventsScheduler.getEventSSkill()
-		if skillRate ~= 100 then
-			SCHEDULE_SKILL_RATE = skillRate
-		end
-
-		local spawnRate = EventsScheduler.getSpawnMonsterSchedule()
-		if spawnRate ~= 100 then
-			SCHEDULE_SPAWN_RATE = spawnRate
-		end
-
-		if expRate ~= 100 or lootRate ~= 100 or spawnRate ~= 100 or skillRate ~= 100 then
-			logger.info("[Events] Exp: {}%, loot: {}%, Spawn: {}%, Skill: {}%", expRate, lootRate, spawnRate, skillRate)
-		end
-	end
-
-	-- Hireling System
-	HirelingsInit()
 end
 
 serverstartup:register()
--- data/scripts/globalevents/startup.lua
+++ data/scripts/globalevents/startup.lua
@@ -1,3 +1,57 @@
+-- Function to reset familiars message storage
+local function resetFamiliarsMessageStorage()
+	db.query("DELETE FROM `player_storage` WHERE `key` = " .. Global.Storage.FamiliarSummonEvent10)
+	db.query("DELETE FROM `player_storage` WHERE `key` = " .. Global.Storage.FamiliarSummonEvent60)
+end
+
+-- Function to move expired bans to ban history
+local function moveExpiredBansToHistory()
+	local resultId = db.storeQuery("SELECT * FROM `account_bans` WHERE `expires_at` != 0 AND `expires_at` <= " .. os.time())
+	if resultId then
+			repeat
+					local accountId = Result.getNumber(resultId, "account_id")
+					db.asyncQuery("INSERT INTO `account_ban_history` (`account_id`, `reason`, `banned_at`, `expired_at`, `banned_by`) VALUES (" .. accountId .. ", " .. db.escapeString(Result.getString(resultId, "reason")) .. ", " .. Result.getNumber(resultId, "banned_at") .. ", " .. Result.getNumber(resultId, "expires_at") .. ", " .. Result.getNumber(resultId, "banned_by") .. ")")
+					db.asyncQuery("DELETE FROM `account_bans` WHERE `account_id` = " .. accountId)
+			until not Result.next(resultId)
+
+			Result.free(resultId)
+	end
+end
+
+-- Function to check and process house auctions
+local function processHouseAuctions()
+	local currentTime = os.time()
+	local resultId = db.storeQuery("SELECT `id`, `highest_bidder`, `last_bid`, " .. "(SELECT `balance` FROM `players` WHERE `players`.`id` = `highest_bidder`) AS `balance` " .. "FROM `houses` WHERE `owner` = 0 AND `bid_end` != 0 AND `bid_end` < " .. currentTime)
+	if resultId then
+			repeat
+					local house = House(Result.getNumber(resultId, "id"))
+					if house then
+							local highestBidder = Result.getNumber(resultId, "highest_bidder")
+							local balance = Result.getNumber(resultId, "balance")
+							local lastBid = Result.getNumber(resultId, "last_bid")
+							if balance >= lastBid then
+									db.query("UPDATE `players` SET `balance` = " .. (balance - lastBid) .. " WHERE `id` = " .. highestBidder)
+									house:setOwnerGuid(highestBidder)
+							end
+
+							db.asyncQuery("UPDATE `houses` SET `last_bid` = 0, `bid_end` = 0, `highest_bidder` = 0, `bid` = 0 " .. "WHERE `id` = " .. house:getId())
+					end
+			until not Result.next(resultId)
+
+			Result.free(resultId)
+	end
+end
+
+-- Function to store towns in the database
+local function storeTownsInDatabase()
+	db.query("TRUNCATE TABLE `towns`")
+
+	for i, town in ipairs(Game.getTowns()) do
+			local position = town:getTemplePosition()
+			db.query("INSERT INTO `towns` (`id`, `name`, `posx`, `posy`, `posz`) VALUES (" .. town:getId() .. ", " .. db.escapeString(town:getName()) .. ", " .. position.x .. ", " .. position.y .. ", " .. position.z .. ")")
+	end
+end
+
 -- Function to check duplicated variable keys and log the results
 local function checkAndLogDuplicateKeys(variableNames)
 	for _, variableName in ipairs(variableNames) do
@@ -11,12 +65,57 @@ local function checkAndLogDuplicateKeys(variableNames)
 	end
 end
 
-local startup = GlobalEvent("Startup")
+-- Function to update event rates based on EventsScheduler data
+local function updateEventRates()
+	local lootRate = EventsScheduler.getEventSLoot()
+	if lootRate ~= 100 then
+			SCHEDULE_LOOT_RATE = lootRate
+	end
+
+	local expRate = EventsScheduler.getEventSExp()
+	if expRate ~= 100 then
+			SCHEDULE_EXP_RATE = expRate
+	end
+
+	local skillRate = EventsScheduler.getEventSSkill()
+	if skillRate ~= 100 then
+			SCHEDULE_SKILL_RATE = skillRate
+	end
+
+	local spawnRate = EventsScheduler.getSpawnMonsterSchedule()
+	if spawnRate ~= 100 then
+			SCHEDULE_SPAWN_RATE = spawnRate
+	end
+
+	-- Log information if any of the rates are not 100%
+	if expRate ~= 100 or lootRate ~= 100 or spawnRate ~= 100 or skillRate ~= 100 then
+			logger.info("[Events] Exp: {}%, loot: {}%, Spawn: {}%, Skill: {}%", expRate, lootRate, spawnRate, skillRate)
+	end
+end
+
+local startup = GlobalEvent("Server Initialization")
 
 function startup.onStartup()
+	-- Call the function to reset familiars message storage
+	resetFamiliarsMessageStorage()
+
+	-- Call the function to move expired bans to ban history
+	moveExpiredBansToHistory()
+
+	-- Call the function to check and process house auctions
+	processHouseAuctions()
+
+	-- Call the function to store towns in the database
+	storeTownsInDatabase()
+
 	-- Call the checkAndLogDuplicateKeys function with the list of variable names
-	local variableNames = {"Global", "GlobalStorage", "Storage"}
-	checkAndLogDuplicateKeys(variableNames)
+	checkAndLogDuplicateKeys({"Global", "GlobalStorage", "Storage"})
+
+	-- Call the function to update event rates
+	updateEventRates()
+
+	-- Hireling System
+	HirelingsInit()
 end
 
 startup:register()

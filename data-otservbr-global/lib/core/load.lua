8ad2153e40f6b0c6f2870f1f8e53a4bd7abf83c3
Marco <marcomoa@hotmail.com>
2024-02-06 16:33:24 -0300

    improvements: reward bags

--- data-otservbr-global/lib/core/load.lua
+++ data-otservbr-global/lib/core/load.lua
@@ -1,2 +1,5 @@
+-- Note: The library of storages must be loaded previously to the other libraries.
 dofile(DATA_DIRECTORY .. "/lib/core/storages.lua")
+
+dofile(DATA_DIRECTORY .. "/lib/core/constans.lua")
 dofile(DATA_DIRECTORY .. "/lib/core/quests.lua")

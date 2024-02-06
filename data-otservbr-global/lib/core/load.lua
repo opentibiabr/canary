9c54dd90bf228d6af5cfdfac8d0e5838f87f0581
Marco <marcomoa@hotmail.com>
2024-02-06 17:12:58 -0300

    small fixes

--- data-otservbr-global/lib/core/load.lua
+++ data-otservbr-global/lib/core/load.lua
@@ -1,5 +1,2 @@
--- Note: The library of storages must be loaded previously to the other libraries.
 dofile(DATA_DIRECTORY .. "/lib/core/storages.lua")
-
-dofile(DATA_DIRECTORY .. "/lib/core/constans.lua")
 dofile(DATA_DIRECTORY .. "/lib/core/quests.lua")

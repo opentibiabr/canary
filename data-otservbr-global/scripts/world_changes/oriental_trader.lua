49e3f05e4295bf92f362958fd063d619754aaf15
Marco <marcomoa@hotmail.com>
2024-02-08 01:17:16 -0300

    fix: oriental trader

--- data-otservbr-global/scripts/world_changes/oriental_trader.lua
+++ data-otservbr-global/scripts/world_changes/oriental_trader.lua
@@ -1,69 +1,44 @@
-local config = {
-	[1] = {
-		removeItems = {
-			{ position = Position(33096, 32882, 6), itemId = 4977 },
-			{ position = Position(33096, 32883, 6), itemId = 4977 },
-			{ position = Position(33096, 32883, 6), itemId = 4920 },
-			{ position = Position(33096, 32884, 6), itemId = 4920 },
-			{ position = Position(33096, 32885, 6), itemId = 4920 },
-		},
-
-		yasirPosition = Position(33102, 32884, 6),
-		mapName = "Ankrahmun",
-	},
-
-	[2] = {
-		yasirPosition = Position(32400, 31815, 6),
-		mapName = "Carlin",
-	},
-
-	[3] = {
-		yasirPosition = Position(32314, 32895, 6),
-		mapName = "Liberty Bay",
-	},
-}
-
-local function yasirwebhook(message)
-	Webhook.sendMessage(":man_wearing_turban_tone4: " .. message, announcementChannels["serverAnnouncements"])
+if npc then
+	npc:setMasterPos(position)
 end
-
-local function removeItems(removeItems)
-	for _, removeItem in pairs(removeItems) do
-		local tile = Tile(removeItem.position)
-		if tile then
-			local item = tile:getItemById(removeItem.itemId)
-			if item then
-				item:remove()
-			end
-		end
-	end
-end
-
-local function spawnYasir(position)
-	local npc = Game.createNpc("yasir", position)
-	if npc then
-		npc:setMasterPos(position)
-	end
 end
 
 local orientalTrader = GlobalEvent("Oriental Trader")
 
 function orientalTrader.onStartup()
-	local randTown = config[math.random(#config)]
-	logger.info("[World Change] Yasir has arrived in {} today!", randTown.mapName)
+local message = "Yasir: not spawned today"
+
+if config.enableSpawn and math.random(100) <= config.spawnChance then
+	local randTown = config.towns[math.random(#config.towns)]
+	if not randTown then
+		return false
+	end
 
 	if randTown.removeItems then
-		removeItems(randTown.removeItems) -- Use the function to remove items
+		for i = 1, #randTown.removeItems do
+			local tile = Tile(randTown.removeItems[i].position)
+			if tile then
+				local item = tile:getItemById(randTown.removeItems[i].itemId)
+				if item then
+					item:remove()
+				end
+			end
+		end
 	end
 
 	local mapName = string.removeAllSpaces(randTown.mapName):lower()
 	Game.loadMap(DATA_DIRECTORY .. "/world/world_changes/oriental_trader/" .. mapName .. ".otbm")
 
-	local message = string.format("[World Change] Yasir has arrived in %s today!", randTown.mapName)
-	addEvent(yasirwebhook, 60000, message)
+	message = string.format("[World Change] Yasir has arrived in %s today!", randTown.mapName)
 	addEvent(spawnYasir, 60000, randTown.yasirPosition)
 
+	logger.info(message)
 	Game.setStorageValue(GlobalStorage.Yasir, 1)
+else
+	logger.info("Yasir: not this time")
+	Game.setStorageValue(GlobalStorage.Yasir, -1)
+end
+addEvent(yasirwebhook, 60000, message)
 end
 
 orientalTrader:register()

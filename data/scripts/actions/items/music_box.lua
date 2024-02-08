f8f7d5e3e7d9586ffd1e07bb8ac9931217459df5
Marco <marcomoa@hotmail.com>
2024-02-08 17:28:57 -0300

    code format

--- data/scripts/actions/items/music_box.lua
+++ data/scripts/actions/items/music_box.lua
@@ -1,13 +1,58 @@
 local config = {
-	["dragonling"] = { mountId = 31, achievement = "Dragon Mimicry", tameMessage = "The wild dragonling will accompany you as a friend from now on.", sound = "FI?" },
-	["draptor"] = { mountId = 6, achievement = "Scales and Tail", tameMessage = "The wild draptor will accompany you as a friend from now on.", sound = "Screeeeeeeeech" },
-	["enraged white deer"] = { mountId = 18, achievement = "Friend of Elves", tameMessage = "The wild deer will accompany you as a friend from now on.", sound = "*bell*" },
-	["ironblight"] = { mountId = 29, achievement = "Magnetised", tameMessage = "The ironblight will accompany you as a friend from now on.", sound = "Plinngggg" },
-	["magma crawler"] = { mountId = 30, achievement = "Way to Hell", tameMessage = "The magma crawler will accompany you as a friend from now on.", sound = "ZzzZzzZzzzZz" },
-	["midnight panther"] = { mountId = 5, achievement = "Starless Night", tameMessage = "The wild panther will accompany you as a friend from now on.", sound = "Purrrrrrr" },
-	["wailing widow"] = { mountId = 1, achievement = "Spin-Off", tameMessage = "You have tamed the wailing widow.", sound = "Sssssssss" },
-	["wild horse"] = { mountId = 17, achievement = "Lucky Horseshoe", tameMessage = "The horse accepts you as its new master.", sound = "*snort*" },
-	["panda"] = { mountId = 19, achievement = "Chequered Teddy", tameMessage = "The panda will accompany you as a friend from now on.", sound = "Rrrrr... Grrrr" },
+	["dragonling"] = {
+			mountId = 31,
+			achievement = "Dragon Mimicry",
+			tameMessage = "The wild dragonling will accompany you as a friend from now on.",
+			sound = "FI?"
+	},
+	["draptor"] = {
+			mountId = 6,
+			achievement = "Scales and Tail",
+			tameMessage = "The wild draptor will accompany you as a friend from now on.",
+			sound = "Screeeeeeeeech"
+	},
+	["enraged white deer"] = {
+			mountId = 18,
+			achievement = "Friend of Elves",
+			tameMessage = "The wild deer will accompany you as a friend from now on.",
+			sound = "*bell*"
+	},
+	["ironblight"] = {
+			mountId = 29,
+			achievement = "Magnetised",
+			tameMessage = "The ironblight will accompany you as a friend from now on.",
+			sound = "Plinngggg"
+	},
+	["magma crawler"] = {
+			mountId = 30,
+			achievement = "Way to Hell",
+			tameMessage = "The magma crawler will accompany you as a friend from now on.",
+			sound = "ZzzZzzZzzzZz"
+	},
+	["midnight panther"] = {
+			mountId = 5,
+			achievement = "Starless Night",
+			tameMessage = "The wild panther will accompany you as a friend from now on.",
+			sound = "Purrrrrrr"
+	},
+	["wailing widow"] = {
+			mountId = 1,
+			achievement = "Spin-Off",
+			tameMessage = "You have tamed the wailing widow.",
+			sound = "Sssssssss"
+	},
+	["wild horse"] = {
+			mountId = 17,
+			achievement = "Lucky Horseshoe",
+			tameMessage = "The horse accepts you as its new master.",
+			sound = "*snort*"
+	},
+	["panda"] = {
+			mountId = 19,
+			achievement = "Chequered Teddy",
+			tameMessage = "The panda will accompany you as a friend from now on.",
+			sound = "Rrrrr... Grrrr"
+	}
 }
 
 local musicBox = Action()

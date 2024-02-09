11e56dfd39cdb80612ab58e8b4df30178494aa14
Elson Costa <elsongabriel@hotmail.com>
2024-02-09 14:08:07 -0300

    moved and improved remove empty parcels script.

---
+++ data/scripts/creaturescripts/others/remove_empty_parcel.lua
@@ -0,0 +1,25 @@
+local removeEmptyParcelsEvent = CreatureEvent("RemoveEmptyParcelsOnLogin")
+
+function removeEmptyParcelsEvent.onLogin(player)
+	local emptyParcelsToRemove = {}
+	local removed = false
+	local countRemovedParcels = 0
+	for _, parcel in ipairs(player:getStoreInbox():getItems(true)) do
+		if parcel:getId() == PARCEL_STAMPED and parcel:getEmptySlots() == 10 then
+			emptyParcelsToRemove[parcel] = parcel
+			countRemovedParcels = countRemovedParcels + 1
+		end
+	end
+
+	for _, parcel in pairs(emptyParcelsToRemove) do
+		parcel:remove()
+		removed = true
+	end
+
+	if removed then
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, countRemovedParcels .. " empty parcels were removed from your store inbox!")
+	end
+	return true
+end
+
+removeEmptyParcelsEvent:register()

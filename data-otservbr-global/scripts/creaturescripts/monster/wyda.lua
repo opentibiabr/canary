085e3996c27c361bea728a6de81f37949853db3a
Jonyrewind <jonyrewind@gmail.com>
2024-02-10 22:03:30 +0100

    Update wyda.lua

--- data-otservbr-global/scripts/creaturescripts/monster/wyda.lua
+++ data-otservbr-global/scripts/creaturescripts/monster/wyda.lua
@@ -1,5 +1,8 @@
 local wyda = CreatureEvent("Wyda")
 function wyda.onDeath(creature, corpse, killer, mostDamage, unjustified, mostDamage_unjustified)
+	if killer:isPlayer() then
+		killer:addAchievement("Someone's Bored")
+	end
 	creature:say("It seems this was just an illusion.", TALKTYPE_ORANGE_1)
 end
 

5a47bc87baaa2d8f4a084963983cc2f4bd329e54
Aerwix <dekarzfajardo@gmail.com>
2024-02-11 18:03:30 -0600

    Nuevos Exp/Skill/Magic Rates

--- data/stages.lua
+++ data/stages.lua
@@ -4,53 +4,88 @@
 experienceStages = {
 	{
 		minlevel = 1,
-		maxlevel = 8,
-		multiplier = 7,
+		maxlevel = 24,
+		multiplier = 100,
 	},
 	{
-		minlevel = 9,
-		maxlevel = 20,
-		multiplier = 6,
+		minlevel = 25,
+		maxlevel = 49,
+		multiplier = 90,
 	},
 	{
-		minlevel = 21,
-		maxlevel = 50,
+		minlevel = 50,
+		maxlevel = 74,
+		multiplier = 80,
+	},
+	{
+		minlevel = 75,
+		maxlevel = 99,
+		multiplier = 70,
+	},
+	{
+		minlevel = 100,
+		maxlevel = 124,
+		multiplier = 60,
+	},
+	{
+		minlevel = 125,
+		maxlevel = 149,
+		multiplier = 50,
+	},
+	{
+		minlevel = 150,
+		maxlevel = 174,
+		multiplier = 40,
+	},
+	{
+		minlevel = 175,
+		maxlevel = 199,
+		multiplier = 30,
+	},
+	{
+		minlevel = 200,
+		maxlevel = 224,
+		multiplier = 20,
+	},
+	{
+		minlevel = 225,
+		maxlevel = 249,
+		multiplier = 10,
+	},
+	{
+		minlevel = 250,
 		multiplier = 5,
-	},
-	{
-		minlevel = 51,
-		maxlevel = 100,
-		multiplier = 4,
-	},
-	{
-		minlevel = 101,
-		multiplier = 2,
 	},
 }
 
 skillsStages = {
 	{
 		minlevel = 10,
-		maxlevel = 60,
+		maxlevel = 89,
+		multiplier = 25,
+	},
+	{
+		minlevel = 90,
+		maxlevel = 99,
+		multiplier = 20,
+	},
+	{
+		minlevel = 100,
+		maxlevel = 109,
 		multiplier = 15,
 	},
 	{
-		minlevel = 61,
-		maxlevel = 80,
+		minlevel = 110,
+		maxlevel = 119,
 		multiplier = 10,
 	},
 	{
-		minlevel = 81,
-		maxlevel = 110,
-		multiplier = 6,
+		minlevel = 120,
+		maxlevel = 129,
+		multiplier = 5,
 	},
 	{
-		minlevel = 111,
-		maxlevel = 125,
-		multiplier = 4,
-	},
-	{
-		minlevel = 126,
+		minlevel = 130,
 		multiplier = 2,
 	},
 }
@@ -59,30 +94,30 @@ magicLevelStages = {
 	{
 		minlevel = 0,
 		maxlevel = 60,
-		multiplier = 10,
+		multiplier = 20,
 	},
 	{
 		minlevel = 61,
 		maxlevel = 80,
-		multiplier = 7,
+		multiplier = 15,
 	},
 	{
 		minlevel = 81,
 		maxlevel = 100,
-		multiplier = 5,
+		multiplier = 10,
 	},
 	{
 		minlevel = 101,
 		maxlevel = 110,
-		multiplier = 4,
+		multiplier = 5,
 	},
 	{
 		minlevel = 111,
-		maxlevel = 125,
+		maxlevel = 120,
 		multiplier = 3,
 	},
 	{
-		minlevel = 126,
+		minlevel = 121,
 		multiplier = 2,
 	},
 }

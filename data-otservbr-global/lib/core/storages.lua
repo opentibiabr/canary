5aeb624bb84b8f53e42c41aafd1995470c50e4fc
Marco <marcomoa@hotmail.com>
2024-02-19 16:35:07 -0300

    refactor: improve code structure for Glooth Fairy lever

--- data-otservbr-global/lib/core/storages.lua
+++ data-otservbr-global/lib/core/storages.lua
@@ -3084,6 +3084,7 @@ GlobalStorage = {
 	NaginataStone = 65003,
 	ExpBoost = 65004,
 	SwordOfFury = 65005,
+	GloothFairyTimer = 65006,
 	LionsRockFields = 65007,
 	TheMummysCurse = 65008,
 	OberonEventTime = 65009,

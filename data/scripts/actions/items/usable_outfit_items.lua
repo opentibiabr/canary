d7682a743a1d1fa1a30041e8b22f096474514763
Elson Costa <elsongabriel@hotmail.com>
2024-02-20 17:23:45 -0300

    resolve merge conflicts.

--- data/scripts/actions/items/usable_outfit_items.lua
+++ data/scripts/actions/items/usable_outfit_items.lua
@@ -13,18 +13,23 @@ local outfitConfig = {
 	[27655] = { female = 1043, male = 1042, textwhite = "By using the plan you knock together a makeshift armour out of wooden planks, rusty nails and leather rags." },
 	[27657] = { female = 1043, male = 1042, addon = 1, achievement = "Cobbled and Patched", textwhite = "You use the wooden planks to knock up a makeshift shield and weapon." },
 	[27656] = { female = 1043, male = 1042, addon = 2, achievement = "Cobbled and Patched", textwhite = "You use the tinged pot as a makeshift helmet." },
+
 	-- hand of the inquisition
 	[31738] = { female = 1244, male = 1243, addon = 1, effect = CONST_ME_HOLYAREA, achievement = "Inquisition's Arm" },
 	[31737] = { female = 1244, male = 1243, addon = 2, effect = CONST_ME_HOLYAREA, achievement = "Inquisition's Arm" },
+
 	-- revenant
 	[34075] = { female = 1323, male = 1322, addon = 1, effect = CONST_ME_HOLYAREA, achievement = "Unleash the Beast", textorange = "Now the beast is unleashed!" },
 	[34076] = { female = 1323, male = 1322, addon = 2, effect = CONST_ME_HOLYAREA, achievement = "Unleash the Beast", textorange = "Wild power flows though your body!" },
+
 	-- poltergeist
 	[32630] = { female = 1271, male = 1270, addon = 1, effect = CONST_ME_BLUE_GHOST, achievement = "Mainstreet Nightmare", textorange = "The spooky hood is yours!" },
 	[32631] = { female = 1271, male = 1270, addon = 2, effect = CONST_ME_BLUE_GHOST, achievement = "Mainstreet Nightmare", textorange = "You can use the ghost claw now!" },
+
 	-- rascoohan
 	[35595] = { female = 1372, male = 1371, addon = 1, achievement = "Honorary Rascoohan", textorange = "You feel a bit more raccoonish." },
 	[35695] = { female = 1372, male = 1371, addon = 2, achievement = "Honorary Rascoohan", textorange = "Hmmm, trash cans!!" },
+
 	-- fire-fighter
 	[39544] = { female = 1569, male = 1568, addon = 1, achievement = "Friendly Fire", textorange = "You feel like fighting a fire!" },
 	[39545] = { female = 1569, male = 1568, addon = 2, achievement = "Friendly Fire", textorange = "The flame engulfs you!" },
@@ -48,7 +53,12 @@ function usableOutfitItems.onUse(player, item, fromPosition, target, toPosition,
 
 		player:addOutfit(outfitInfo.female)
 		player:addOutfit(outfitInfo.male)
-		player:getPosition():sendMagicEffect(outfitInfo.effect)
+		player:getPosition():sendMagicEffect(outfitInfo.effect or CONST_ME_GIFT_WRAPS)
+		if outfitInfo.textorange then
+			player:say(outfitInfo.textorange, TALKTYPE_MONSTER_SAY)
+		else
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, outfitInfo.textwhite)
+		end
 		item:remove(1)
 		return true
 	end
@@ -60,7 +70,12 @@ function usableOutfitItems.onUse(player, item, fromPosition, target, toPosition,
 
 	player:addOutfitAddon(outfitInfo.female, outfitInfo.addon)
 	player:addOutfitAddon(outfitInfo.male, outfitInfo.addon)
-	player:getPosition():sendMagicEffect(outfitInfo.effect)
+	player:getPosition():sendMagicEffect(outfitInfo.effect or CONST_ME_GIFT_WRAPS)
+	if outfitInfo.textorange then
+		player:say(outfitInfo.textorange, TALKTYPE_MONSTER_SAY)
+	else
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, outfitInfo.textwhite)
+	end
 
 	if player:hasOutfit(looktype, 3) then
 		player:addAchievement(outfitInfo.achievement)

d584a3346ceb11350dcb025f677a7271f85409af
Marco <marcomoa@hotmail.com>
2024-02-19 22:13:58 -0300

    chore: moving iron servant transformation to realmap (#2265)
    Moving Eventcallback to rlmap, as it is not part of the core as it is
    just a quest.

--- data/scripts/eventcallbacks/monster/on_spawn.lua
+++ data/scripts/eventcallbacks/monster/on_spawn.lua
@@ -1,30 +1,3 @@
-local function handleIronServantReplica(monster)
-	if monster:getName():lower() ~= "iron servant replica" then
-		return
-	end
-
-	local chance = math.random(100)
-	if Game.getStorageValue(GlobalStorage.ForgottenKnowledge.MechanismDiamond) >= 1 and Game.getStorageValue(GlobalStorage.ForgottenKnowledge.MechanismGolden) >= 1 then
-		if chance > 30 then
-			local monsterType = math.random(2) == 1 and "diamond servant replica" or "golden servant replica"
-			Game.createMonster(monsterType, monster:getPosition(), false, true)
-			monster:remove()
-		end
-		return
-	end
-
-	if Game.getStorageValue(GlobalStorage.ForgottenKnowledge.MechanismDiamond) >= 1 and chance > 30 then
-		Game.createMonster("diamond servant replica", monster:getPosition(), false, true)
-		monster:remove()
-		return
-	end
-
-	if Game.getStorageValue(GlobalStorage.ForgottenKnowledge.MechanismGolden) >= 1 and chance > 30 then
-		Game.createMonster("golden servant replica", monster:getPosition(), false, true)
-		monster:remove()
-	end
-end
-
 local callback = EventCallback()
 
 function callback.monsterOnSpawn(monster, position)
@@ -37,8 +10,6 @@ function callback.monsterOnSpawn(monster, position)
 	if monster:getType():isRewardBoss() then
 		monster:setReward(true)
 	end
-
-	handleIronServantReplica(monster)
 
 	if not monster:getType():canSpawn(position) then
 		monster:remove()

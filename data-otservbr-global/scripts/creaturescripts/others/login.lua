2d707eed0534f60cbb1d1823d13d65b65890d881
Elson Costa <elsongabriel@hotmail.com>
2024-02-23 14:47:29 -0300

    feature: streak level and checks improvements (#2289)

--- data-otservbr-global/scripts/creaturescripts/others/login.lua
+++ data-otservbr-global/scripts/creaturescripts/others/login.lua
@@ -33,6 +33,9 @@ function playerLogin.onLogin(player)
 	else
 		player:openChannel(3) -- World chat
 		player:openChannel(5) -- Advertsing main
+		if player:getGuild() then
+			player:openChannel(0x00) -- guild
+		end
 	end
 	return true
 end

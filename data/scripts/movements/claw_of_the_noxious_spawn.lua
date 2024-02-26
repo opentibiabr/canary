42b851512d356362d2470f198a4780b95ab03f18
Marco <marcomoa@hotmail.com>
2024-02-26 14:21:36 -0300

    fix: boolean_random error (#2323)

--- data/scripts/movements/claw_of_the_noxious_spawn.lua
+++ data/scripts/movements/claw_of_the_noxious_spawn.lua
@@ -3,8 +3,8 @@ local clawOfTheNoxiousSpawn = MoveEvent()
 function clawOfTheNoxiousSpawn.onEquip(player, item, slot, isCheck)
 	if not isCheck then
 		if not Tile(player:getPosition()):hasFlag(TILESTATE_PROTECTIONZONE) then
-			doTargetCombat(0, player, COMBAT_PHYSICALDAMAGE, -150, -200, CONST_ME_DRAWBLOOD)
-			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, (math.boolean_random() and "It tightens around your wrist as you take it on." or "Ouch! The serpent claw stabbed you."))
+			doTargetCombatHealth(0, player, COMBAT_PHYSICALDAMAGE, -150, -200, CONST_ME_DRAWBLOOD)
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, (math.random(2) == 1 and "It tightens around your wrist as you take it on." or "Ouch! The serpent claw stabbed you."))
 			return true
 		end
 	end

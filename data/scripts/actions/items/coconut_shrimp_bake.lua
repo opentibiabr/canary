76c5f5fa65a22550d8f034378097169da0eea4a8
Marco <marcomoa@hotmail.com>
2024-02-26 01:15:18 -0300

    add cooldown check for Coconut Shrimp Bake haste condition

--- data/scripts/actions/items/coconut_shrimp_bake.lua
+++ data/scripts/actions/items/coconut_shrimp_bake.lua
@@ -7,15 +7,15 @@ function coconutShrimpBake.onUse(player, item, fromPosition, target, toPosition,
 	end
 
 	local headItem = player:getSlotItem(CONST_SLOT_HEAD)
-	if not headItem or headItem.itemid ~= 11585 then
+	if not headItem or (headItem.itemid ~= 5460 and headItem.itemid ~= 11585 and headItem.itemid ~= 13995) then
 		return true
 	end
 
-	player:setExhaustion("coconut-shrimp-bake", 24 * 60 * 60)
 	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Underwater walking speed increased.")
 	player:say("Yum.", TALKTYPE_MONSTER_SAY)
 	player:getPosition():sendMagicEffect(CONST_ME_MAGIC_RED)
 	player:setExhaustion("jean-pierre-foods", 10 * 60)
+	player:setExhaustion("coconut-shrimp-bake", 24 * 60 * 60)
 	item:remove(1)
 	return true
 end

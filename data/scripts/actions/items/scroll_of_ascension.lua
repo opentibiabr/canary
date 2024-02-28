5a28d7b91c58ea9f0986b9e7909dd92ff36a79da
Marco <marcomoa@hotmail.com>
2024-02-28 20:08:26 -0300

    chore: move scroll of ascencion to core

---
+++ data/scripts/actions/items/scroll_of_ascension.lua
@@ -0,0 +1,27 @@
+local scrollOfAscension = Action()
+
+function scrollOfAscension.onUse(player, item, fromPosition, target, toPosition, isHotkey)
+	if player:hasExhaustion("scroll-of-ascension") then
+		return true
+	end
+
+	local playerTile = Tile(player:getPosition())
+	if playerTile and playerTile:getGround() and isInArray(swimmingTiles, playerTile:getGround():getId()) then
+		player:say("The scroll could get wet, step out of the water first.", TALKTYPE_MONSTER_SAY)
+		return true
+	end
+
+	if math.random(10) > 1 then
+		player:setMonsterOutfit("Demon", 30 * 10 * 1000)
+	else
+		player:setMonsterOutfit("Ferumbras", 30 * 10 * 1000)
+	end
+
+	player:setExhaustion("scroll-of-ascension", 60 * 60)
+	player:say("Magical sparks whirl around the scroll as you read it and then your appearance is changing.", TALKTYPE_MONSTER_SAY)
+	player:getPosition():sendMagicEffect(CONST_ME_MAGIC_RED)
+	return true
+end
+
+scrollOfAscension:id(22771)
+scrollOfAscension:register()

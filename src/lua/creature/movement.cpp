355fca65dfe92b73d01a82b2d081064e97fa6e64
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2024-02-29 09:09:16 -0300

    improve: rework in unscripted weapons and moveevents (#1494)
    Add moveevent and weapons unscripted to load in items.xml
    
    Added "chain" logic, which can be configured in config.lua
    
    By default, the system is activated, it can be deactivated by setting the "toggleChainSystem" config tag to false and it will work to attack various creatures with the rod/wand or club/sword/axe weapon.
    
    The default creature limit is 5.
    
    Conversor link: https://github.com/dudantas/weapon-moveevents-conversor
    
    ---------
    
    Co-authored-by: Luan Santos <git@luan.sh>

--- src/lua/creature/movement.cpp
+++ src/lua/creature/movement.cpp
@@ -15,7 +15,34 @@
 #include "lua/callbacks/events_callbacks.hpp"
 #include "lua/creature/movement.hpp"
 
-void MoveEvents::clear() {
+void MoveEvents::clear(bool isFromXML /*= false*/) {
+	if (isFromXML) {
+		int numRemoved = 0;
+		for (auto &pair : itemIdMap) {
+			MoveEventList &moveEventList = pair.second;
+
+			for (int moveEventType = 0; moveEventType < MOVE_EVENT_LAST; ++moveEventType) {
+				auto &eventList = moveEventList.moveEvent[moveEventType];
+
+				int originalSize = eventList.size();
+
+				eventList.remove_if([&](const std::shared_ptr<MoveEvent> &moveEvent) {
+					bool removed = moveEvent && moveEvent->isFromXML();
+					if (removed) {
+						g_logger().debug("MoveEvent with id '{}' is from XML and will be removed.", pair.first);
+						++numRemoved;
+					}
+					return removed;
+				});
+			}
+		}
+
+		if (numRemoved > 0) {
+			g_logger().debug("Removed '{}' MoveEvent from XML.", numRemoved);
+		}
+		return;
+	}
+
 	uniqueIdMap.clear();
 	actionIdMap.clear();
 	itemIdMap.clear();

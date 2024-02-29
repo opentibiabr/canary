31a57b0eee795b7561234dd7cedef5fb016a1b34
Marco <marcomoa@hotmail.com>
2024-02-29 13:57:25 -0300

    refactor: fireworks rocket to core and fix kegs path (#2337)

--- data/scripts/actions/items/fireworks_rocket.lua
+++ data/scripts/actions/items/fireworks_rocket.lua
@@ -4,12 +4,13 @@ function fireworksRocket.onUse(player, item, fromPosition, target, toPosition, i
 	if fromPosition.x ~= CONTAINER_POSITION then
 		fromPosition:sendMagicEffect(math.random(CONST_ME_FIREWORK_YELLOW, CONST_ME_FIREWORK_BLUE))
 	else
-		local position = player:getPosition()
-		position:sendMagicEffect(CONST_ME_HITBYFIRE)
-		position:sendMagicEffect(CONST_ME_EXPLOSIONAREA)
+		player:getPosition():sendMagicEffect(CONST_ME_FIREAREA)
+		player:getPosition():sendMagicEffect(CONST_ME_HITBYFIRE)
 		player:say("Ouch! Rather place it on the ground next time.", TALKTYPE_MONSTER_SAY)
+		player:addAchievementProgress("Rocket in Pocket", 3)
 		player:addHealth(-10, COMBAT_PHYSICALDAMAGE)
 	end
+
 	player:addAchievementProgress("Fireworks in the Sky", 250)
 	item:remove()
 	return true

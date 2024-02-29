4244940f4f4d1a1c48dc15ece3de7a69b95b490f
Majesty <32709570+majestyotbr@users.noreply.github.com>
2024-02-29 16:38:33 -0300

    fix: quest condition

--- data-otservbr-global/scripts/actions/rookgaard/rapier_quest.lua
+++ data-otservbr-global/scripts/actions/rookgaard/rapier_quest.lua
@@ -3,7 +3,7 @@ local rapierQuest = Action()
 function rapierQuest.onUse(player, item, fromPosition, target, toPosition, isHotkey)
 	local rewardId = 3272
 	if not player:canGetReward(rewardId, "rapier") then
-		return false
+		return true
 	end
 	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have found a rapier.")
 	player:addItem(rewardId, 1)

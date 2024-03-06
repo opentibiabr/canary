4f4832c8cb07ba3a874905921228f1212e26bc17
Marco <marcomoa@hotmail.com>
2024-03-06 14:03:46 -0300

    fix: sendMagicEffect error

--- data/scripts/actions/items/die.lua
+++ data/scripts/actions/items/die.lua
@@ -5,11 +5,13 @@ function die.onUse(player, item, fromPosition, target, toPosition, isHotkey)
 	local value = math.random(1, 6)
 	local isInGhostMode = player:isInGhostMode()
 
-	position:sendMagicEffect(CONST_ME_CRAPS, isInGhostMode and player)
+	if not isInGhostMode then
+		player:getPosition():sendMagicEffect(CONST_ME_CRAPS)
 
-	local spectators = Game.getSpectators(position, false, true, 3, 3)
-	for _, spectator in ipairs(spectators) do
-		player:say(player:getName() .. " rolled a " .. value .. ".", TALKTYPE_MONSTER_SAY, isInGhostMode, spectator, position)
+		local spectators = Game.getSpectators(position, false, true, 3, 3)
+		for _, spectator in ipairs(spectators) do
+			player:say(player:getName() .. " rolled a " .. value .. ".", TALKTYPE_MONSTER_SAY, isInGhostMode, spectator, position)
+		end
 	end
 
 	if value == 6 then

d14f9255b7ad2eac8022ec21e6cad8bb1a2d1732
Aerwix <dekarzfajardo@gmail.com>
2024-03-07 23:06:09 -0600

    Update snow.lua

--- data/scripts/movements/snow.lua
+++ data/scripts/movements/snow.lua
@@ -1,18 +1,19 @@
 local snow = MoveEvent()
 
 function snow.onStepOut(creature, item, position, fromPosition)
-	local player = creature:getPlayer()
-	if player and player:isInGhostMode() then
+	if creature:isInGhostMode() then
 		return true
 	end
-
 	if item.itemid == 799 then
 		item:transform(6594)
 	else
 		item:transform(item.itemid + 15)
 	end
-
-	player:addAchievementProgress("Snowbunny", 10000)
+	
+	local player = creature:getPlayer()
+	if player and player:isPlayer() then
+		player:addAchievementProgress("Snowbunny", 10000)
+	end
 	item:decay()
 	return true
 end

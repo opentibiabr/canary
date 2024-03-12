40ca88b28918c54ade084a49f205e0b9b9b6d444
odisk777 <65802862+odisk777@users.noreply.github.com>
2024-03-12 18:37:00 -0500

    Update cask_and_kegs.lua
    The error you've shown seems to be related to the getItem() method you're trying to call on the target object. However, in your code, you're checking if target or target:getItem() is nil before calling this method. This means that the error shouldn't occur at this point.
    
    Since the error mentioned line 29 of your cask_and_kegs.lua file, I suggest you review that specific line to ensure you're not trying to call getItem() incorrectly or from an incorrect object.
    
    If line 29 is where getItem() is called, there might be an issue with how the target object is being obtained or passed. Make sure target is a valid object that has the getItem() method defined. Additionally, verify that target is not nil before attempting to call its getItem() method.
    
    [error] Lua Script Error Detected
    ---------------------------------------
    Interface: Scripts Interface
    Script ID: /data/scripts/actions/objects/cask_and_kegs.lua:callback
    Error Description: .../data/scripts/actions/objects/cask_and_kegs.lua:29: attempt to call method 'getItem' (a nil value)
    stack traceback:
            [C]: in function 'getItem'
            .../data/scripts/actions/objects/cask_and_kegs.lua:29: in function <.../data/scripts/actions/objects/cask_and_kegs.lua:28>

--- data/scripts/actions/objects/cask_and_kegs.lua
+++ data/scripts/actions/objects/cask_and_kegs.lua
@@ -26,7 +26,7 @@ local targetIdList = {
 local flasks = Action()
 
 function flasks.onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	if not target or not target:getItem() then
+	if not target then
 		return false
 	end
 

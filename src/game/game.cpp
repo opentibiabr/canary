41f715fea96272d0bacf779a429df9a2bc8ab9c6
Luan Luciano <luanluciano@outlook.com>
2024-03-24 04:20:26 -0300

    Update game.cpp

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -3415,7 +3415,6 @@ void Game::playerAutoWalk(uint32_t playerId, const std::vector<Direction> &listD
 	}
 
 	player->resetIdleTime();
-	player->setNextWalkTask(nullptr);
 	player->startAutoWalk(listDir, false);
 }
 
@@ -3431,7 +3430,6 @@ void Game::forcePlayerAutoWalk(uint32_t playerId, const std::vector<Direction> &
 	player->setFollowCreature(nullptr);
 
 	player->resetIdleTime();
-	player->setNextWalkTask(nullptr);
 
 	player->startAutoWalk(listDir, true);
 }

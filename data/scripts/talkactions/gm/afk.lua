4ba32fbc8a102ed6d8e9c0922ebca041ce5cb9c1
Elson Costa <elsongabriel@hotmail.com>
2024-04-07 21:40:34 -0300

    afk script: some improvements and added remove message when player logout.

--- data/scripts/talkactions/gm/afk.lua
+++ data/scripts/talkactions/gm/afk.lua
@@ -11,13 +11,22 @@ local function checkIsAFK(id)
 	return { afk = false }
 end
 
+local function showAfkMessage(playerPosition)
+	local spectators = Game.getSpectators(playerPosition, false, true, 8, 8, 8, 8)
+	if #spectators > 0 then
+		for _, spectator in ipairs(spectators) do
+			spectator:say("AFK !", TALKTYPE_MONSTER_SAY, false, spectator, playerPosition)
+		end
+	end
+end
+
 function afk.onSay(player, words, param)
 	if param == "" then
 		player:sendCancelMessage("You need to specify on/off param.")
 		return true
 	end
 
-	local id, position = player:getId(), player:getPosition()
+	local id, playerPosition = player:getId(), player:getPosition()
 	local isAfk = checkIsAFK(id)
 	if param == "on" then
 		if isAfk.afk then
@@ -25,24 +34,19 @@ function afk.onSay(player, words, param)
 			return true
 		end
 
-		table.insert(playersAFKs, { id = id, position = position })
+		table.insert(playersAFKs, { id = id, position = playerPosition })
 		if player:isInGhostMode() then
 			player:setGhostMode(false)
 		end
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You are now AFK!")
-		position:sendMagicEffect(CONST_ME_REDSMOKE)
-		local spectators = Game.getSpectators(position, false, true, 8, 8, 8, 8)
-		if #spectators > 0 then
-			for _, spectator in ipairs(spectators) do
-				spectator:say("AFK !", TALKTYPE_MONSTER_SAY, false, spectator, position)
-			end
-		end
+		playerPosition:sendMagicEffect(CONST_ME_REDSMOKE)
+		showAfkMessage(playerPosition)
 	elseif param == "off" then
 		if isAfk.afk then
 			table.remove(playersAFKs, isAfk.index)
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You are no longer AFK!")
+			playerPosition:sendMagicEffect(CONST_ME_REDSMOKE)
 		end
-		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You are no longer AFK!")
-		position:sendMagicEffect(CONST_ME_REDSMOKE)
 	end
 
 	return true
@@ -56,12 +60,7 @@ afk:register()
 local afkEffect = GlobalEvent("GodAfkEffect")
 function afkEffect.onThink(interval)
 	for _, player in ipairs(playersAFKs) do
-		local spectators = Game.getSpectators(player.position, false, true, 8, 8, 8, 8)
-		if #spectators > 0 then
-			for _, spectator in ipairs(spectators) do
-				spectator:say("AFK !", TALKTYPE_MONSTER_SAY, false, spectator, player.position)
-			end
-		end
+		showAfkMessage(player.position)
 	end
 	return true
 end
@@ -81,3 +80,15 @@ function callback.playerOnWalk(player, creature, creaturePos, toPos)
 end
 
 callback:register()
+
+------------------ Player Logout ------------------
+local godAfkLogout = CreatureEvent("GodAfkLogout")
+function godAfkLogout.onLogout(player)
+	local isAfk = checkIsAFK(player:getId())
+	if isAfk.afk then
+		table.remove(playersAFKs, isAfk.index)
+	end
+	return true
+end
+
+godAfkLogout:register()

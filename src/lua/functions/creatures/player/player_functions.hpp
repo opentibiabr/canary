76b1a3357919e6ecc06c81d71c1a31c2f7b5b927
Luan Luciano <luanluciano@outlook.com>
2024-04-10 20:28:36 -0300

    Update player_functions.hpp

--- src/lua/functions/creatures/player/player_functions.hpp
+++ src/lua/functions/creatures/player/player_functions.hpp
@@ -720,7 +720,7 @@ private:
 	static int luaPlayerAddAchievementPoints(lua_State* L);
 	static int luaPlayerRemoveAchievementPoints(lua_State* L);
 
-	static int luaPlayerSendTakeScreenshot(lua_State* L);
+	static int luaPlayerTakeScreenshot(lua_State* L);
 
 	friend class CreatureFunctions;
 };

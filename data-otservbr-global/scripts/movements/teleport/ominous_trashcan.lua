63b24e0f2468efbbdc92d77f5786e049d5ef7098
Felipe Paluco <fepaluco@gmail.com>
2024-04-27 10:58:38 -0300

    feat: add bulltaur lair

---
+++ data-otservbr-global/scripts/movements/teleport/ominous_trashcan.lua
@@ -0,0 +1,62 @@
+local config = {
+    teleports = { 
+    -- { position = Position(33187, 31190, 7), destination = Position(33217, 31123, 14) }, -- Entrace Nimmersatt's Breeding Ground
+    -- { position = Position(33216, 31126,14), destination = Position(33188, 31193, 7) }, -- Leave Nimmersatt's Breeding Ground
+    {
+        position = Position(32799, 32365, 8),
+        destination = Position(32872, 32372, 8)
+    }, -- Entrace Bulltaur Lair
+    {
+        position = Position(32874, 32374, 8),
+        destination = Position(32800, 32368, 8)
+    } -- Leave Bulltaur Lair
+    }
+}
+
+local Omnious_Trashcan = MoveEvent()
+
+function Omnious_Trashcan.onStepIn(creature, item, position, fromPosition)
+    local player = creature:getPlayer()
+    if not player then
+        return false
+    end
+
+    for c = 1, #config.teleports do
+        if config.teleports[c].position then
+            if player:getPosition() == Position(config.teleports[c].position) then
+                if config.teleports[c].access then
+                    if player:getStorageValue(config.teleports[c].access) > 0 then
+                        player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+                        player:teleportTo(config.teleports[c].destination)
+                        player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+                    else
+                        player:teleportTo(fromPosition)
+                    end
+                else
+                    player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+                    player:teleportTo(config.teleports[c].destination)
+                    player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+                end
+            end
+        elseif config.teleports[c].positions then
+            for d = 1, #config.teleports[c].positions do
+                if player:getPosition() == Position(config.teleports[c].positions[d]) then
+                    player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+                    player:teleportTo(config.teleports[c].destination)
+                    player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+                end
+            end
+        end
+    end
+end
+
+for a = 1, #config.teleports do
+    if config.teleports[a].position then
+        Omnious_Trashcan:position(config.teleports[a].position)
+    elseif config.teleports[a].positions then
+        for b = 1, #config.teleports[a].positions do
+            Omnious_Trashcan:position(config.teleports[a].positions[b])
+        end
+    end
+end
+Omnious_Trashcan:register()
---
+++ data-otservbr-global/scripts/spells/monster/bulltaur_alchemist_avalanche.lua
@@ -0,0 +1,28 @@
+local combat = Combat()
+combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
+combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICEAREA)
+arr = {
+	{ 0, 0, 0, 1, 1, 1, 0, 0, 0 },
+	{ 0, 0, 1, 1, 1, 1, 1, 0, 0 },
+	{ 0, 0, 1, 1, 1, 1, 1, 0, 0 },
+	{ 1, 1, 1, 1, 3, 1, 1, 1, 1 },
+	{ 0, 0, 1, 1, 1, 1, 1, 0, 0 },
+	{ 0, 0, 1, 1, 1, 1, 1, 0, 0 },
+	{ 0, 0, 0, 1, 1, 1, 0, 0, 0 },
+}
+
+local area = createCombatArea(arr)
+combat:setArea(area)
+
+local spell = Spell("instant")
+
+function spell.onCastSpell(creature, var)
+	return combat:execute(creature, var)
+end
+
+spell:name("bulltaur avalanche")
+spell:words("###7000")
+spell:needLearn(true)
+spell:cooldown("2000")
+spell:isSelfTarget(true)
+spell:register()

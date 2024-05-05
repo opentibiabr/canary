432ef06ffbdeb52480939bf77e939e722f3675ff
Carlos Pessoa <pessoacarlos98@gmail.com>
2024-05-05 20:27:31 -0300

    add migrations

--- data-otservbr-global/migrations/44.lua
+++ data-otservbr-global/migrations/44.lua
@@ -1,3 +1,5 @@
 function onUpdateDatabase()
-	return false -- true = There are others migrations file | false = this is the last migration file
+	db.query("ALTER TABLE `players` MODIFY COLUMN `manashield` INT UNSIGNED NOT NULL DEFAULT '0'")
+	db.query("ALTER TABLE `players` MODIFY COLUMN `max_manashield` INT UNSIGNED NOT NULL DEFAULT '0'")
+	return true
 end

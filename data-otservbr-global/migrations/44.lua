50fe3db0b473851952e16a8795c7a0fbfb5d9e9c
Carlos Pessoa <pessoacarlos98@gmail.com>
2024-05-07 14:17:11 -0300

    log

--- data-otservbr-global/migrations/44.lua
+++ data-otservbr-global/migrations/44.lua
@@ -1,4 +1,5 @@
 function onUpdateDatabase()
+	logger.info("Updating database to version 44 (fix: mana shield more than 65k")
 	db.query("ALTER TABLE `players` MODIFY COLUMN `manashield` INT UNSIGNED NOT NULL DEFAULT '0'")
 	db.query("ALTER TABLE `players` MODIFY COLUMN `max_manashield` INT UNSIGNED NOT NULL DEFAULT '0'")
 	return true

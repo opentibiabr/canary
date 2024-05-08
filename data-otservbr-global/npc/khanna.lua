11f02142c8d511633c5b77c05a6c90e75bbff243
Guilherme <guilherme.vrsantana@gmail.com>
2024-05-08 11:10:30 -0300

    Update khanna.lua

--- data-otservbr-global/npc/khanna.lua
+++ data-otservbr-global/npc/khanna.lua
@@ -119,7 +119,7 @@ local itemsTable = {
 		{ itemName = "spellbook of enlightenment", clientId = 8072, sell = 4000 },
 		{ itemName = "spellbook of lost souls", clientId = 8075, sell = 19000 },
 		{ itemName = "spellbook of mind control", clientId = 8074, sell = 13000 },
-		{ itemName = "spellbook of warding", clientId = 8074, sell = 8000 },
+		{ itemName = "spellbook of warding", clientId = 8073, sell = 8000 },
 	},
 	["others"] = {
 		{ itemName = "spellwand", clientId = 651, sell = 299 },

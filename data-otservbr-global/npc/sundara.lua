06a3c5a39f8ee7a130e19f74521b6e8ceb3bd6bd
Guilherme <guilherme.vrsantana@gmail.com>
2024-05-08 08:09:23 -0300

    Update sundara.lua

--- data-otservbr-global/npc/sundara.lua
+++ data-otservbr-global/npc/sundara.lua
@@ -109,7 +109,7 @@ local itemsTable = {
 		{ itemName = "lasting exercise rod", clientId = 35289, buy = 7560000, count = 14400 },
 		{ itemName = "lasting exercise wand", clientId = 35290, buy = 7560000, count = 14400 },
 	},
-	["creature products"] = {
+	["others"] = {
 		{ itemName = "spellwand", clientId = 651, sell = 299 },
 	},
 }

b6e368ffb9f3b82f92ce5969dbd6f96f93991f73
Luan Luciano <luanluciano@outlook.com>
2024-05-09 02:57:33 -0300

    Update monster.cpp

--- src/creatures/monsters/monster.cpp
+++ src/creatures/monsters/monster.cpp
@@ -80,8 +80,7 @@ void Monster::setName(const std::string &name) {
 
 	// NOTE: Due to how client caches known creatures,
 	// it is not feasible to send creature update to everyone that has ever met it
-	Spectators spectators;
-	spectators.find<Player>(position, true);
+	auto spectators = Spectators().find<Player>(position, true);
 	for (const auto spectator : spectators) {
 		if (const auto tmpPlayer = spectator->getPlayer()) {
 			tmpPlayer->sendUpdateTileCreature(this);

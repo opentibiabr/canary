6314cdbb1033edbbb2aba0fa20c6be07b921c8c4
Aluisio Penna Ordones <aluisioordones1@gmail.com>
2024-05-10 20:28:51 -0300

    fix: spikes and blades behavior / add autoTransformItems function

--- data-otservbr-global/scripts/globalevents/others/additems_to_auto_transform.lua
+++ data-otservbr-global/scripts/globalevents/others/additems_to_auto_transform.lua
@@ -1,5 +1,5 @@
 -- Automated transform items are sent through this callback as soon as they transform, just like your Tile
-function onTransformIntoBlades(tile, item)
+local function onTransformIntoBlades(tile, item)
 	if not tile:hasFlag(TILESTATE_PROTECTIONZONE) and item:getId() == 2146 then
 		for _, creature in ipairs(tile:getCreatures()) do
 			doTargetCombatHealth(0, creature, COMBAT_PHYSICALDAMAGE, -50, -100, CONST_ME_NONE)
@@ -13,14 +13,15 @@ local slitsBlades = {
 		-- poi end
 		-- some slits from poi is not here since these are traps exclusively
 		-- triggered by the player's walking, not by autoTransformItems
-		{ position = Position(32839, 32251, 10), intervalSeconds = 1, creatureOnTopDelaySecs = 0, onTransform = onTransformIntoBlades },
-		{ position = Position(32839, 32253, 10), intervalSeconds = 1, creatureOnTopDelaySecs = 0, onTransform = onTransformIntoBlades },
+		{ position = Position(32839, 32251, 10), intervalSeconds = 1, onTransform = onTransformIntoBlades },
+		{ position = Position(32839, 32253, 10), intervalSeconds = 1, onTransform = onTransformIntoBlades },
 		--
 	},
 }
 
 -- Automated transform items are sent through this callback as soon as they transform, just like your Tile
-function onTransformIntoSpikes(tile, item)
+local function onTransformIntoSpikes(tile, item)
+	print("transform", item:getId())
 	if not tile:hasFlag(TILESTATE_PROTECTIONZONE) and item:getId() == 2148 then
 		for _, creature in ipairs(tile:getCreatures()) do
 			doTargetCombatHealth(0, creature, COMBAT_PHYSICALDAMAGE, -15, -30, CONST_ME_NONE)
@@ -32,28 +33,28 @@ local holesSpikes = {
 	ids = { 2147, 2148 },
 	params = {
 		-- poi end
-		{ position = Position(32833, 32250, 10), intervalSeconds = 6, creatureOnTopDelaySecs = 0, onTransform = onTransformIntoSpikes },
-		{ position = Position(32833, 32251, 10), intervalSeconds = 2, creatureOnTopDelaySecs = 0, onTransform = onTransformIntoSpikes },
-		{ position = Position(32833, 32252, 10), intervalSeconds = 4, creatureOnTopDelaySecs = 0, onTransform = onTransformIntoSpikes },
-		{ position = Position(32833, 32253, 10), intervalSeconds = 2, creatureOnTopDelaySecs = 0, onTransform = onTransformIntoSpikes },
-		{ position = Position(32833, 32254, 10), intervalSeconds = 6, creatureOnTopDelaySecs = 0, onTransform = onTransformIntoSpikes },
-		{ position = Position(32835, 32250, 10), intervalSeconds = 2, creatureOnTopDelaySecs = 0, onTransform = onTransformIntoSpikes },
-		{ position = Position(32835, 32251, 10), intervalSeconds = 4, creatureOnTopDelaySecs = 0, onTransform = onTransformIntoSpikes },
-		{ position = Position(32835, 32252, 10), intervalSeconds = 6, creatureOnTopDelaySecs = 0, onTransform = onTransformIntoSpikes },
-		{ position = Position(32835, 32253, 10), intervalSeconds = 4, creatureOnTopDelaySecs = 0, onTransform = onTransformIntoSpikes },
-		{ position = Position(32835, 32254, 10), intervalSeconds = 2, creatureOnTopDelaySecs = 0, onTransform = onTransformIntoSpikes },
-		{ position = Position(32838, 32250, 10), intervalSeconds = 1, creatureOnTopDelaySecs = 0, onTransform = onTransformIntoSpikes },
-		{ position = Position(32838, 32251, 10), intervalSeconds = 2, creatureOnTopDelaySecs = 0, onTransform = onTransformIntoSpikes },
-		{ position = Position(32838, 32252, 10), intervalSeconds = 1, creatureOnTopDelaySecs = 0, onTransform = onTransformIntoSpikes },
-		{ position = Position(32838, 32253, 10), intervalSeconds = 2, creatureOnTopDelaySecs = 0, onTransform = onTransformIntoSpikes },
-		{ position = Position(32838, 32254, 10), intervalSeconds = 1, creatureOnTopDelaySecs = 0, onTransform = onTransformIntoSpikes },
+		{ position = Position(32833, 32250, 10), intervalSeconds = 6, onTransform = onTransformIntoSpikes },
+		{ position = Position(32833, 32251, 10), intervalSeconds = 2, onTransform = onTransformIntoSpikes },
+		{ position = Position(32833, 32252, 10), intervalSeconds = 4, onTransform = onTransformIntoSpikes },
+		{ position = Position(32833, 32253, 10), intervalSeconds = 2, onTransform = onTransformIntoSpikes },
+		{ position = Position(32833, 32254, 10), intervalSeconds = 6, onTransform = onTransformIntoSpikes },
+		{ position = Position(32835, 32250, 10), intervalSeconds = 2, onTransform = onTransformIntoSpikes },
+		{ position = Position(32835, 32251, 10), intervalSeconds = 4, onTransform = onTransformIntoSpikes },
+		{ position = Position(32835, 32252, 10), intervalSeconds = 6, onTransform = onTransformIntoSpikes },
+		{ position = Position(32835, 32253, 10), intervalSeconds = 4, onTransform = onTransformIntoSpikes },
+		{ position = Position(32835, 32254, 10), intervalSeconds = 2, onTransform = onTransformIntoSpikes },
+		{ position = Position(32838, 32250, 10), intervalSeconds = 1, onTransform = onTransformIntoSpikes },
+		{ position = Position(32838, 32251, 10), intervalSeconds = 2, onTransform = onTransformIntoSpikes },
+		{ position = Position(32838, 32252, 10), intervalSeconds = 1, onTransform = onTransformIntoSpikes },
+		{ position = Position(32838, 32253, 10), intervalSeconds = 2, onTransform = onTransformIntoSpikes },
+		{ position = Position(32838, 32254, 10), intervalSeconds = 1, onTransform = onTransformIntoSpikes },
 		--
 	},
 }
 
 local function addItemsToAutoTransform(data)
 	for _, param in ipairs(data.params) do
-		autoTransformItems(data.ids, param.position, param.intervalSeconds, param.creatureOnTopDelaySecs, param.onTransform)
+		autoTransformItems(data.ids, param.position, param.intervalSeconds, param.onTransform)
 	end
 end
 
--- data/scripts/globalevents/auto_transform_items.lua
+++ data/scripts/globalevents/auto_transform_items.lua
@@ -1,10 +1,9 @@
 local itemsToTransform = {}
 
 -- Automatically transforms items with the ids received into @itemsIds in a cyclical way,
--- present in the Tile at position @position, every @intervalSeconds,
--- with a delay of @creatureOnTopDelaySecs.
+-- present in the Tile at position @position, every @intervalSeconds
 -- When transforming and if defined, the onTransform function is called with the transformed tile and item.
-function autoTransformItems(itemIds, position, intervalSeconds, creatureOnTopDelaySecs, onTransform)
+function autoTransformItems(itemIds, position, intervalSeconds, onTransform)
 	local tile = Tile(position)
 	if not tile then
 		logger.warn("[autoTransformItems]: cannot find tile at Position{}", position:toString())
@@ -33,55 +32,51 @@ function autoTransformItems(itemIds, position, intervalSeconds, creatureOnTopDel
 		logger.warn("[autoTransformItems]: invalid interval for item with id {} at Position{}", fromItem:getId(), position:toString())
 		return
 	end
-	if not creatureOnTopDelaySecs then
-		creatureOnTopDelaySecs = 0
-	end
-	itemsToTransform[#itemsToTransform + 1] = {
+	local data = {
 		ids = itemIds,
-		position = position,
-		defaultIntervalMs = intervalSeconds * 1000,
-		creatureOnTopDelayMs = creatureOnTopDelaySecs * 1000,
 		index = index,
-		interval = 0,
-		creatureOnTopInterval = 0,
+		position = position,
 		onTransform = onTransform,
 	}
+	local defaultIntervalMs = intervalSeconds * 1000
+	if itemsToTransform[defaultIntervalMs] then
+		itemsToTransform[defaultIntervalMs].items[#itemsToTransform[defaultIntervalMs].items + 1] = data
+	else
+		itemsToTransform[defaultIntervalMs] = {
+			items = { data },
+			currentMs = 0,
+		}
+	end
 end
 
 local autoTransformItemsEvent = GlobalEvent("autoTransformItems.onThink")
 
 function autoTransformItemsEvent.onThink(interval, lastExecution)
-	for _, items in ipairs(itemsToTransform) do
-		local tile = Tile(items.position)
-		if tile then
-			-- If a creature above creates a transform delay, the indices must be updated so that the items
-			-- remain synchronized with the time of other items on the map that have the same interval.
-			-- This does not imply that he will be transformed now.
-			local prevId = items.ids[items.index]
-			items.interval = items.interval + interval
-			if items.interval == items.defaultIntervalMs then
-				items.interval = 0
-				local nextIndex = items.index + 1
-				if nextIndex > #items.ids then
-					items.index = 1
-				else
-					items.index = nextIndex
-				end
-			end
-			local nextId = items.ids[items.index]
-			local creatureCount = tile:getCreatureCount()
-			if items.creatureOnTopDelayMs > 0 and creatureCount > 0 then
-				items.creatureOnTopInterval = items.creatureOnTopInterval + items.creatureOnTopDelayMs
-			elseif items.creatureOnTopInterval > 0 then
-				items.creatureOnTopInterval = items.creatureOnTopInterval - interval
-			elseif prevId ~= nextId then
-				local targetItem = tile:getItemById(prevId)
-				if targetItem then
-					targetItem:transform(nextId)
-					-- some items have default decay, but in this case we deactivate it because we are controlling the item's transformations
-					targetItem:setAttribute(ITEM_ATTRIBUTE_DECAYSTATE, 0)
-					if items.onTransform then
-						items.onTransform(tile, targetItem)
+	for defaultIntervalMs, data in pairs(itemsToTransform) do
+		data.currentMs = data.currentMs + interval
+		if data.currentMs >= defaultIntervalMs then
+			data.currentMs = 0
+			for _, items in ipairs(data.items) do
+				local tile = Tile(items.position)
+				if tile then
+					local prevId = items.ids[items.index]
+					local nextIndex = items.index + 1
+					if nextIndex > #items.ids then
+						items.index = 1
+					else
+						items.index = nextIndex
+					end
+					local nextId = items.ids[items.index]
+					if prevId ~= nextId then
+						local targetItem = tile:getItemById(prevId)
+						if targetItem then
+							targetItem:transform(nextId)
+							-- some items have default decay, but in this case we deactivate it because we are controlling the item's transformations
+							targetItem:setAttribute(ITEM_ATTRIBUTE_DECAYSTATE, 0)
+							if items.onTransform then
+								items.onTransform(tile, targetItem)
+							end
+						end
 					end
 				end
 			end

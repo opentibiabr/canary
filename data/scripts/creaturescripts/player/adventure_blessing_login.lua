4af13eefdcc71f900587f3f4e9afa8083a1fe1ae
Marco <marcomoa@hotmail.com>
2024-06-04 22:17:28 -0300

    fix: small adjustments and corrections to the core (#2524)
    - Previously, players were not being removed from the room upon killing
    the boss, as the relevant script was confined to rlmap only. This update
    ensures its functionality across any datapack.
    - Hirelings are now accessible beyond rlmap environments.
    - Addressed an issue with the loading of VIP days library.
    - Relocated the large sea shell asset to the core.
    - Implemented VIP status check and associated actions.
    - Transferred weapons scripts to the core.

--- data/scripts/creaturescripts/player/adventure_blessing_login.lua
+++ data/scripts/creaturescripts/player/adventure_blessing_login.lua
@@ -1,6 +1,7 @@
 dofile(CORE_DIRECTORY .. "/modules/scripts/blessings/blessings.lua")
 
 local adventurerBlessingLogin = CreatureEvent("AdventurerBlessingLogin")
+
 function adventurerBlessingLogin.onLogin(player)
 	return Blessings.doAdventurerBlessing(player)
 end

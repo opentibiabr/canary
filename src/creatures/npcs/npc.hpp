95dd0fb3c5c04a49aa121323e5c8f8a417512c6e
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2024-06-22 22:13:37 -0300

    fix: hireling shop (#2707)

--- src/creatures/npcs/npc.hpp
+++ src/creatures/npcs/npc.hpp
@@ -96,6 +96,13 @@ public:
 	}
 
 	const std::vector<ShopBlock> &getShopItemVector(uint32_t playerGUID) const {
+		if (playerGUID != 0) {
+			auto it = shopPlayers.find(playerGUID);
+			if (it != shopPlayers.end() && !it->second.empty()) {
+				return it->second;
+			}
+		}
+
 		return npcType->info.shopItemVector;
 	}
 
@@ -160,7 +167,7 @@ public:
 
 	bool isShopPlayer(uint32_t playerGUID) const;
 
-	void addShopPlayer(uint32_t playerGUID);
+	void addShopPlayer(uint32_t playerGUID, const std::vector<ShopBlock> &shopItems);
 	void removeShopPlayer(uint32_t playerGUID);
 	void closeAllShopWindows();
 
@@ -179,7 +186,7 @@ private:
 
 	std::map<uint32_t, uint16_t> playerInteractions;
 
-	std::unordered_set<uint32_t> shopPlayers;
+	std::unordered_map<uint32_t, std::vector<ShopBlock>> shopPlayers;
 
 	std::shared_ptr<NpcType> npcType;
 	std::shared_ptr<SpawnNpc> spawnNpc;

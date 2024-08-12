88870caf317271b0fcb71acc76696fc52f01578c
Vitor <vitor.documents@gmail.com>
2024-08-12 19:54:07 -0300

    game store wild card limit and buyer npc

--- data/modules/scripts/gamestore/gamestore.lua
+++ data/modules/scripts/gamestore/gamestore.lua
@@ -6438,9 +6438,9 @@ GameStore.Categories = {
 			{
 				icons = { "Prey_Bonus_Reroll.png" },
 				name = "Prey Wildcard",
-				price = 50,
-				count = 20,
-				description = "<i>Use Prey Wildcards to reroll the bonus of an active prey, to lock your active prey or to select a prey of your choice.</i>\n\n{character}\n{info} added directly to Prey dialog\n{info} maximum amount that can be owned by character: 50",
+				price = 250,
+				count = 100,
+				description = "<i>Use Prey Wildcards to reroll the bonus of an active prey, to lock your active prey or to select a prey of your choice.</i>\n\n{character}\n{info} added directly to Prey dialog\n{info} maximum amount that can be owned by character: 500",
 				type = GameStore.OfferTypes.OFFER_TYPE_PREYBONUS,
 			},
 			{

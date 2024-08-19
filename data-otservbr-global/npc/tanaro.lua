5cd7223e3fe52d08c7a6e4b7e4073438a7da0ecd
Majesty <32709570+majestyotbr@users.noreply.github.com>
2024-08-19 11:59:06 -0300

    fix: price mismatch between NPCs and typo in the rent house message
    Co-Authored-By: Leilani A. <168607226+kaleohanopahala@users.noreply.github.com>

--- data-otservbr-global/npc/tanaro.lua
+++ data-otservbr-global/npc/tanaro.lua
@@ -67,8 +67,6 @@ npcConfig.shop = {
 	{ itemName = "salmon", clientId = 3579, buy = 6 },
 	{ itemName = "valentine's cake", clientId = 6392, buy = 100 },
 	{ itemName = "white mushroom", clientId = 3723, buy = 6 },
-	{ itemName = "vial of wine", clientId = 2874, buy = 3, count = 1, subType = 2 },
-	{ itemName = "vial of water", clientId = 2874, buy = 2, count = 1 },
 }
 -- On buy npc shop message
 npcType.onBuyItem = function(npc, player, itemId, subType, amount, ignore, inBackpacks, totalCost)

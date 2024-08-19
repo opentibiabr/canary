df27aa8228c235f51a2bee288f12ae34eacac6a4
Marco <marcomoa@hotmail.com>
2024-08-19 11:28:01 -0300

    fix: change log level to debug and improve message (#2803)

--- data/scripts/systems/reward_chest.lua
+++ data/scripts/systems/reward_chest.lua
@@ -12,9 +12,9 @@ function bossDeath.onDeath(creature, corpse, killer, mostDamageKiller, lastHitUn
 	if monsterType and monsterType:isRewardBoss() then
 		if not corpse or not corpse.isContainer or not corpse:isContainer() then
 			if corpse.getId then
-				logger.warn("[bossDeath.onDeath] Corpse (id: {}, name: {}) for reward boss {} is not a container.", corpse:getId(), corpse:getName(), creature:getName())
+				logger.debug("[bossDeath.onDeath] Boss {} has a corpse (id: {}, name: {}), but it is not a container.", creature:getName(), corpse:getId(), corpse:getName())
 			else
-				logger.warn("[bossDeath.onDeath] Error to get corpseId from boss: {}", creature:getName())
+				logger.debug("[bossDeath.onDeath] Boss {} does not have a corpse or corpse not found at position {}", creature:getName(), creature:getPosition())
 			end
 			corpse = Game.createItem(ITEM_BAG, 1)
 		end

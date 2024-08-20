9ab641f124312ce488f654584afe366b09c4bf61
murilo09 <78226931+murilo09@users.noreply.github.com>
2024-08-20 17:59:23 -0300

    fix: store history

--- data-otservbr-global/migrations/46.lua
+++ data-otservbr-global/migrations/46.lua
@@ -1,3 +1,10 @@
 function onUpdateDatabase()
-	return false -- true = There are others migrations file | false = this is the last migration file
+	logger.info("Updating database to version 46 (feat: gamestore cpp)")
+
+	db.query([[
+			ALTER TABLE `store_history`
+			ADD `type` smallint(2) UNSIGNED NOT NULL DEFAULT '0'
+		]])
+
+	return true
 end

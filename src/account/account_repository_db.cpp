ad30cf0dc3ffcfb149dbe1183c1a03b3d4dda946
Pedro Henrique Alves Cruz <phac@cin.ufpe.br>
2024-08-27 10:41:07 -0300

    fix: avoid pulling all columns from db

--- src/account/account_repository_db.cpp
+++ src/account/account_repository_db.cpp
@@ -65,7 +65,7 @@ bool AccountRepositoryDB::save(const AccountInfo &accInfo) {
 };
 
 bool AccountRepositoryDB::getCharacterByNameAndAccountId(const uint32_t &id, const std::string &name) {
-	auto result = g_database().storeQuery(fmt::format("SELECT * FROM `players` WHERE `account_id` = {} AND `name` = {}", id, g_database().escapeString(name)));
+	auto result = g_database().storeQuery(fmt::format("SELECT `id` FROM `players` WHERE `account_id` = {} AND `name` = {}", id, g_database().escapeString(name)));
 	if (!result) {
 		g_logger().error("Failed to get character: [{}] from account: [{}]!", name, id);
 		return false;

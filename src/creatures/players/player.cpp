432524045fa5c424c2d55c68bdc692f9e3f3100e
Carlos Pessoa <pessoacarlos98@gmail.com>
2024-09-16 21:53:37 -0300

    fix: prevent sell items with duration

--- src/creatures/players/player.cpp
+++ src/creatures/players/player.cpp
@@ -4120,6 +4120,9 @@ std::map<uint32_t, uint32_t> &Player::getAllItemTypeCount(std::map<uint32_t, uin
 
 std::map<uint16_t, uint16_t> &Player::getAllSaleItemIdAndCount(std::map<uint16_t, uint16_t> &countMap) const {
 	for (const auto &item : getAllInventoryItems(false, true)) {
+		if(item->hasAttribute(ItemAttribute_t::DURATION)){
+			continue;
+		}
 		countMap[item->getID()] += item->getItemCount();
 	}
 

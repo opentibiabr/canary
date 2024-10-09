adbe715d17deb26c6ffe4bb21e1d751b404569ba
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2024-10-09 16:18:53 -0300

    fix: overload when setting item decay when player logs in (#2927)
    This change optimizes the decay initiation process for items within
    containers. Previously, `Container::startDecaying()` method would
    recursively start the decay for each item in a container upon the
    container's own decay initiation. This could lead to inefficiencies,
    especially with many nested containers. The updated
    approach collects all decaying items in a vector first and then
    initiates decay after all have been added, improving performance by
    reducing overhead and simplifying the management of decay states.
    
    In the stop decay process, instead of iterating through all containers, we will now halt the decay immediately after each item is saved. This approach leverages an existing function, eliminating the need for an additional overlay and enhancing efficiency.

--- src/io/functions/iologindata_load_player.cpp
+++ src/io/functions/iologindata_load_player.cpp
@@ -475,26 +475,24 @@ void IOLoginDataLoad::loadPlayerInventoryItems(std::shared_ptr<Player> player, D
 	}
 
 	bool oldProtocol = g_configManager().getBoolean(OLD_PROTOCOL, __FUNCTION__) && player->getProtocolVersion() < 1200;
-	Database &db = Database::getInstance();
-	std::ostringstream query;
-	query << "SELECT `pid`, `sid`, `itemtype`, `count`, `attributes` FROM `player_items` WHERE `player_id` = " << player->getGUID() << " ORDER BY `sid` DESC";
+	auto query = fmt::format("SELECT pid, sid, itemtype, count, attributes FROM player_items WHERE player_id = {} ORDER BY sid DESC", player->getGUID());
 
 	ItemsMap inventoryItems;
 	std::vector<std::pair<uint8_t, std::shared_ptr<Container>>> openContainersList;
+	std::vector<std::shared_ptr<Item>> itemsToStartDecaying;
 
 	try {
-		if ((result = db.storeQuery(query.str()))) {
+		if ((result = g_database().storeQuery(query))) {
 			loadItems(inventoryItems, result, player);
 
 			for (ItemsMap::const_reverse_iterator it = inventoryItems.rbegin(), end = inventoryItems.rend(); it != end; ++it) {
 				const std::pair<std::shared_ptr<Item>, int32_t> &pair = it->second;
-				std::shared_ptr<Item> item = pair.first;
+				const std::shared_ptr<Item> &item = pair.first;
 				if (!item) {
 					continue;
 				}
 
 				int32_t pid = pair.second;
-
 				if (pid >= CONST_SLOT_FIRST && pid <= CONST_SLOT_LAST) {
 					player->internalAddThing(pid, item);
 					item->startDecaying();
@@ -504,14 +502,15 @@ void IOLoginDataLoad::loadPlayerInventoryItems(std::shared_ptr<Player> player, D
 						continue;
 					}
 
-					std::shared_ptr<Container> container = it2->second.first->getContainer();
+					const std::shared_ptr<Container> &container = it2->second.first->getContainer();
 					if (container) {
 						container->internalAddThing(item);
-						item->startDecaying();
+						// Here, the sub-containers do not yet have a parent, since the main backpack has not yet been added to the player, so we need to postpone
+						itemsToStartDecaying.emplace_back(item);
 					}
 				}
 
-				std::shared_ptr<Container> itemContainer = item->getContainer();
+				const std::shared_ptr<Container> &itemContainer = item->getContainer();
 				if (itemContainer) {
 					if (!oldProtocol) {
 						auto cid = item->getAttribute<int64_t>(ItemAttribute_t::OPENCONTAINER);
@@ -535,6 +534,11 @@ void IOLoginDataLoad::loadPlayerInventoryItems(std::shared_ptr<Player> player, D
 			}
 		}
 
+		// Now that all items and containers have been added and parent chain is established, start decay
+		for (const auto &item : itemsToStartDecaying) {
+			item->startDecaying();
+		}
+
 		if (!oldProtocol) {
 			std::ranges::sort(openContainersList.begin(), openContainersList.end(), [](const std::pair<uint8_t, std::shared_ptr<Container>> &left, const std::pair<uint8_t, std::shared_ptr<Container>> &right) {
 				return left.first < right.first;
@@ -545,8 +549,9 @@ void IOLoginDataLoad::loadPlayerInventoryItems(std::shared_ptr<Player> player, D
 				player->onSendContainer(it.second);
 			}
 		}
+
 	} catch (const std::exception &e) {
-		g_logger().error("[IOLoginDataLoad::loadPlayerInventoryItems] - Exceção durante o carregamento do inventário: {}", e.what());
+		g_logger().error("[IOLoginDataLoad::loadPlayerInventoryItems] - Exception during inventory loading: {}", e.what());
 	}
 }
 
@@ -585,11 +590,10 @@ void IOLoginDataLoad::loadPlayerDepotItems(std::shared_ptr<Player> player, DBRes
 		return;
 	}
 
-	Database &db = Database::getInstance();
 	ItemsMap depotItems;
-	std::ostringstream query;
-	query << "SELECT `pid`, `sid`, `itemtype`, `count`, `attributes` FROM `player_depotitems` WHERE `player_id` = " << player->getGUID() << " ORDER BY `sid` DESC";
-	if ((result = db.storeQuery(query.str()))) {
+	std::vector<std::shared_ptr<Item>> itemsToStartDecaying;
+	auto query = fmt::format("SELECT pid, sid, itemtype, count, attributes FROM player_depotitems WHERE player_id = {} ORDER BY sid DESC", player->getGUID());
+	if ((result = g_database().storeQuery(query))) {
 		loadItems(depotItems, result, player);
 		for (ItemsMap::const_reverse_iterator it = depotItems.rbegin(), end = depotItems.rend(); it != end; ++it) {
 			const std::pair<std::shared_ptr<Item>, int32_t> &pair = it->second;
@@ -611,10 +615,16 @@ void IOLoginDataLoad::loadPlayerDepotItems(std::shared_ptr<Player> player, DBRes
 				std::shared_ptr<Container> container = it2->second.first->getContainer();
 				if (container) {
 					container->internalAddThing(item);
-					item->startDecaying();
+					// Here, the sub-containers do not yet have a parent, since the main backpack has not yet been added to the player, so we need to postpone
+					itemsToStartDecaying.emplace_back(item);
 				}
 			}
 		}
+	}
+
+	// Now that all items and containers have been added and parent chain is established, start decay
+	for (const auto &item : itemsToStartDecaying) {
+		item->startDecaying();
 	}
 }
 
@@ -624,10 +634,9 @@ void IOLoginDataLoad::loadPlayerInboxItems(std::shared_ptr<Player> player, DBRes
 		return;
 	}
 
-	Database &db = Database::getInstance();
-	std::ostringstream query;
-	query << "SELECT `pid`, `sid`, `itemtype`, `count`, `attributes` FROM `player_inboxitems` WHERE `player_id` = " << player->getGUID() << " ORDER BY `sid` DESC";
-	if ((result = db.storeQuery(query.str()))) {
+	std::vector<std::shared_ptr<Item>> itemsToStartDecaying;
+	auto query = fmt::format("SELECT pid, sid, itemtype, count, attributes FROM player_inboxitems WHERE player_id = {} ORDER BY sid DESC", player->getGUID());
+	if ((result = g_database().storeQuery(query))) {
 		ItemsMap inboxItems;
 		loadItems(inboxItems, result, player);
 
@@ -647,10 +656,15 @@ void IOLoginDataLoad::loadPlayerInboxItems(std::shared_ptr<Player> player, DBRes
 				std::shared_ptr<Container> container = it2->second.first->getContainer();
 				if (container) {
 					container->internalAddThing(item);
-					item->startDecaying();
+					itemsToStartDecaying.emplace_back(item);
 				}
 			}
 		}
+	}
+
+	// Now that all items and containers have been added and parent chain is established, start decay
+	for (const auto &item : itemsToStartDecaying) {
+		item->startDecaying();
 	}
 }
 

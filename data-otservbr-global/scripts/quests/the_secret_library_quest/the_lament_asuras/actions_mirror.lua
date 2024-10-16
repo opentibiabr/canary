ec0ec389ea5cbdba8f9327a1260e9d64cd897461
HT Cesta <58153179+htc16@users.noreply.github.com>
2024-10-16 10:30:33 -0300

    The Secret Library Quest - more

--- data-otservbr-global/scripts/quests/the_secret_library_quest/the_lament_asuras/actions_mirror.lua
+++ data-otservbr-global/scripts/quests/the_secret_library_quest/the_lament_asuras/actions_mirror.lua
@@ -4,11 +4,11 @@ local actions_asura_mirror = Action()
 
 function actions_asura_mirror.onUse(player, item, fromPosition, itemEx, toPosition)
 	if player:getStorageValue(Storage.Quest.U11_80.TheSecretLibrary.Asuras.FlammingOrchid) >= 1 and player:getStorageValue(Storage.Quest.U11_80.TheSecretLibrary.Asuras.Questline) >= 1 then
-		if player:getvalue() >= 250 then
+		if player:getLevel() >= 250 then
 			player:teleportTo(goPos)
 			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 		else
-			player:sendCancelMessage("You do not have enough value.")
+			player:sendCancelMessage("You do not have enough level.")
 		end
 	else
 		player:sendCancelMessage("You do not have permission.")

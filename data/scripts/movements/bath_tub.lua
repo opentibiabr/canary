bb6862c963aacb009ba01e67396331b1c12f0124
Marco <marcomoa@hotmail.com>
2024-10-24 15:37:55 -0300

    fix: bath tube for custom data

--- data/scripts/movements/bath_tub.lua
+++ data/scripts/movements/bath_tub.lua
@@ -1,35 +1,38 @@
-local playerBathTub = 26087
-
 local bathtubEnter = MoveEvent()
 
 function bathtubEnter.onStepIn(creature, item, position, fromPosition)
-	if not creature:isPlayer() then
-		return false
+	local player = creature:getPlayer()
+	if not player then
+		return true
 	end
 
 	local condition = Condition(CONDITION_OUTFIT)
-	condition:setOutfit({ lookTypeEx = playerBathTub })
+	condition:setOutfit({ lookTypeEx = 26087 })
 	condition:setTicks(-1)
 
 	position:sendMagicEffect(CONST_ME_WATERSPLASH)
 	item:transform(BATHTUB_FILLED_NOTMOVABLE)
-	creature:addCondition(condition)
+	player:addCondition(condition)
 	return true
 end
 
+bathtubEnter:type("stepin")
 bathtubEnter:id(BATHTUB_FILLED)
 bathtubEnter:register()
 
 local bathtubExit = MoveEvent()
+
 function bathtubExit.onStepOut(creature, item, position, fromPosition)
-	if not creature:isPlayer() then
-		return false
+	local player = creature:getPlayer()
+	if not player then
+		return true
 	end
 
 	item:transform(BATHTUB_FILLED)
-	creature:removeCondition(CONDITION_OUTFIT)
+	player:removeCondition(CONDITION_OUTFIT)
 	return true
 end
 
+bathtubExit:type("stepout")
 bathtubExit:id(BATHTUB_FILLED_NOTMOVABLE)
 bathtubExit:register()

08c1eadf0b7368d13b6394d681776cfbc023c9ce
Marco <marcomoa@hotmail.com>
2024-10-30 17:32:59 -0300

    improvement: addMounts command to assign mounts to players

--- data/scripts/talkactions/god/add_mounts.lua
+++ data/scripts/talkactions/god/add_mounts.lua
@@ -1,32 +1,52 @@
--- /addmounts playername
+local addMounts = TalkAction("/addmount")
 
-local mounts = TalkAction("/addmounts")
-function mounts.onSay(player, words, param)
-	local target
+function addMounts.onSay(player, words, param)
+	-- Create log
+	logCommand(player, words, param)
+
 	if param == "" then
-		target = player:getTarget()
-		if not target then
-			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Unlocks all mounts for certain player. Usage: /mounts <player name>")
-			return true
-		end
-	else
-		target = Player(param)
-	end
-
-	if not target then
-		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Player " .. param .. " is not currently online.")
+		player:sendCancelMessage("Command param required.")
 		return true
 	end
 
-	for i = 1, 221 do
-		target:addMount(i)
+	local split = param:split(",")
+	if #split < 2 then
+		player:sendCancelMessage("Usage: /addmount <playername>, <mount id or 'all'>")
+		return true
 	end
 
-	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "All mounts unlocked for: " .. target:getName())
-	target:sendTextMessage(MESSAGE_EVENT_ADVANCE, "All of your mounts have been unlocked!")
+	local playerName = split[1]
+	local target = Player(playerName)
+
+	if not target then
+		player:sendCancelMessage("Player not found.")
+		return true
+	end
+
+	local mountParam = string.trim(split[2])
+	if mountParam == "all" then
+		for mountId = 1, 231 do
+			target:addMount(mountId)
+		end
+
+		target:sendTextMessage(MESSAGE_EVENT_ADVANCE, string.format("%s has added all mounts to you.", player:getName()))
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, string.format("You have successfully added all mounts to player %s.", target:getName()))
+	else
+		local mountId = tonumber(mountParam)
+		if not mountId then
+			player:sendCancelMessage("Invalid mount ID.")
+			return true
+		end
+
+		target:addMount(mountId)
+		target:sendTextMessage(MESSAGE_EVENT_ADVANCE, string.format("%s has added a new mount for you.", player:getName()))
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, string.format("You have successfully added mount %d to player %s.", mountId, target:getName()))
+	end
+
+	logger.debug("[addMounts.onSay] - Player: {} has added mount: {} to the player: {}", player:getName(), mountParam, target:getName())
 	return true
 end
 
-mounts:separator(" ")
-mounts:groupType("god")
-mounts:register()
+addMounts:separator(" ")
+addMounts:groupType("god")
+addMounts:register()

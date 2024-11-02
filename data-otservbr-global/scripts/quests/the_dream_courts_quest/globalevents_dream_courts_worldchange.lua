89fb651640b0990f81129614b5af0a2987101dc5
HT Cesta <58153179+htc16@users.noreply.github.com>
2024-11-02 12:33:54 -0300

    Update globalevents_dream_courts_worldchange.lua

--- data-otservbr-global/scripts/quests/the_dream_courts_quest/globalevents_dream_courts_worldchange.lua
+++ data-otservbr-global/scripts/quests/the_dream_courts_quest/globalevents_dream_courts_worldchange.lua
@@ -14,10 +14,10 @@ local globalevents_dream_courts_worldchange = GlobalEvent("startupCourts")
 
 function globalevents_dream_courts_worldchange.onStartup(interval)
 	if spawnByDay then
-		print(">> [dream courts] loaded: " .. config[os.date("%A")])
+		logger.info("[The Dream Courts Quest] loaded: " .. config[os.date("%A")])
 		Game.loadMap("data/world/worldchanges/dream_courts_bosses/" .. config[os.date("%A")] .. ".otbm")
 	else
-		print(">> dream courts boss: not boss today")
+		logger.warn("[The Dream Courts Quest] bosses: not boss today")
 	end
 	return true
 end
@@ -28,10 +28,10 @@ local globalevents_dream_courts_worldchange = GlobalEvent("fixCourts")
 
 function globalevents_dream_courts_worldchange.onTime(interval)
 	if spawnByDay then
-		print(">> [dream courts] loaded: " .. config[os.date("%A")])
+		logger.info("[The Dream Courts Quest] loaded: " .. config[os.date("%A")])
 		Game.loadMap("data/world/worldchanges/dream_courts_bosses/" .. config[os.date("%A")] .. ".otbm")
 	else
-		print(">> dream courts boss: not boss today")
+		logger.warn("[The Dream Courts Quest] bosses: not boss today")
 	end
 
 	return true

83fe3119615b3fefec56e11872597ecd5f1e3e69
Renato Machado <mehahx@gmail.com>
2024-11-08 20:41:45 -0300

    init

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -7431,7 +7431,6 @@ bool Game::combatChangeHealth(const std::shared_ptr<Creature> &attacker, const s
 
 			if (targetMonster->israndomStepping()) {
 				targetMonster->setIgnoreFieldDamage(true);
-				targetMonster->updateMapCache();
 			}
 		}
 

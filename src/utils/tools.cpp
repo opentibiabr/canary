1382b7767907d55ce7f19753de4de4cf2cefa981
Pedro Cruz <phac@cin.ufpe.br>
2024-11-12 22:06:10 -0300

    fix: wheel bonuses (#3058)
    This fixes all vocations wheel spell and gem bonuses, const references
    and refactors.
    
    Fixes #3032 
    Fixes #2150 
    Fixes #2039

--- src/utils/tools.cpp
+++ src/utils/tools.cpp
@@ -1583,6 +1583,12 @@ SpellGroup_t stringToSpellGroup(const std::string &value) {
 	if (tmpStr == "ultimatestrikes" || tmpStr == "8") {
 		return SPELLGROUP_ULTIMATESTRIKES;
 	}
+	if (tmpStr == "burstsofnature" || tmpStr == "9") {
+		return SPELLGROUP_BURSTS_OF_NATURE;
+	}
+	if (tmpStr == "greatbeams" || tmpStr == "10") {
+		return SPELLGROUP_GREAT_BEAMS;
+	}
 
 	return SPELLGROUP_NONE;
 }

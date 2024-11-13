a72c5d05bfc603b507dea363edd9d5831f65139d
murilo09 <78226931+murilo09@users.noreply.github.com>
2024-11-13 00:02:48 -0300

    refactor: move blessing bytes to cpp (#3064)
    Moving bytes from modules to cpp.

--- src/creatures/players/player.cpp
+++ src/creatures/players/player.cpp
@@ -3450,7 +3450,6 @@ void Player::death(const std::shared_ptr<Creature> &lastHitCreature) {
 
 	g_game().sendSingleSoundEffect(static_self_cast<Player>()->getPosition(), sex == PLAYERSEX_FEMALE ? SoundEffect_t::HUMAN_FEMALE_DEATH : SoundEffect_t::HUMAN_MALE_DEATH, getPlayer());
 	if (skillLoss) {
-		uint8_t unfairFightReduction = 100;
 		int playerDmg = 0;
 		int othersDmg = 0;
 		uint32_t sumLevels = 0;
@@ -3467,10 +3466,13 @@ void Player::death(const std::shared_ptr<Creature> &lastHitCreature) {
 				}
 			}
 		}
+
 		bool pvpDeath = false;
 		if (playerDmg > 0 || othersDmg > 0) {
 			pvpDeath = (Player::lastHitIsPlayer(lastHitCreature) || playerDmg / (playerDmg + static_cast<double>(othersDmg)) >= 0.05);
 		}
+
+		uint8_t unfairFightReduction = 100;
 		if (pvpDeath && sumLevels > level) {
 			double reduce = level / static_cast<double>(sumLevels);
 			unfairFightReduction = std::max<uint8_t>(20, std::floor((reduce * 100) + 0.5));
@@ -3480,7 +3482,7 @@ void Player::death(const std::shared_ptr<Creature> &lastHitCreature) {
 		uint64_t sumMana = 0;
 		uint64_t lostMana = 0;
 
-		// sum up all the mana
+		// Sum up all the mana
 		for (uint32_t i = 1; i <= magLevel; ++i) {
 			sumMana += vocation->getReqMana(i);
 		}
@@ -3490,12 +3492,10 @@ void Player::death(const std::shared_ptr<Creature> &lastHitCreature) {
 		double deathLossPercent = getLostPercent() * (unfairFightReduction / 100.);
 
 		// Charm bless bestiary
-		if (lastHitCreature && lastHitCreature->getMonster()) {
-			if (charmRuneBless != 0) {
-				const auto mType = g_monsters().getMonsterType(lastHitCreature->getName());
-				if (mType && mType->info.raceid == charmRuneBless) {
-					deathLossPercent = (deathLossPercent * 90) / 100;
-				}
+		if (lastHitCreature && lastHitCreature->getMonster() && charmRuneBless != 0) {
+			const auto &mType = g_monsters().getMonsterType(lastHitCreature->getName());
+			if (mType && mType->info.raceid == charmRuneBless) {
+				deathLossPercent = (deathLossPercent * 90) / 100;
 			}
 		}
 
@@ -3517,7 +3517,9 @@ void Player::death(const std::shared_ptr<Creature> &lastHitCreature) {
 		}
 
 		// Level loss
-		auto expLoss = static_cast<uint64_t>(experience * deathLossPercent);
+		auto expLoss = static_cast<uint64_t>(std::ceil((experience * deathLossPercent) / 100.));
+		g_logger().debug("[{}] - experience lost {}", __FUNCTION__, expLoss);
+
 		g_events().eventPlayerOnLoseExperience(static_self_cast<Player>(), expLoss);
 		g_callbacks().executeCallback(EventCallback_t::playerOnLoseExperience, &EventCallback::playerOnLoseExperience, getPlayer(), expLoss);
 
@@ -3526,9 +3528,9 @@ void Player::death(const std::shared_ptr<Creature> &lastHitCreature) {
 		lostExp << "You lost " << expLoss << " experience.";
 
 		// Skill loss
-		for (uint8_t i = SKILL_FIRST; i <= SKILL_LAST; ++i) { // for each skill
+		for (uint8_t i = SKILL_FIRST; i <= SKILL_LAST; ++i) { // For each skill
 			uint64_t sumSkillTries = 0;
-			for (uint16_t c = 11; c <= skills[i].level; ++c) { // sum up all required tries for all skill levels
+			for (uint16_t c = 11; c <= skills[i].level; ++c) { // Sum up all required tries for all skill levels
 				sumSkillTries += vocation->getReqSkillTries(i, c);
 			}
 
@@ -3604,13 +3606,20 @@ void Player::death(const std::shared_ptr<Creature> &lastHitCreature) {
 			bless.empty() ? blessOutput << "You weren't protected with any blessings."
 						  : blessOutput << "You were blessed with " << bless;
 
-			// Make player lose bless
+			const auto playerSkull = getSkull();
+			bool hasSkull = (playerSkull == Skulls_t::SKULL_RED || playerSkull == Skulls_t::SKULL_BLACK);
 			uint8_t maxBlessing = 8;
-			if (pvpDeath && hasBlessing(1)) {
+			if (!hasSkull && pvpDeath && hasBlessing(1)) {
 				removeBlessing(1, 1); // Remove TOF only
 			} else {
 				for (int i = 2; i <= maxBlessing; i++) {
 					removeBlessing(i, 1);
+				}
+
+				const auto &playerAmulet = getThing(CONST_SLOT_NECKLACE);
+				bool usingAol = (playerAmulet && playerAmulet->getItem()->getID() == ITEM_AMULETOFLOSS);
+				if (usingAol) {
+					removeItemOfType(ITEM_AMULETOFLOSS, 1, -1);
 				}
 			}
 		}
@@ -6293,21 +6302,30 @@ double Player::getLostPercent() const {
 		return std::max<int32_t>(0, deathLosePercent) / 100.;
 	}
 
+	bool isRetro = g_configManager().getBoolean(TOGGLE_SERVER_IS_RETRO);
+	const auto factor = (isRetro ? 6.31 : 8);
+	double percentReduction = (blessingCount * factor) / 100.;
+
 	double lossPercent;
 	if (level >= 24) {
 		const double tmpLevel = level + (levelPercent / 100.);
 		lossPercent = ((tmpLevel + 50) * 50 * ((tmpLevel * tmpLevel) - (5 * tmpLevel) + 8)) / experience;
 	} else {
-		lossPercent = 5;
+		percentReduction = (percentReduction >= 0.40 ? 0.50 : percentReduction);
+		lossPercent = 10;
 	}
 
-	double percentReduction = 0;
+	g_logger().debug("[{}] - lossPercent {}", __FUNCTION__, lossPercent);
+	g_logger().debug("[{}] - before promotion {}", __FUNCTION__, percentReduction);
+
 	if (isPromoted()) {
-		percentReduction += 30;
+		percentReduction += 30 / 100.;
+		g_logger().debug("[{}] - after promotion {}", __FUNCTION__, percentReduction);
 	}
 
-	percentReduction += blessingCount * 8;
-	return lossPercent * (1 - (percentReduction / 100.)) / 100.;
+	g_logger().debug("[{}] - total lost percent {}", __FUNCTION__, lossPercent - (lossPercent * percentReduction));
+
+	return lossPercent - (lossPercent * percentReduction);
 }
 
 [[nodiscard]] const std::string &Player::getGuildNick() const {

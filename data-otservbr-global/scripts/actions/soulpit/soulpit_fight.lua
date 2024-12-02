3b2f4ed61c4701bb7bd183e3f81d1774ba9de4cc
Pedro Henrique Alves Cruz <phac@cin.ufpe.br>
2024-12-02 18:35:59 -0300

    fix: soulpit fight

--- data-otservbr-global/scripts/actions/soulpit/soulpit_fight.lua
+++ data-otservbr-global/scripts/actions/soulpit/soulpit_fight.lua
@@ -40,15 +40,75 @@ local config = {
 		[15] = CONST_ME_REDTELEPORT,
 		[40] = CONST_ME_PURPLETELEPORT,
 	},
+	bossAbilitiesIndexes = {},
+	bossAbilities = {
+		["enrage"] = {
+			monsterEvents = {
+				"enrageSoulPit",
+			},
+		},
+		["opressor"] = {
+			spells = {
+				{ name = "soulpit opressor", interval = 2000, chance = 25, minDamage = 0, maxDamage = 0 },
+				{ name = "soulpit powerless", interval = 2000, chance = 30, minDamage = 0, maxDamage = 0 },
+				{ name = "soulpit hexer", interval = 2000, chance = 15, minDamage = 0, maxDamage = 0 },
+			},
+		},
+		["overpower"] = {
+			playerEvents = {
+				"overpowerSoulPit"
+			},
+		},
+	},
 	checkMonstersDelay = 4.5 * 1000, -- 4.5 seconds | The check delay should never be less than the timeToSpawnMonsters.
 	timeToSpawnMonsters = 4 * 1000, -- 4 seconds
 	totalMonsters = 7,
 	bossPosition = Position(32372, 31135, 8),
-	exit = Position(33659, 32897, 14),
+	exit = Position(32371, 31164, 8),
 	zone = Zone("soulpit"),
 }
 
+for name, _ in pairs(config.bossAbilities) do
+	table.insert(config.bossAbilitiesIndexes, name)
+	logger.warn(name)
+end
+
 config.zone:addArea(Position(32365, 31134, 8), Position(32382, 31152, 8))
+
+local enrage = CreatureEvent("enrageSoulPit")
+function enrage.onHealthChange(creature, attacker, primaryDamage, primaryType, secondaryDamage, secondaryType)
+	if not creature or not creature:isMonster() then
+		return true
+	end
+
+	local healthPercentage = creature:getHealth() / creature:getMaxHealth()
+
+	if healthPercentage >= 0.5 and healthPercentage <= 0.7 then
+		primaryDamage = primaryDamage * 0.9 -- 10% damage reduction
+		secondaryDamage = secondaryDamage * 0.9 -- 10% damage reduction
+	elseif healthPercentage >= 0.3 and healthPercentage < 0.5 then
+		primaryDamage = primaryDamage * 0.75 -- 25% damage reduction
+		secondaryDamage = secondaryDamage * 0.75 -- 25% damage reduction
+	elseif healthPercentage > 0 and healthPercentage < 0.3 then
+		primaryDamage = primaryDamage * 0.6 -- 40% damage reduction
+		secondaryDamage = secondaryDamage * 0.6 -- 40% damage reduction
+	end
+
+	return primaryDamage, primaryType, secondaryDamage, secondaryType
+end
+enrage:register()
+
+local overpower = CreatureEvent("overpowerSoulPit")
+function overpower.onHealthChange(creature, attacker, primaryDamage, primaryType, secondaryDamage, secondaryType)
+	if attacker:getForgeStack() == 40 then
+		primaryDamage = primaryDamage * 1.1
+		secondaryDamage = secondaryDamage * 1.1
+		creature:getPosition():sendMagicEffect(CONST_ME_CRITICAL_DAMAGE)
+	end
+
+	return primaryDamage, primaryType, secondaryDamage, secondaryType
+end
+overpower:register()
 
 local soulPitAction = Action()
 function soulPitAction.onUse(player, item, fromPosition, target, toPosition, isHotkey)
@@ -122,6 +182,30 @@ function soulPitAction.onUse(player, item, fromPosition, target, toPosition, isH
 					monster:removeIcon("forge")
 					monster:setIcon("soulpit", CreatureIconCategory_Modifications, icon, stack <= 15 and stack or 0)
 					monster:setDropLoot(false)
+					if stack == 40 then
+						local monsterType = monster:getType()
+						local randomAbility = bossAbilitiesIndexes[math.random(#bossAbilitiesIndexes)]
+						logger.warn("the random ability chosen was: " .. randomAbility)
+						for _, ability in pairs(config.bossAbilities[randomAbility]) do
+							if ability.spells then
+								for _, spell in pairs(ability.spells) do
+									monsterType:addAttack(readSpell(spell, monsterType))
+								end
+							end
+							if ability.monsterEvents then
+								for _, name in pairs(ability.monsterEvents) do
+									monster:registerEvent(name)
+								end
+							end
+							if ability.playerEvents then
+								for _, name in pairs(ability.playerEvents) do
+									for _, players in pairs(config.zone:getPlayers()) do
+										player:registerEvent(name)
+									end
+								end
+							end
+						end
+					end
 				end, config.timeToSpawnMonsters, monsterName, stack, position)
 			end
 		end
@@ -131,6 +215,20 @@ function soulPitAction.onUse(player, item, fromPosition, target, toPosition, isH
 		encounter:addStage({
 		start = waveStart,
 		}):autoAdvance({ delay = config.checkMonstersDelay, monstersKilled = true })
+	end
+
+	function encounter:onReset(position)
+		for _, player in pairs(config.zone:getPlayers()) do
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, string.format("You have defeated the core of the %s soul and unlocked its animus mastery!", monsterName))
+			player:teleportTo(config.exit)
+			for abilityName, abilities in pairs(config.bossAbilities) do
+				if abilities.playerEvents then
+					for _, eventName in pairs(abilities.playerEvents) do
+						player:registerEvent(eventName)
+					end
+				end
+			end
+		end
 	end
 
 	encounter:start()

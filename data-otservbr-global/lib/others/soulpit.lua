dd2f9672cf86dcc2b8b6f52328ef1398062c8813
Felipe Paluco <fepaluco@gmail.com>
2024-12-04 14:42:23 -0300

    feat: add upgrade soul prism mechanic

--- data-otservbr-global/lib/others/soulpit.lua
+++ data-otservbr-global/lib/others/soulpit.lua
@@ -1,65 +1,110 @@
 SoulPit = {
+	SoulCoresConfiguration = {
+		chanceToGetSameMonsterSoulCore = 30, -- 30%
+		chanceToDropSoulCore = 50, -- 50%
+		chanceToGetOminousSoulCore = 5, -- 5%
+		monsterVariationsSoulCore = {
+			["Horse"] = "horse soul core (taupe)",
+			["Brown Horse"] = "horse soul core (brown)",
+			["Grey Horse"] = "horse soul core (gray)",
+			["Nomad"] = "nomad soul core (basic)",
+			["Nomad Blue"] = "nomad soul core (blue)",
+			["Nomad Female"] = "nomad soul core (female)",
+			["Purple Butterfly"] = "butterfly soul core (purple)",
+			["Butterfly"] = "butterfly soul core (blue)",
+			["Blue Butterfly"] = "butterfly soul core (blue)",
+			["Red Butterfly"] = "butterfly soul core (red)"
+		},
+		monstersDifficulties = {
+			["Harmless"] = 1,
+			["Trivial"] = 2,
+			["Easy"] = 3,
+			["Medium"] = 4,
+			["Hard"] = 5,
+			["Challenge"] = 6
+		}
+	},
 	encounter = nil,
 	kickEvent = nil,
 	soulCores = Game.getSoulCoreItems(),
 	requiredLevel = 8,
 	playerPositions = {
-		{ pos = Position(32371, 31155, 8), teleport = Position(32373, 31138, 8), effect = CONST_ME_TELEPORT },
-		{ pos = Position(32371, 31156, 8), teleport = Position(32373, 31138, 8), effect = CONST_ME_TELEPORT },
-		{ pos = Position(32371, 31157, 8), teleport = Position(32373, 31138, 8), effect = CONST_ME_TELEPORT },
-		{ pos = Position(32371, 31158, 8), teleport = Position(32373, 31138, 8), effect = CONST_ME_TELEPORT },
-		{ pos = Position(32371, 31159, 8), teleport = Position(32373, 31138, 8), effect = CONST_ME_TELEPORT },
+		{
+			pos = Position(32371, 31155, 8),
+			teleport = Position(32373, 31138, 8),
+			effect = CONST_ME_TELEPORT
+		},
+		{
+			pos = Position(32371, 31156, 8),
+			teleport = Position(32373, 31138, 8),
+			effect = CONST_ME_TELEPORT
+		},
+		{
+			pos = Position(32371, 31157, 8),
+			teleport = Position(32373, 31138, 8),
+			effect = CONST_ME_TELEPORT
+		},
+		{
+			pos = Position(32371, 31158, 8),
+			teleport = Position(32373, 31138, 8),
+			effect = CONST_ME_TELEPORT
+		},
+		{
+			pos = Position(32371, 31159, 8),
+			teleport = Position(32373, 31138, 8),
+			effect = CONST_ME_TELEPORT
+		}
 	},
 	waves = {
 		[1] = {
 			stacks = {
-				[1] = 7,
-			},
+				[1] = 7
+			}
 		},
 		[2] = {
 			stacks = {
 				[1] = 4,
-				[5] = 3,
-			},
+				[5] = 3
+			}
 		},
 		[3] = {
 			stacks = {
 				[1] = 5,
-				[15] = 2,
-			},
+				[15] = 2
+			}
 		},
 		[4] = {
 			stacks = {
 				[1] = 3,
 				[5] = 3,
-				[40] = 1,
-			},
-		},
+				[40] = 1
+			}
+		}
 	},
 	effects = {
 		[1] = CONST_ME_TELEPORT,
 		[5] = CONST_ME_ORANGETELEPORT,
 		[15] = CONST_ME_REDTELEPORT,
-		[40] = CONST_ME_PURPLETELEPORT,
+		[40] = CONST_ME_PURPLETELEPORT
 	},
 	possibleAbilities = {
 		"overpowerSoulPit",
 		"enrageSoulPit",
-		"opressorSoulPit",
+		"opressorSoulPit"
 	},
 	bossAbilities = {
-		["overpowerSoulPit"] = {
+		overpowerSoulPit = {
 			player = true,
-			monster = false,
+			monster = false
 		},
-		["enrageSoulPit"] = {
+		enrageSoulPit = {
 			player = false,
-			monster = true,
+			monster = true
 		},
-		["opressorSoulPit"] = {
+		opressorSoulPit = {
 			player = false,
-			monster = true,
-		},
+			monster = true
+		}
 	},
 	timeToKick = 3 * 1000, -- 3 seconds
 	checkMonstersDelay = 4.5 * 1000, -- 4.5 seconds | The check delay should never be less than the timeToSpawnMonsters.

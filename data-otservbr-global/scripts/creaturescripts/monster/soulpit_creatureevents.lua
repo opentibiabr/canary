9f654d05c6a72287b773da521deb7331150c3c8f
Pedro Henrique Alves Cruz <phac@cin.ufpe.br>
2024-12-05 19:58:12 -0300

    improve: customizable bosses abilities

--- data-otservbr-global/scripts/creaturescripts/monster/soulpit_creatureevents.lua
+++ data-otservbr-global/scripts/creaturescripts/monster/soulpit_creatureevents.lua
@@ -6,18 +6,10 @@ function enrage.onHealthChange(creature, attacker, primaryDamage, primaryType, s
 
 	local healthPercentage = creature:getHealth() / creature:getMaxHealth()
 
-	if healthPercentage >= 0.6 and healthPercentage <= 0.8 then
-		primaryDamage = primaryDamage * 0.9 -- 10% damage reduction
-		secondaryDamage = secondaryDamage * 0.9 -- 10% damage reduction
-	elseif healthPercentage >= 0.4 and healthPercentage < 0.6 then
-		primaryDamage = primaryDamage * 0.75 -- 25% damage reduction
-		secondaryDamage = secondaryDamage * 0.75 -- 25% damage reduction
-	elseif healthPercentage >= 0.2 and healthPercentage < 0.4 then
-		primaryDamage = primaryDamage * 0.6 -- 40% damage reduction
-		secondaryDamage = secondaryDamage * 0.6 -- 40% damage reduction
-	elseif healthPercentage > 0 and healthPercentage < 0.2 then
-		primaryDamage = primaryDamage * 0.4 -- 60% damage reduction
-		secondaryDamage = secondaryDamage * 0.4 -- 60% damage reduction
+	for bounds, reduction in pairs(SoulPit.bossAbilities.enrageSoulPit.bounds) do
+		if healthPercentage > bounds[2] and healthPercentage <= bounds[1] then
+			return primaryDamage * reduction, primaryType, secondaryDamage * reduction, secondaryType
+		end
 	end
 
 	return primaryDamage, primaryType, secondaryDamage, secondaryType
@@ -27,8 +19,9 @@ enrage:register()
 local overpower = CreatureEvent("overpowerSoulPit")
 function overpower.onHealthChange(creature, attacker, primaryDamage, primaryType, secondaryDamage, secondaryType)
 	if creature and attacker and attacker:isMonster() and attacker:getForgeStack() == 40 and not attacker:getMaster() then
-		primaryDamage = primaryDamage * 1.1
-		secondaryDamage = secondaryDamage * 1.1
+		local bonusCriticalDamage = SoulPit.bossAbilities.overpowerSoulPit.bonusCriticalDamage
+		primaryDamage = primaryDamage * bonusCriticalDamage
+		secondaryDamage = secondaryDamage * bonusCriticalDamage
 		creature:getPosition():sendMagicEffect(CONST_ME_CRITICAL_DAMAGE)
 	end
 

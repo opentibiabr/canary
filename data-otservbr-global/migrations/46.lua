ce08db0f9d2761fd976f697a3a66891be12543d7
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2024-12-09 16:33:30 -0300

    fix: order

--- data-otservbr-global/migrations/46.lua
+++ data-otservbr-global/migrations/46.lua
@@ -1,24 +1,6 @@
 function onUpdateDatabase()
-	logger.info("Updating database to version 46 (hireling)")
+	logger.info("Updating database to version 47 (fix: creature speed and conditions)")
 
-	db.query([[
-		CREATE TABLE IF NOT EXISTS `player_hirelings` (
-            `id` INT NOT NULL PRIMARY KEY auto_increment,
-            `player_id` INT NOT NULL,
-            `name` varchar(255),
-            `active` tinyint unsigned NOT NULL DEFAULT '0',
-            `sex` tinyint unsigned NOT NULL DEFAULT '0',
-            `posx` int(11) NOT NULL DEFAULT '0',
-            `posy` int(11) NOT NULL DEFAULT '0',
-            `posz` int(11) NOT NULL DEFAULT '0',
-            `lookbody` int(11) NOT NULL DEFAULT '0',
-            `lookfeet` int(11) NOT NULL DEFAULT '0',
-            `lookhead` int(11) NOT NULL DEFAULT '0',
-            `looklegs` int(11) NOT NULL DEFAULT '0',
-            `looktype` int(11) NOT NULL DEFAULT '136',
-
-            FOREIGN KEY(`player_id`) REFERENCES `players`(`id`)
-                ON DELETE CASCADE
-		)
-	]])
+	db.query("ALTER TABLE `players` MODIFY `conditions` mediumblob NOT NULL;")
 end
+

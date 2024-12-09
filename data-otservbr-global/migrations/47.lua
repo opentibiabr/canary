b4dc46bf1b345a1dc9597ecd64d7bec5a9eea9f8
murilo09 <78226931+murilo09@users.noreply.github.com>
2024-12-09 17:24:12 -0300

    fix: files removed by resolving conflicts

--- data-otservbr-global/migrations/47.lua
+++ data-otservbr-global/migrations/47.lua
@@ -1,3 +1,29 @@
 function onUpdateDatabase()
-	return false -- true = There are others migrations file | false = this is the last migration file
+	logger.info("Updating database to version 48 (House Auction)")
+
+	db.query([[
+		ALTER TABLE `houses`
+		DROP `bid`,
+		DROP `bid_end`,
+		DROP `last_bid`,
+		DROP `highest_bidder`
+	]])
+
+	db.query([[
+		ALTER TABLE `houses`
+		ADD `bidder` int(11) NOT NULL DEFAULT '0',
+		ADD `bidder_name` varchar(255) NOT NULL DEFAULT '',
+		ADD `highest_bid` int(11) NOT NULL DEFAULT '0',
+		ADD `internal_bid` int(11) NOT NULL DEFAULT '0',
+		ADD `bid_end_date` int(11) NOT NULL DEFAULT '0',
+		ADD `state` smallint(5) UNSIGNED NOT NULL DEFAULT '0',
+		ADD `transfer_status` tinyint(1) DEFAULT '0'
+	]])
+
+	db.query([[
+		ALTER TABLE `accounts`
+		ADD `house_bid_id` int(11) NOT NULL DEFAULT '0'
+	]])
+
+	return true
 end

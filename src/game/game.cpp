55de86ee4f475925b2d3098b2d6ac6da19e17a68
murilo09 <78226931+murilo09@users.noreply.github.com>
2024-12-31 13:27:34 -0300

    fix: damage console (#3203)

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -7418,7 +7418,7 @@ bool Game::combatChangeHealth(const std::shared_ptr<Creature> &attacker, const s
 		}
 
 		auto targetHealth = target->getHealth();
-		realDamage = damage.primary.value + damage.secondary.value;
+		realDamage = std::min<int32_t>(targetHealth, damage.primary.value + damage.secondary.value);
 		if (realDamage == 0) {
 			return true;
 		} else if (realDamage >= targetHealth) {

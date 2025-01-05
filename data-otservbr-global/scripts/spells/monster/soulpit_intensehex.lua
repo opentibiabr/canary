363524a5c8a56f780a65fbff66a2f3cccadb40ba
Pedro Henrique Alves Cruz <phac@cin.ufpe.br>
2025-01-04 22:03:40 -0300

    perf: condition

--- data-otservbr-global/scripts/spells/monster/soulpit_intensehex.lua
+++ data-otservbr-global/scripts/spells/monster/soulpit_intensehex.lua
@@ -3,6 +3,8 @@ combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_STUN)
 combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_NONE)
 
 local condition = Condition(CONDITION_INTENSEHEX)
+condition:setParameter(CONDITION_PARAM_BUFF_DAMAGEDEALT, 50)
+condition:setParameter(CONDITION_PARAM_BUFF_HEALINGRECEIVED, 50)
 condition:setParameter(CONDITION_PARAM_TICKS, 3000)
 combat:addCondition(condition)
 

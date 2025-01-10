fa0a0a0e9da9c37edb71a4d4a736a5eed838ed83
Leilani A. <168607226+kaleohanopahala@users.noreply.github.com>
2025-01-10 17:28:29 -0300

    Update manage_storage.lua

--- data/scripts/talkactions/god/manage_storage.lua
+++ data/scripts/talkactions/god/manage_storage.lua
@@ -2,49 +2,49 @@ function Player.getStorageValueTalkaction(self, param)
 	-- Sanity check for parameters
 	-- Example: /getstorage god, wheel.scroll.abridged
 	-- Example: /getstorage god, 10000
-	if not HasValidTalkActionParams(self, param, "Usage: /getstorage <playername>, <storage key or name>") then
-		return true
-	end
+    if not HasValidTalkActionParams(self, param, "Usage: /getstorage <playername>, <storage key or name>") then
+        return true
+    end
 
-	local split = param:split(",")
-	if split[2] == nil then
-		player:sendCancelMessage("Insufficient parameters.")
-		return true
-	end
+    local split = param:split(",")
+    if not split[2] then
+        self:sendCancelMessage("Insufficient parameters.")
+        return true
+    end
 
-	local target = Player(split[1])
-	if target == nil then
-		self:sendCancelMessage("A player with that name is not online.")
-		return true
-	end
+    local target = Player(split[1]:trim())
+    if not target then
+        self:sendCancelMessage("A player with that name is not online.")
+        return true
+    end
 
-	split[2] = split[2]:trimSpace()
+    -- Storage key Validation
+    local storageKey = tonumber(split[2]) or split[2]:trim()
+    if not storageKey then
+        self:sendCancelMessage("Invalid storage key or name.")
+        return true
+    end
 
-	-- Try to convert the second parameter to a number. If it's not a number, treat it as a storage name
-	local storageKey = tonumber(split[2])
-	if storageKey == nil then
-		-- Get the key for this storage name
-		local storageName = tostring(split[2])
-		local storageValue = target:getStorageValueByName(storageName)
-		self:sendTextMessage(MESSAGE_EVENT_ADVANCE, "The storage with id: " .. storageName .. " from player " .. split[1] .. " is: " .. storageValue .. ".")
-	else
-		local storageValue = target:getStorageValue(storageKey)
-		self:sendTextMessage(MESSAGE_EVENT_ADVANCE, "The storage with id: " .. storageKey .. " from player " .. split[1] .. " is: " .. storageValue .. ".")
-	end
+    -- Get the storage key
+    local storageValue = target:getStorageValue(storageKey)
+    if storageValue == nil then
+        self:sendTextMessage(MESSAGE_EVENT_ADVANCE, "The storage with id: " .. split[2] .. " does not exist or is not set for player " .. target:getName() .. ".")
+    else
+        self:sendTextMessage(MESSAGE_EVENT_ADVANCE, "The storage with id: " .. split[2] .. " from player " .. target:getName() .. " is: " .. storageValue .. ".")
+    end
 
-	return true
+    return true
 end
 
 local storageGet = TalkAction("/getstorage")
 
 function storageGet.onSay(player, words, param)
-	return player:getStorageValueTalkaction(param)
+    return player:getStorageValueTalkaction(param)
 end
 
 storageGet:separator(" ")
 storageGet:groupType("gamemaster")
 storageGet:register()
-
 ---------------- // ----------------
 function Player.setStorageValueTalkaction(self, param)
 	-- Sanity check for parameters

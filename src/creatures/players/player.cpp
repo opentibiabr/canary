75ba89d563e9cb6fd022d1f6eda6c4c97c2e6745
bdzicc <52944047+bdzicc@users.noreply.github.com>
2025-03-15 13:19:21 -0400

    fix: remove player from party on death (#3384)

--- src/creatures/players/player.cpp
+++ src/creatures/players/player.cpp
@@ -3852,6 +3852,10 @@ void Player::despawn() {
 		player->vip()->notifyStatusChange(static_self_cast<Player>(), VipStatus_t::Pending, false);
 	}
 
+	if (m_party && g_configManager().getBoolean(LEAVE_PARTY_ON_DEATH)) {
+		m_party->leaveParty(static_self_cast<Player>());
+	}
+
 	setDead(true);
 }
 

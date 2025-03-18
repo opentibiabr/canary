908bcc26a92c5ee8da0dee459afdf76c08befb12
mdbeng <37564938+mdbeng@users.noreply.github.com>
2025-03-18 11:37:51 -0400

    fix: exori gran con damage bonus (#3321)
    This fixes the excessive damage output from skill wheel such as enhanced
    ethereal spear, the damage output still too high in comparison to real
    tibia. (The high output is on all the spells boosted by skill wheel )
    
    Resolves #3313

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -7227,6 +7227,17 @@ bool Game::combatChangeHealth(const std::shared_ptr<Creature> &attacker, const s
 			return false;
 		}
 
+		if (damage.origin != ORIGIN_NONE) {
+			const auto events = target->getCreatureEvents(CREATURE_EVENT_HEALTHCHANGE);
+			if (!events.empty()) {
+				for (const auto &creatureEvent : events) {
+					creatureEvent->executeHealthChange(target, attacker, damage);
+				}
+				damage.origin = ORIGIN_NONE;
+				return combatChangeHealth(attacker, target, damage);
+			}
+		}
+
 		// Wheel of destiny apply combat effects
 		applyWheelOfDestinyEffectsToDamage(damage, attackerPlayer, target);
 
@@ -7448,17 +7459,6 @@ bool Game::combatChangeHealth(const std::shared_ptr<Creature> &attacker, const s
 		auto realDamage = damage.primary.value + damage.secondary.value;
 		if (realDamage == 0) {
 			return true;
-		}
-
-		if (damage.origin != ORIGIN_NONE) {
-			const auto events = target->getCreatureEvents(CREATURE_EVENT_HEALTHCHANGE);
-			if (!events.empty()) {
-				for (const auto &creatureEvent : events) {
-					creatureEvent->executeHealthChange(target, attacker, damage);
-				}
-				damage.origin = ORIGIN_NONE;
-				return combatChangeHealth(attacker, target, damage);
-			}
 		}
 
 		// Apply Custom PvP Damage (must be placed here to avoid recursive calls)

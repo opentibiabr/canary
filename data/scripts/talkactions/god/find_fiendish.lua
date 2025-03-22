a5426ea00f598d3e6a8d5a7dadde3523ad1d7490
Leilani A. <168607226+kaleohanopahala@users.noreply.github.com>
2025-03-22 12:46:30 -0300

    Add files via upload

---
+++ data/scripts/talkactions/god/find_fiendish.lua
@@ -0,0 +1,28 @@
+local teleportToFiend = TalkAction("/gotoFiend")
+
+function teleportToFiend.onSay(player, words, param)
+    logCommand(player, words, param)
+
+    local fiendishMonsters = Game.getFiendishMonsters()
+    if #fiendishMonsters == 0 then
+        player:sendCancelMessage("No fiendish creatures found.")
+        return true
+    end
+
+    local randomIndex = math.random(1, #fiendishMonsters)
+    local monsterId = fiendishMonsters[randomIndex]
+    local monster = Creature(monsterId)
+
+    if monster then
+        local position = monster:getPosition()
+        player:teleportTo(position)
+    else
+        player:sendCancelMessage("Fiendish creature not found.")
+    end
+
+    return true
+end
+
+teleportToFiend:separator(" ")
+teleportToFiend:groupType("god")
+teleportToFiend:register()

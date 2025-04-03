58bebdc232b14a01ddd7457563e5851f6c5bcad1
HT Cesta <Heitor.cestaro@hotmail.com>
2025-04-03 10:25:10 -0300

    Update rookgaard_advance.lua

--- data-otservbr-global/scripts/creaturescripts/others/rookgaard_advance.lua
+++ data-otservbr-global/scripts/creaturescripts/others/rookgaard_advance.lua
@@ -1,15 +1,19 @@
 local rookgaardAdvance = CreatureEvent("RookgaardAdvance")
+
 function rookgaardAdvance.onAdvance(player, skill, oldLevel, newLevel)
-	if skill ~= SKILL_LEVEL or newLevel ~= 8 or player:getVocation():getId() ~= 0 then
-		return true
-	end
+	if skill ~= SKILL_LEVEL or newLevel ~= 8 then
+        return true
+    end
 
-	if player:getStorageValue(Storage.Quest.U10_55.Dawnport.GoMain) == 1 then
-		player:setStorageValue(Storage.Quest.U10_55.Dawnport.GoMain, 2)
-	end
+    if player:getVocation():getId() == 0 then
+        player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Congratulations! You are ready to leave this island and choose a vocation now. Go see the Oracle over the academy in Rookgaard before you advance to level 10!")
+    end
 
-	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Congratulations! You are ready to leave this island and choose a vocation now. Go see the Oracle over the academy in Rookgaard before you advance to level 10!")
-	return true
+    if player:getStorageValue(Storage.Quest.U10_55.Dawnport.GoMain) == 1 then
+        player:setStorageValue(Storage.Quest.U10_55.Dawnport.GoMain, 2)
+    end
+
+    return true
 end
 
 rookgaardAdvance:register()

d08c68fc5c1f3af6481e354ac3c8f1ba9f3d4c7e
LeoTK <leonardo.fabian@hotmail.com>
2025-04-06 00:50:18 -0300

    #fix Spawns_npc_by_time

--- data-otservbr-global/scripts/world_changes/spawns_npc_by_time.lua
+++ data-otservbr-global/scripts/world_changes/spawns_npc_by_time.lua
@@ -1,28 +1,81 @@
-local npcSpawns = {
-	{ name = "Ghostly Wolf", spawnPeriod = LIGHT_STATE_SUNSET, despawnPeriod = LIGHT_STATE_SUNRISE, position = { x = 33332, y = 32052, z = 7 } },
-	{ name = "Talila", spawnPeriod = LIGHT_STATE_SUNSET, despawnPeriod = LIGHT_STATE_SUNRISE, position = { x = 33504, y = 32222, z = 7 } },
-	{ name = "Valindara", spawnPeriod = LIGHT_STATE_SUNRISE, despawnPeriod = LIGHT_STATE_SUNSET, position = { x = 33504, y = 32222, z = 7 } },
-}
+local activeNpcRefs = {}
+
+local npcSpawns = {{
+    name = "Ghostly Wolf",
+    spawnPeriod = LIGHT_STATE_SUNSET,
+    despawnPeriod = LIGHT_STATE_SUNRISE,
+    position = Position(33332, 32052, 7)
+}, {
+    name = "Talila",
+    spawnPeriod = LIGHT_STATE_SUNSET,
+    despawnPeriod = LIGHT_STATE_SUNRISE,
+    position = Position(33504, 32222, 7)
+}, {
+    name = "Valindara",
+    spawnPeriod = LIGHT_STATE_SUNRISE,
+    despawnPeriod = LIGHT_STATE_SUNSET,
+    position = Position(33504, 32222, 7)
+}}
 
 local spawnsNpcByTime = GlobalEvent("SpawnsNpcByTime")
 
 function spawnsNpcByTime.onPeriodChange(period)
-	for _, npcSpawn in ipairs(npcSpawns) do
-		if npcSpawn.spawnPeriod == period then
-			local spawnNpc = Game.createNpc(npcSpawn.name, npcSpawn.position)
-			if spawnNpc then
-				spawnNpc:setMasterPos(npcSpawn.position)
-				spawnNpc:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-			end
-		elseif npcSpawn.despawnPeriod == period then
-			local despawnNpc = Npc(npcSpawn.name)
-			if despawnNpc then
-				despawnNpc:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-				despawnNpc:remove()
-			end
-		end
-	end
-	return true
+    local npcsToRemove = {}
+    local npcsToAdd = {}
+    for i, npcData in ipairs(npcSpawns) do
+        if npcData.despawnPeriod == period and activeNpcRefs[i] then
+            table.insert(npcsToRemove, {
+                index = i,
+                data = npcData
+            })
+        elseif npcData.spawnPeriod == period and not activeNpcRefs[i] then
+            table.insert(npcsToAdd, {
+                index = i,
+                data = npcData
+            })
+        end
+    end
+    for _, entry in ipairs(npcsToRemove) do
+        local npc = Creature(activeNpcRefs[entry.index])
+        if npc and npc:isNpc() then
+            npc:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+            npc:remove()
+        end
+        activeNpcRefs[entry.index] = nil
+    end
+    if #npcsToAdd > 0 then
+        addEvent(function()
+            for _, entry in ipairs(npcsToAdd) do
+                local npc = Game.createNpc(entry.data.name, entry.data.position)
+                if npc then
+                    npc:setMasterPos(entry.data.position)
+                    npc:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+                    activeNpcRefs[entry.index] = npc:getId()
+                else
+                    print("[ERRO] Falha ao spawnar:", entry.data.name)
+                end
+            end
+        end, 5000)
+    end
+
+    return true
 end
 
 spawnsNpcByTime:register()
+
+local spawnsNpcBySpawn = GlobalEvent("SpawnsNpcBySpawn")
+
+function spawnsNpcBySpawn.onStartup()
+	local currentPeriod = LIGHT_STATE_DAY
+
+	local mode = getTibiaTimerDayOrNight()
+	if mode == "night" then
+		currentPeriod = LIGHT_STATE_NIGHT
+	else
+		currentPeriod = LIGHT_STATE_DAY
+	end
+	spawnsNpcByTime.onPeriodChange(currentPeriod)
+	return true
+end
+
+spawnsNpcBySpawn:register()

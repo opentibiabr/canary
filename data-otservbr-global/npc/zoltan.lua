ce41a45be7d70361c8c815be9215b79a485703df
hecmo94 <ted.lu94@gmail.com>
2025-05-28 22:24:22 +0200

    Update zoltan.lua

--- data-otservbr-global/npc/zoltan.lua
+++ data-otservbr-global/npc/zoltan.lua
@@ -73,21 +73,26 @@ local function creatureSayCallback(npc, creature, type, message)
 		npcHandler:say("... I cannot believe my eyes. You retrieved this hat from Ferumbras' remains? That is incredible. If you give it to me, I will grant you the right to wear this hat as addon. What do you say?", npc, creature)
 		npcHandler:setTopic(playerId, 1)
 	elseif npcHandler:getTopic(playerId) == 1 then
-		if player:getSex() == PLAYERSEX_MALE and not player:hasOutfit(130, 2) then
 			if MsgContains(message, "yes") then
-				if player:getItemCount(5903) == 1 then
+			local hasAddon = (player:getSex() == PLAYERSEX_MALE and player:hasOutfit(130, 2)) or (player:getSex() == PLAYERSEX_FEMALE and player:hasOutfit(138, 2))
+
+				if hasAddon then
+					npcHandler:say("You already have the Ferumbras' hat addon.", npc, creature)
+				elseif player:getItemCount(5903) < 1 then
+					npcHandler:say("Sorry you don't have the Ferumbras' hat.", npc, creature)
+				else
 					npcHandler:say("I bow to you, player, and hereby grant you the right to wear Ferumbras' hat as accessory. Congratulations!", npc, creature)
 					player:removeItem(5903, 1)
-					player:addOutfitAddon(130, 2) -- male mage addon
-					player:addOutfitAddon(141, 2) -- female summoner addon
+					if isMale then
+						player:addOutfitAddon(130, 2) -- male mage addon
+					else
+						player:addOutfitAddon(138, 2) -- female mage addon
+					end
+					player:addOutfitAddon(141, 2) -- summoner addon for both
 					player:getPosition():sendMagicEffect(CONST_ME_MAGIC_RED)
-				else
-					npcHandler:say("Sorry you don't have the Ferumbras' hat.", npc, creature)
 				end
-			else
-				npcHandler:say("This task is only available for male players who don't already have the addon.", npc, creature)
+				npcHandler:setTopic(playerId, 0)
 			end
-		end
 	elseif MsgContains(message, "myra") then
 		if player:getSex() == PLAYERSEX_FEMALE and player:getStorageValue(Storage.Quest.U7_8.MageAndSummonerOutfits.AddonHatCloak) == 10 and not player:hasOutfit(138, 2) then
 			npcHandler:say({

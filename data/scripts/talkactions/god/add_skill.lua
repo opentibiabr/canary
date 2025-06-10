92f2dcb50bcbf4d6ef0f9e5ea49681b4285f0dec
cleberma <cleberomito@gmail.com>
2025-06-09 22:13:00 -0300

    /setskill level, correction.
    corrected the well pointed level leak where the level and XP would be changed correctly but health, mana and capacity stats stay the same.
    tested every scenario and all seems to be working fine, please review.
    
    Also I don't think the code should be separated in two files because it's the same thing with two different approaches, there's no need to split and duplicate code/files... and performance wouldn't be an issue since the script is only triggered on god talkaction, it's not a big deal.

--- data/scripts/talkactions/god/add_skill.lua
+++ data/scripts/talkactions/god/add_skill.lua
@@ -1,6 +1,3 @@
-    --Clebin, OTBR
-        --(Feito com auxilio de IA https://deepwiki.com/opentibiabr/canary & Gemini 2.5 Pro (preview), fica a recomendacao pois qualquer iniciante consegue se virar com essas ferramentas)
-        
 -- Enhanced skill map with all available skills
 local skillMap = {
     club = SKILL_CLUB,
@@ -176,13 +173,78 @@ function setSkill.onSay(player, words, param)
     local skillPrefix = skillParam:sub(1, 1)
 
     -- Handle player level setting
-    if skillPrefix == "l" then
-        targetPlayer:setLevel(skillAmount)
-        targetPlayer:sendTextMessage(MESSAGE_EVENT_ADVANCE, string.format("%s has set your level to %d.", player:getName(), skillAmount))
-        player:sendTextMessage(MESSAGE_EVENT_ADVANCE, string.format("You have successfully set player %s level to %d.", targetPlayer:getName(), skillAmount))
+    if skillPrefix == 'l' then
+        local newLevel = tonumber(skillAmount)
+        if newLevel <= 0 then
+            player:sendCancelMessage("Invalid level.")
+            return true
+        end
+
+        local currentLevel = targetPlayer:getLevel()
+        if newLevel == currentLevel then
+            player:sendCancelMessage("Player is already at that level.")
+            return true
+        end
+
+        -- First, update the player's experience to match the new level
+        local experience = Game.getExperienceForLevel(newLevel)
+        if experience > targetPlayer:getExperience() then
+            targetPlayer:addExperience(experience - targetPlayer:getExperience(), false)
+        else
+            targetPlayer:removeExperience(targetPlayer:getExperience() - experience, false)
+        end
+
+        -- Now, perform a full stat recalculation from base values (this is necessary because of levels 0 to 8 as no vocation)
+        local vocation = targetPlayer:getVocation()
+        
+        -- Base stats at level 1
+        local health = 150
+        local mana = 55
+        local capacity = 400
+
+        -- Stat gains table (id, hp, mana, cap)
+        local vocGains = {
+            [0] = {5, 5, 10}, -- None
+            [1] = {5, 30, 10}, -- Sorcerer
+            [2] = {5, 30, 10}, -- Druid
+            [3] = {10, 15, 20}, -- Paladin
+            [4] = {15, 5, 25}, -- Knight
+            [5] = {5, 30, 10}, -- Master Sorcerer
+            [6] = {5, 30, 10}, -- Elder Druid
+            [7] = {10, 15, 20}, -- Royal Paladin
+            [8] = {15, 5, 25} -- Elite Knight
+        }
+        
+        local noVocGains = vocGains[0]
+        local currentVocGains = vocGains[vocation:getId()] or noVocGains
+
+        for i = 2, newLevel do
+            if i <= 8 then
+                health = health + noVocGains[1]
+                mana = mana + noVocGains[2]
+                capacity = capacity + noVocGains[3]
+            else
+                health = health + currentVocGains[1]
+                mana = mana + currentVocGains[2]
+                capacity = capacity + currentVocGains[3]
+            end
+        end
+
+        -- Set final calculated stats
+        targetPlayer:setMaxHealth(health)
+        targetPlayer:addHealth(health - targetPlayer:getHealth())
+        targetPlayer:setMaxMana(mana)
+        targetPlayer:addMana(mana - targetPlayer:getMana())
+        
+        
+
+        targetPlayer:sendTextMessage(MESSAGE_EVENT_ADVANCE, ('%s has set your level to %d.'):format(player:getName(), newLevel))
+        player:sendTextMessage(MESSAGE_EVENT_ADVANCE, ('You have successfully set player %s level to %d.'):format(targetPlayer:getName(), newLevel))
+        return true
+    end
 
     -- Handle magic level setting
-    elseif skillPrefix == "m" then
+    if skillPrefix == "m" then
         targetPlayer:setMagicLevel(skillAmount, 0)
         targetPlayer:sendTextMessage(MESSAGE_EVENT_ADVANCE, string.format("%s has set your magic level to %d.", player:getName(), skillAmount))
         player:sendTextMessage(MESSAGE_EVENT_ADVANCE, string.format("You have successfully set player %s magic level to %d.", targetPlayer:getName(), skillAmount))

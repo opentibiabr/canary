6967d3ff67106c6ee17b18352f3fe5ed13a3151f
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2025-07-08 08:36:43 -0300

    Add participants column to player_deaths table
    Introduces a database migration that adds a 'participants' TEXT column to the 'player_deaths' table, with a default empty string. This supports tracking participants in player deaths.

---
+++ data-otservbr-global/migrations/51.lua
@@ -0,0 +1,6 @@
+
+function onUpdateDatabase()
+	logger.info("Updating database to version 51 (feat: player death participants)")
+
+	db.query("ALTER TABLE `player_deaths` ADD COLUMN `participants` TEXT NOT NULL DEFAULT '';")
+end

5729f53ae6f951fc894d6f7080b4d1e5a567bb3c
Leilani A. <168607226+kaleohanopahala@users.noreply.github.com>
2025-07-27 22:50:44 -0300

    Add files via upload

---
+++ data-otservbr-global/scripts/actions/object/dancingfairy.lua
@@ -0,0 +1,31 @@
+local dancingfairy = Action()
+
+local ORIGINAL_ID = 25747
+local TRANSFORMED_ID = 25748
+local REVERT_DELAY = 1 * 60 * 1000
+
+function dancingfairy.onUse(player, item, fromPosition, target, toPosition, isHotkey)
+	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Doooon't touch me! *puff*")
+	item:transform(TRANSFORMED_ID)
+
+	local position = item:getPosition()
+
+	addEvent(function(pos)
+		local tile = Tile(pos)
+		if not tile then return end
+
+		for _, tileItem in ipairs(tile:getItems() or {}) do
+			if tileItem:getId() == TRANSFORMED_ID then
+				tileItem:transform(ORIGINAL_ID)
+				player:addAchievementProgress("Fairy Teasing", 100)
+				break
+			end
+		end
+	end, REVERT_DELAY, position)
+
+	return true
+end
+
+dancingfairy:id(ORIGINAL_ID)
+dancingfairy:register()
+

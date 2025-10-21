5dd27b70129c034fb33946dc0f1745485fec0b6b
Leilani A. <168607226+kaleohanopahala@users.noreply.github.com>
2025-10-21 17:08:15 -0300

    Update mc_check.lua

--- data/scripts/talkactions/gm/mc_check.lua
+++ data/scripts/talkactions/gm/mc_check.lua
@@ -1,48 +1,59 @@
 local mcCheck = TalkAction("/mc")
 
 function mcCheck.onSay(player, words, param)
-	-- create log
-	logCommand(player, words, param)
+    -- create log
+    logCommand(player, words, param)
 
-	player:sendTextMessage(MESSAGE_ADMINISTRATOR, "Multiclient Check List:")
-        local ipList = {}
-        local players = Game.getPlayers()
-		for i = 1, #players do
-			local tmpPlayer = players[i]
-			local ipString = tmpPlayer:getIpString()
-			local key
-			if ipString and ipString ~= "" then
-				key = ipString
-			else
-				key = tmpPlayer:getIp()
-			end
-			if key and key ~= "" and key ~= 0 then
-                        local list = ipList[key]
-                        if not list then
-                                list = {}
-                                ipList[key] = list
-                        end
-                        list[#list + 1] = tmpPlayer
-                end
+    player:sendTextMessage(MESSAGE_ADMINISTRATOR, "Multiclient Check List:")
+
+    local ipList = {}
+    local players = Game.getPlayers()
+
+    for i = 1, #players do
+        local tmpPlayer = players[i]
+        local ipString = tmpPlayer:getIpString()
+
+        local key
+        if ipString and ipString ~= "" then
+            key = ipString
+        else
+            key = tmpPlayer:getIp()
         end
 
-        for _, list in pairs(ipList) do
-                local listLength = #list
-                if listLength > 1 then
-                        local tmpPlayer = list[1]
-                        local message = ("%s: %s [%d]"):format(
-                                Game.convertIpToString(tmpPlayer:getIp(), tmpPlayer:getIpString()),
-                                tmpPlayer:getName(),
-                                tmpPlayer:getLevel()
-                        )
-                        for i = 2, listLength do
-                                tmpPlayer = list[i]
-                                message = ("%s, %s [%d]"):format(message, tmpPlayer:getName(), tmpPlayer:getLevel())
-                        end
-                        player:sendTextMessage(MESSAGE_ADMINISTRATOR, message .. ".")
-                end
+        if key and key ~= "" and key ~= 0 then
+            local list = ipList[key]
+            if not list then
+                list = {}
+                ipList[key] = list
+            end
+            list[#list + 1] = tmpPlayer
         end
-	return true
+    end
+
+    for _, list in pairs(ipList) do
+        local listLength = #list
+        if listLength > 1 then
+            local tmpPlayer = list[1]
+            local message = ("%s: %s [%d]"):format(
+                Game.convertIpToString(tmpPlayer:getIp(), tmpPlayer:getIpString()),
+                tmpPlayer:getName(),
+                tmpPlayer:getLevel()
+            )
+
+            for i = 2, listLength do
+                tmpPlayer = list[i]
+                message = ("%s, %s [%d]"):format(
+                    message,
+                    tmpPlayer:getName(),
+                    tmpPlayer:getLevel()
+                )
+            end
+
+            player:sendTextMessage(MESSAGE_ADMINISTRATOR, message .. ".")
+        end
+    end
+
+    return true
 end
 
 mcCheck:separator(" ")

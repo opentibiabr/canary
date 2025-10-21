05a0a4bac78d089e3493b0f5f3b0cca80cab644e
Leilani A. <168607226+kaleohanopahala@users.noreply.github.com>
2025-10-21 17:08:44 -0300

    Update unban.lua

--- data/scripts/talkactions/gm/unban.lua
+++ data/scripts/talkactions/gm/unban.lua
@@ -1,29 +1,42 @@
 local unban = TalkAction("/unban")
 
 function unban.onSay(player, words, param)
-	-- create log
-	logCommand(player, words, param)
+    -- create log
+    logCommand(player, words, param)
 
-	if param == "" then
-		player:sendCancelMessage("Command param required.")
-		return true
-	end
+    if param == "" then
+        player:sendCancelMessage("Command param required.")
+        return true
+    end
 
-	local resultId = db.storeQuery("SELECT `account_id`, `lastip` FROM `players` WHERE `name` = " .. db.escapeString(param))
-	if resultId == false then
-		return true
-	end
+    local resultId = db.storeQuery(
+        "SELECT `account_id`, `lastip` FROM `players` WHERE `name` = " .. db.escapeString(param)
+    )
 
-        db.asyncQuery("DELETE FROM `account_bans` WHERE `account_id` = " .. Result.getNumber(resultId, "account_id"))
-        local lastIp = Result.getString(resultId, "lastip")
-        if lastIp and lastIp ~= "" then
-                db.asyncQuery("DELETE FROM `ip_bans` WHERE `ip` = " .. db.escapeString(lastIp))
-        end
-	Result.free(resultId)
-	local text = param .. " has been unbanned."
-	player:sendTextMessage(MESSAGE_ADMINISTRATOR, text)
-	Webhook.sendMessage("Player Unbanned", text .. " (by: " .. player:getName() .. ")", WEBHOOK_COLOR_YELLOW, announcementChannels["serverAnnouncements"])
-	return true
+    if not resultId then
+        return true
+    end
+
+    db.asyncQuery("DELETE FROM `account_bans` WHERE `account_id` = " .. Result.getNumber(resultId, "account_id"))
+
+    local lastIp = Result.getString(resultId, "lastip")
+    if lastIp and lastIp ~= "" then
+        db.asyncQuery("DELETE FROM `ip_bans` WHERE `ip` = " .. db.escapeString(lastIp))
+    end
+
+    Result.free(resultId)
+
+    local text = param .. " has been unbanned."
+    player:sendTextMessage(MESSAGE_ADMINISTRATOR, text)
+
+    Webhook.sendMessage(
+        "Player Unbanned",
+        text .. " (by: " .. player:getName() .. ")",
+        WEBHOOK_COLOR_YELLOW,
+        announcementChannels["serverAnnouncements"]
+    )
+
+    return true
 end
 
 unban:separator(" ")

94dee88690a510c1174c1e42c1d0d67acef38242
Leilani A. <168607226+kaleohanopahala@users.noreply.github.com>
2025-10-21 16:52:08 -0300

    Update player.lua

--- data/events/scripts/player.lua
+++ data/events/scripts/player.lua
@@ -205,39 +205,45 @@ local function useConcoctionTime(player)
 end
 
 function Player:onLookInBattleList(creature, distance)
-	if not creature then
-		return false
-	end
+    if not creature then
+        return false
+    end
 
-	local description = "You see " .. creature:getDescription(distance)
-	if creature:isMonster() then
-		local master = creature:getMaster()
-		local summons = { "sorcerer familiar", "knight familiar", "druid familiar", "paladin familiar" }
-		if master and table.contains(summons, creature:getName():lower()) then
-			local familiarSummonTime = master:kv():get("familiar-summon-time") or 0
-			description = description .. " (Master: " .. master:getName() .. "). \z
-				It will disappear in " .. Game.getTimeInWords(familiarSummonTime - os.time())
-		end
-	end
-	if self:getGroup():getAccess() then
-		local str = "%s\nHealth: %d / %d"
-		if creature:isPlayer() and creature:getMaxMana() > 0 then
-			str = string.format("%s, Mana: %d / %d", str, creature:getMana(), creature:getMaxMana())
-		end
-		description = string.format(str, description, creature:getHealth(), creature:getMaxHealth()) .. "."
+    local description = "You see " .. creature:getDescription(distance)
 
-		local position = creature:getPosition()
-		description = string.format("%s\nPosition: %d, %d, %d", description, position.x, position.y, position.z)
+    if creature:isMonster() then
+        local master = creature:getMaster()
+        local summons = { "sorcerer familiar", "knight familiar", "druid familiar", "paladin familiar" }
 
-                if creature:isPlayer() then
-                        description = string.format(
-                                "%s\nIP: %s",
-                                description,
-                                Game.convertIpToString(creature:getIp(), creature:getIpString())
-                        )
-                end
-	end
-	self:sendTextMessage(MESSAGE_LOOK, description)
+        if master and table.contains(summons, creature:getName():lower()) then
+            local familiarSummonTime = master:kv():get("familiar-summon-time") or 0
+            description = description .. " (Master: " .. master:getName() .. "). \z
+                It will disappear in " .. Game.getTimeInWords(familiarSummonTime - os.time())
+        end
+    end
+
+    if self:getGroup():getAccess() then
+        local str = "%s\nHealth: %d / %d"
+
+        if creature:isPlayer() and creature:getMaxMana() > 0 then
+            str = string.format("%s, Mana: %d / %d", str, creature:getMana(), creature:getMaxMana())
+        end
+
+        description = string.format(str, description, creature:getHealth(), creature:getMaxHealth()) .. "."
+
+        local position = creature:getPosition()
+        description = string.format("%s\nPosition: %d, %d, %d", description, position.x, position.y, position.z)
+
+        if creature:isPlayer() then
+            description = string.format(
+                "%s\nIP: %s",
+                description,
+                Game.convertIpToString(creature:getIp(), creature:getIpString())
+            )
+        end
+    end
+
+    self:sendTextMessage(MESSAGE_LOOK, description)
 end
 
 local exhaust = {}

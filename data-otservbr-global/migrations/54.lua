95cc1241f927b569108c102feb1170cf90486a0f
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2026-01-17 13:53:17 -0300

    Refactor database update for version 54

--- data-otservbr-global/migrations/54.lua
+++ data-otservbr-global/migrations/54.lua
@@ -1,13 +1,12 @@
 function onUpdateDatabase()
-    logger.info("Updating database to version 54 (set community manager and god to type/group 6)")
+	logger.info("Updating database to version 54 (set god to account type 6)")
 
-    local updateAccounts = db.query([[UPDATE accounts SET type = 6 WHERE type = 5;]])
-    local updatePlayers = db.query([[UPDATE players SET group_id = 6 WHERE group_id = 5;]])
+	local updateAccounts = db.query([[UPDATE accounts SET type = 6 WHERE type = 5;]])
 
-    if not updateAccounts or not updatePlayers then
-        logger.error("Failed to migrate god group values to 6.")
-        return false
-    end
+	if not updateAccounts then
+		logger.error("Failed to migrate god type values to 6.")
+		return false
+	end
 
-    return true
+	return true
 end

7e94eeef110a10257b1a384075ad6c14b2edc322
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2026-01-31 05:20:50 -0300

    Rename statistics retrieval methods for clarity

--- src/io/iomarket.cpp
+++ src/io/iomarket.cpp
@@ -379,12 +379,12 @@ void IOMarket::updateStatistics() {
 	}
 }
 
-IOMarket::StatisticsMap IOMarket::getPurchaseStatisticsCopy() const {
+IOMarket::StatisticsMap IOMarket::getPurchaseStatistics() const {
 	std::lock_guard<std::mutex> lock(statisticsMutex);
 	return purchaseStatistics;
 }
 
-IOMarket::StatisticsMap IOMarket::getSaleStatisticsCopy() const {
+IOMarket::StatisticsMap IOMarket::getSaleStatistics() const {
 	std::lock_guard<std::mutex> lock(statisticsMutex);
 	return saleStatistics;
 }

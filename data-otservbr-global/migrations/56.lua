651aaabb88d4e62c49bdce6e38dc61aba6408e75
slawkens <slawkens@gmail.com>
2026-03-13 20:41:16 +0100

    Add error handling

--- data-otservbr-global/migrations/56.lua
+++ data-otservbr-global/migrations/56.lua
@@ -1,7 +1,15 @@
 function onUpdateDatabase()
 	logger.info("Updating database to version 56 (feat: accounts email + password indexes)")
 
-	db.query("ALTER TABLE `accounts` MODIFY `password` VARCHAR(255) NOT NULL;")
-	db.query("CREATE INDEX `accounts_email` ON `accounts` (`email`);")
-	db.query("CREATE INDEX `accounts_password` ON `accounts` (`password`);")
+	if not db.query("ALTER TABLE `accounts` MODIFY `password` VARCHAR(255) NOT NULL;") then
+		logger.error("Failed to modify password field.")
+	end
+
+	if not db.query("CREATE INDEX `accounts_email` ON `accounts` (`email`);") then
+		logger.error("Failed to create accounts email index.")
+	end
+
+	if not db.query("CREATE INDEX `accounts_password` ON `accounts` (`password`);") then
+		logger.error("Failed to create accounts password index."
+	end
 end

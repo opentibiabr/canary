91a72b547b94b7cceebebcdf9c1c5061d2b43fe6
Jonyrewind <jonyrewind@gmail.com>
2026-04-15 02:23:47 +0200

    Update actions_entrances.lua

--- data-otservbr-global/scripts/quests/rotten_blood_quest/actions_entrances.lua
+++ data-otservbr-global/scripts/quests/rotten_blood_quest/actions_entrances.lua
@@ -28,7 +28,6 @@ function teleportEvent.onUse(player, item, fromPosition, target, toPosition, isH
 	local access = player:kv():scoped("rotten-blood-quest"):get("access") or 0
 	if access < 5 then
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You should pay respect to the Bloodshade guarding this realm before entering.")
-		player:teleportTo(fromPosition, true)
 		return false
 	end
 
@@ -63,7 +62,6 @@ function bakragoreEntrance.onUse(player, item, fromPosition, target, toPosition,
 
 	if player:getLevel() < 250 then
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You need at least level 250 to enter.")
-		player:teleportTo(fromPosition, true)
 		return false
 	end
 
@@ -77,7 +75,6 @@ function bakragoreEntrance.onUse(player, item, fromPosition, target, toPosition,
 
 	if text ~= "" then
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You still need to defeat: " .. text)
-		player:teleportTo(fromPosition, true)
 		return false
 	end
 

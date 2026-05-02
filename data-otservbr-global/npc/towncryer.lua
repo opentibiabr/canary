c11e31a992ef766893f71624b32a699a810d50e3
Jonyrewind <jonyrewind@gmail.com>
2026-05-02 22:46:00 +0200

    Update towncryer.lua

--- data-otservbr-global/npc/towncryer.lua
+++ data-otservbr-global/npc/towncryer.lua
@@ -32,15 +32,29 @@ npcConfig.voices = {
 }
 
 local worldChanges = {
-	{ text = "In Ankrahmun's desert, a storm has revealed the entry to a nightmare that can't be sealed. Horrible creatures there spell instant death to all young adventurers who dare take a breath!", storage = GlobalStorage.WorldBoard.NightmareIsle.AnkrahmunNorth },
+	{ text = "In Ankrahmun's desert, a storm has revealed the entry to a nightmare that can't be sealed. Horrible creatures there spell instant death to all young adventurers who dare take a breath!", storage = GlobalStorage.WorldBoard.NightmareIsle.AnkhramunNorth },
 	{ text = "Near Darashia's coast, a storm has revealed the entry to a nightmare that can't be sealed. Horrible creatures there spell instant death to all young adventurers who dare take a breath!", storage = GlobalStorage.WorldBoard.NightmareIsle.DarashiaNorth },
 	{ text = "Near Drefia's mountains, a storm has revealed the entry to a nightmare that can't be sealed. Horrible creatures there spell instant death to all young adventurers who dare take a breath!", storage = GlobalStorage.WorldBoard.NightmareIsle.DarashiaWest },
 	{ text = "Hear ye! Hear ye! What a lucky and beautiful day! Visit Carlin, Ankrahmun, or Liberty Bay. Yasir, the oriental trader might be there. Gather your creature products, for this chance is rare.", storage = GlobalStorage.Yasir },
 	{ text = "Hear ye! Hear ye! A fiery gate has opened, threatening a city! Guard the people frightened, their death would be a pity!", storage = GlobalStorage.FuryGates },
+	{ text = "Hear ye! Hear ye! The witch Wyda seems to be bored. Pay her a visit but sharpen your sword. She might come up with a terrible surprise, are you brave enough to believe your eyes?", kv = KV.scoped("worldchanges"):scoped("bored"), kvKey = "active" },
 }
 
+local function isWorldChangeActive(entry)
+	if entry.storage ~= nil then
+		return Game.getStorageValue(entry.storage) > 0
+	end
+
+	if entry.kv ~= nil then
+		local kvValue = entry.kv:get(entry.kvKey or "value")
+		return kvValue == true or kvValue == 1
+	end
+
+	return false
+end
+
 for i = 1, #worldChanges do
-	if Game.getStorageValue(worldChanges[i].storage) > 0 then
+	if isWorldChangeActive(worldChanges[i]) then
 		table.insert(npcConfig.voices, { text = worldChanges[i].text })
 	end
 end

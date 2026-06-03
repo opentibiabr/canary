bf724acd62930a8cb417b0afdc8e9298bede5d0c
Paco <frjafopa@gmail.com>
2026-06-03 14:50:00 -0600

    feat: rotate in android
    Use !r or !rotate commands to turn your character direction.

---
+++ data/scripts/talkactions/player/rotate.lua
@@ -0,0 +1,12 @@
+local function onSayRotate(player, words, param)
+	local currentDir = player:getDirection()
+	local nextDir = (currentDir + 1) % 4
+	player:setDirection(nextDir)
+	local dirNames = { [0] = "north", [1] = "east", [2] = "south", [3] = "west" }
+	return true
+end
+
+local talkAction = TalkAction("!r", "!rotate")
+talkAction:onSay(onSayRotate)
+talkAction:groupType("normal")
+talkAction:register()

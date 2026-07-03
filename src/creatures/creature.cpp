c7209b4d78f2899dfe9b7f3a36003d781e744f98
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2026-07-03 16:47:34 -0300

    perf(creature): reduce async task bucket slice

--- src/creatures/creature.cpp
+++ src/creatures/creature.cpp
@@ -33,7 +33,7 @@
 
 namespace {
 	constexpr size_t CREATURE_ASYNC_TASK_BUCKET_COUNT = 32;
-	constexpr size_t CREATURE_ASYNC_TASK_BATCH_SIZE = 64;
+	constexpr size_t CREATURE_ASYNC_TASK_BATCH_SIZE = 16;
 
 	struct CreatureAsyncTaskBucket {
 		std::mutex mutex;

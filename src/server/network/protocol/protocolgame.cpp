69ff8a2bdf4c0aaad8ee40467fdec5f7486fa11b
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2026-07-03 16:54:43 -0300

    fix(protocol): populate game event screenshot payloads

--- src/server/network/protocol/protocolgame.cpp
+++ src/server/network/protocol/protocolgame.cpp
@@ -11139,7 +11139,7 @@ void ProtocolGame::sendDisableLoginMusic() {
 	writeToOutputBuffer(msg);
 }
 
-void ProtocolGame::sendTakeScreenshot(Screenshot_t screenshotType) {
+void ProtocolGame::sendTakeScreenshot(Screenshot_t screenshotType, uint8_t skillId, uint16_t skillLevel, const std::string &achievementName, uint16_t raceId, uint8_t bestiaryStep) {
 	if (screenshotType == SCREENSHOT_TYPE_NONE || oldProtocol) {
 		return;
 	}
@@ -11149,14 +11149,33 @@ void ProtocolGame::sendTakeScreenshot(Screenshot_t screenshotType) {
 
 	if (hasProtocolFeature(protocolProfile, ProtocolFeature::GameEventPayload)) {
 		switch (screenshotType) {
+			case SCREENSHOT_TYPE_ACHIEVEMENT:
+				if (achievementName.empty()) {
+					return;
+				}
+				msg.addByte(0x02);
+				msg.addString(achievementName);
+				break;
+			case SCREENSHOT_TYPE_BESTIARYENTRYCOMPLETED:
+			case SCREENSHOT_TYPE_BESTIARYENTRYUNLOCKED:
+				if (raceId == 0) {
+					return;
+				}
+				msg.addByte(0x06);
+				msg.add<uint16_t>(raceId);
+				msg.addByte(bestiaryStep);
+				break;
 			case SCREENSHOT_TYPE_LEVELUP:
 				msg.addByte(0x04);
 				msg.add<uint16_t>(std::min<uint32_t>(player ? player->getLevel() : 0, 0xFFFF));
 				break;
 			case SCREENSHOT_TYPE_SKILLUP:
+				if (skillId == 0 || skillLevel == 0) {
+					return;
+				}
 				msg.addByte(0x05);
-				msg.addByte(0x00);
-				msg.add<uint16_t>(0x00);
+				msg.addByte(skillId);
+				msg.add<uint16_t>(skillLevel);
 				break;
 			case SCREENSHOT_TYPE_BOSSDEFEATED:
 				msg.addByte(0x01);
@@ -11193,12 +11212,9 @@ void ProtocolGame::sendTakeScreenshot(Screenshot_t screenshotType) {
 			default:
 				return;
 		}
-
-		writeToOutputBuffer(msg);
-		return;
+	} else {
+		msg.addByte(screenshotType);
 	}
-
-	msg.addByte(screenshotType);
 
 	writeToOutputBuffer(msg);
 }

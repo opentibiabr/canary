25b9bda24c85c0858f013dcf94e5193bee2b310e
blakinio <75369544+blakinio@users.noreply.github.com>
2026-07-10 16:14:17 +0200

    feat: share The Ape City catacomb access by account

--- data-otservbr-global/scripts/quests/the_ape_city/movements_mission9_the_deepest_catacomb_teleport.lua
+++ data-otservbr-global/scripts/quests/the_ape_city/movements_mission9_the_deepest_catacomb_teleport.lua
@@ -9,6 +9,7 @@ local amphoraStorages = {
 
 local cooldownStorage = Storage.Quest.U7_6.TheApeCity.TheLargeAmphorasCooldown
 local cooldownTime = 5 * 60
+local accountQuestId = "the-ape-city"
 
 function mission9TheDeepestCatacombTeleport.onStepIn(creature, item, position, fromPosition)
 	local player = creature:getPlayer()
@@ -16,10 +17,20 @@ function mission9TheDeepestCatacombTeleport.onStepIn(creature, item, position, f
 		return true
 	end
 
-	if item.uid == 9257 and player:getStorageValue(Storage.Quest.U7_6.TheApeCity.Questline) >= 17 then
-		player:teleportTo(Position(32749, 32536, 10))
-		position:sendMagicEffect(CONST_ME_TELEPORT)
-		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+	if item.uid == 9257 then
+		local hasCharacterAccess = player:getStorageValue(Storage.Quest.U7_6.TheApeCity.Questline) >= 17
+		local hasAccountAccess = player:hasAccountQuestAccess(accountQuestId)
+
+		if hasCharacterAccess then
+			player:unlockAccountQuestAccess(accountQuestId)
+		end
+
+		if hasCharacterAccess or hasAccountAccess then
+			player:teleportTo(Position(32749, 32536, 10))
+			position:sendMagicEffect(CONST_ME_TELEPORT)
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+			return true
+		end
 	elseif item.uid == 9258 then
 		local cooldownStart = player:getStorageValue(cooldownStorage)
 		if cooldownStart > 0 and (os.time() - cooldownStart) > cooldownTime then
@@ -31,8 +42,7 @@ function mission9TheDeepestCatacombTeleport.onStepIn(creature, item, position, f
 
 		local allAmphorasBroken = true
 		for _, storage in pairs(amphoraStorages) do
-			local storageValue = player:getStorageValue(storage)
-			if storageValue < 1 then
+			if player:getStorageValue(storage) < 1 then
 				allAmphorasBroken = false
 				break
 			end
@@ -42,17 +52,18 @@ function mission9TheDeepestCatacombTeleport.onStepIn(creature, item, position, f
 			player:teleportTo(Position(32885, 32632, 11))
 			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 			return true
-		else
-			player:teleportTo(fromPosition)
-			position:sendMagicEffect(CONST_ME_TELEPORT)
-			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You need to break all 4 large amphoras to use this teleporter.")
-			return true
 		end
-	else
-		player:teleportTo(fromPosition, true)
-		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You don't have access to this area.")
+
+		player:teleportTo(fromPosition)
+		position:sendMagicEffect(CONST_ME_TELEPORT)
+		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You need to break all 4 large amphoras to use this teleporter.")
+		return true
 	end
+
+	player:teleportTo(fromPosition, true)
+	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You don't have access to this area.")
+	return true
 end
 
 mission9TheDeepestCatacombTeleport:type("stepin")

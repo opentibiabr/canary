c629e1764454bca20b160ea6af1226ff55dddea3
blakinio <75369544+blakinio@users.noreply.github.com>
2026-07-10 14:46:34 +0200

    fix: restore periodic server saves
    Restore interval-based saves by using the correct GlobalEvent callback and decoupling periodic saves from the daily global server save time.

--- data/scripts/globalevents/save_interval.lua
+++ data/scripts/globalevents/save_interval.lua
@@ -1,55 +1,29 @@
-local saveTimeStr = configManager.getString(configKeys.GLOBAL_SERVER_SAVE_TIME) or "00:00"
+local save = GlobalEvent("save")
+local warningDelay = 60 * 1000
+local configuredInterval = SAVE_INTERVAL_CONFIG_TIME * SAVE_INTERVAL_TIME
 local isSaveScheduled = false
 
-local function serverSave(interval)
+local function serverSave()
 	isSaveScheduled = false
+
 	if configManager.getBoolean(configKeys.TOGGLE_SAVE_INTERVAL_CLEAN_MAP) then
 		cleanMap()
 	end
 
+	logger.info("Starting interval server save...")
 	saveServer()
-	local message = string.format(SAVE_INTERVAL_CONFIG_TIME > 1 and "Server save complete. Next save in %d %ss!" or "Server save complete. Next save in %d %s!", SAVE_INTERVAL_CONFIG_TIME, SAVE_INTERVAL_TYPE)
+
+	local message = string.format(
+		SAVE_INTERVAL_CONFIG_TIME > 1 and "Server save complete. Next save in %d %ss!" or "Server save complete. Next save in %d %s!",
+		SAVE_INTERVAL_CONFIG_TIME,
+		SAVE_INTERVAL_TYPE
+	)
 	Game.broadcastMessage(message, MESSAGE_GAME_HIGHLIGHT)
 	logger.info(message)
 	Webhook.sendMessage("Server save", message, WEBHOOK_COLOR_WARNING)
 end
 
-local function getTimeLeftMs(timeStr)
-	if type(timeStr) ~= "string" then
-		return nil
-	end
-
-	local hStr, mStr = timeStr:match("^(%d+):(%d+)$")
-	if not hStr or not mStr then
-		return nil
-	end
-
-	local h, m = tonumber(hStr), tonumber(mStr)
-	if not h or not m or h < 0 or h > 23 or m < 0 or m > 59 then
-		return nil
-	end
-
-	local nowTs = os.time()
-	local nowDate = os.date("*t", nowTs)
-	local saveTs = os.time({
-		year = nowDate.year,
-		month = nowDate.month,
-		day = nowDate.day,
-		hour = h,
-		min = m,
-		sec = 0,
-	})
-
-	if saveTs <= nowTs then
-		saveTs = saveTs + 24 * 60 * 60
-	end
-
-	return (saveTs - nowTs) * 1000
-end
-
-local save = GlobalEvent("save")
-
-function save.onTime(interval)
+function save.onThink(interval)
 	if not configManager.getBoolean(configKeys.TOGGLE_SAVE_INTERVAL) then
 		return true
 	end
@@ -58,31 +32,22 @@ function save.onTime(interval)
 		return true
 	end
 
-	local WARNING = 60 * 1000
-	local timeLeft = getTimeLeftMs(saveTimeStr)
-
-	if not timeLeft or timeLeft > WARNING then
-		return true
-	end
-
-	if timeLeft <= 1000 then
-		serverSave(interval)
-	else
-		local secs = math.floor(timeLeft / 1000)
-		local msg = string.format("The server will save all accounts within %d seconds. " .. "You might lag or freeze for 5 seconds, please find a safe place.", secs)
-		Game.broadcastMessage(msg, MESSAGE_GAME_HIGHLIGHT)
-		logger.info(msg)
+	if configuredInterval > warningDelay then
+		local message = "The server will save all accounts within 60 seconds. You might lag or freeze briefly; please find a safe place."
+		Game.broadcastMessage(message, MESSAGE_GAME_HIGHLIGHT)
+		logger.info(message)
 		isSaveScheduled = true
-		addEvent(serverSave, timeLeft - 1000, interval)
+		addEvent(serverSave, warningDelay)
+	else
+		serverSave()
 	end
 
 	return true
 end
 
-if SAVE_INTERVAL_TIME ~= 0 then
-	save:interval(SAVE_INTERVAL_CONFIG_TIME * SAVE_INTERVAL_TIME)
-else
-	return logger.error(string.format("[save.onTime] - Save interval type '%s' is not valid, use 'second', 'minute' or 'hour'", SAVE_INTERVAL_TYPE))
+if SAVE_INTERVAL_TIME == 0 then
+	return logger.error(string.format("[save.onThink] - Save interval type '%s' is not valid, use 'second', 'minute' or 'hour'", SAVE_INTERVAL_TYPE))
 end
 
+save:interval(configuredInterval)
 save:register()

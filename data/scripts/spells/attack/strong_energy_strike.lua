e57fc9fb6b5267df605fff9e73c7f50e34af6d03
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2026-07-13 19:58:13 -0300

    fix(sorcerer): update strike spell base powers

--- data/scripts/spells/attack/strong_energy_strike.lua
+++ data/scripts/spells/attack/strong_energy_strike.lua
@@ -1,12 +1,13 @@
+local SPELL_BASE_POWER = 125
+
 local combat = Combat()
 combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ENERGYAREA)
 combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_ENERGY)
 
 function onGetFormulaValues(player, level, maglevel)
-	local min = (level / 5) + (maglevel * 2.8) + 16
-	local max = (level / 5) + (maglevel * 4.4) + 28
-	return -min, -max
+	local damage = player:calculateFlatDamageHealing() + (SPELL_BASE_POWER / 25 * maglevel) + (SPELL_BASE_POWER / 4)
+	return -(damage * 0.9), -(damage * 1.1)
 end
 
 combat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
--- data/scripts/spells/attack/ultimate_energy_strike.lua
+++ data/scripts/spells/attack/ultimate_energy_strike.lua
@@ -1,12 +1,13 @@
+local SPELL_BASE_POWER = 210
+
 local combat = Combat()
 combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ENERGYAREA)
 combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_ENERGY)
 
 function onGetFormulaValues(player, level, maglevel)
-	local min = (level / 5) + (maglevel * 4.5) + 35
-	local max = (level / 5) + (maglevel * 7.3) + 55
-	return -min, -max
+	local damage = player:calculateFlatDamageHealing() + (SPELL_BASE_POWER / 25 * maglevel) + (SPELL_BASE_POWER / 4)
+	return -(damage * 0.9), -(damage * 1.1)
 end
 
 combat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")

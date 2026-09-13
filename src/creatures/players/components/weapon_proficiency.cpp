b18d32a4deb74c69cde8712823d22ac76d8405a5
LIBERGOD <37589902+libergod@users.noreply.github.com>
2026-09-12 19:46:15 -0700

    commit-1
    feat: add sound effects for eating actions and VIP login/logout notifications
    
    fix: duplicated sound (heal) for every attack even when not having life/mana leech

--- src/creatures/players/components/weapon_proficiency.cpp
+++ src/creatures/players/components/weapon_proficiency.cpp
@@ -1385,22 +1385,26 @@ void WeaponProficiency::applyOn(WeaponProficiencyHealth_t healthType, WeaponProf
 		statsType = gainType == WeaponProficiencyGain_t::KILL ? MANA_GAIN_ON_KILL : MANA_GAIN_ON_HIT;
 	}
 
+	const auto statValue = getStat(statsType);
+	if (statValue <= 0) {
+		return;
+	}
+
 	CombatParams params;
 	params.combatType = COMBAT_HEALING;
-	params.soundImpactEffect = SoundEffect_t::SPELL_LIGHT_HEALING;
 
 	CombatDamage damage;
 
 	damage.origin = ORIGIN_WEAPON_PROFICIENCY;
 
 	damage.primary.type = params.combatType;
-	damage.primary.value = getStat(statsType);
+	damage.primary.value = static_cast<int32_t>(statValue);
 
 	const auto &playerCreature = m_player.getCreature();
 	if (healthType == WeaponProficiencyHealth_t::LIFE) {
-		Combat::doCombatHealth(nullptr, playerCreature, damage, params);
+		Combat::doCombatHealth(playerCreature, playerCreature, damage, params);
 	} else {
-		Combat::doCombatMana(nullptr, playerCreature, damage, params);
+		Combat::doCombatMana(playerCreature, playerCreature, damage, params);
 	}
 }
 
